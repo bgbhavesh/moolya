763018ca8d6b19755e1973456c1a1dcb767a505d
mohasinraksan <mohammed.mohasin@raksan.in>
2017-01-31 15:46:17 +0530

    Added generic  Form Handler HOC for form components.
    1) Form Handler  HOC wraps the form component to provide the loading and error functionality whic is common for forms.
    
    2) form component should pass the handleMethod,handleSuccess,handleError params to handler function.

---
+++ client/commons/containers/MlFormHandler.jsx
@@ -0,0 +1,59 @@
+/**
+ * Created by mohdmohasin on 29/1/17.
+ */
+import React, { Component } from 'react';
+
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+export default function formHandler() {
+  return (SourceComponent) => {
+    class MlFormHandler extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          loading: false,
+          error: '',
+        };
+        return;
+      }
+
+      handler(handleMethod,handleSuccess,handleError){
+        if(handleMethod){
+            this.setState({ loading: true, error: '' });
+
+            try{
+              let resp=handleMethod();
+              handleSuccess();
+            }catch(error){
+              this.setState({ loading: false, error: '' });
+              if(handleError){
+                handleError();
+              }
+            }
+        }
+      }
+
+
+
+      render() {
+        const showLoader=this.state.loading;
+
+        return (
+          <div>
+          {showLoader===true? (<div>Loading...</div>) :
+            (<SourceComponent {...this.props} handler={this.handler}/>)
+          }
+        </div>
+        )
+
+       // return <SourceComponent {...this.props} handler={this.handler}/>
+      }
+    }
+
+    MlFormHandler.displayName = `MlFormHandler(${getDisplayName(SourceComponent)})`;
+    hoistNonReactStatic(MlFormHandler, SourceComponent);
+    return MlFormHandler;
+  }
+};

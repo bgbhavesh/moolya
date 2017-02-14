0e717d064fc33f0d3e8b72384c87e3361dbf9358
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-02-14 13:29:47 +0530

    MOOLYA-123 google map component integrated.

--- client/commons/components/map/MoolyaMapView.jsx
+++ client/commons/components/map/MoolyaMapView.jsx
@@ -2,20 +2,72 @@ import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import  $ from 'jquery'
-export default class MoolyaCluster extends Component {
+import shouldPureComponentUpdate from 'react-pure-render/function';
+import controllable from 'react-controllables';
+import GoogleMap from 'google-map-react';
+import MapMarkers from './mapMarkers'
 
-  render(){
+@controllable(['center', 'zoom', 'hoverKey', 'clickKey'])
+export default class MoolyaMapView extends Component {
 
+  constructor(props){
+    super(props);
+  }
+  componentWillMount(){
+    this.setState({
+      center: {lat: 23.6850, lng: 90.3563},
+      zoom: 1,
+    });
+  }
+
+  render()
+  {
+    const places = this.props.data?this.props.data.data.map(place => {
+      // const { ...data} = place;
+      return (
+        <MapMarkers key={place.countryId} lat={place.latitude} lng={place.longitude} text={place.displayName} hover={this.props.hoverKey === place._id} desc={place.about}/>
+      );
+    }):[];
     return(
-        <div className="map_view_block">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d64726659.874872215!2d23.88162120664553!3d8.039924591525171!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1482583926409" width="100%" height="100%" frameBorder={0} style={{border: 0, float: 'left'}} allowFullScreen />
-
-        </div>
-
-
+      <div className="map">
+        <GoogleMap
+          bootstrapURLKeys={{key: "AIzaSyDnlajbJKYdilDs22Rvso9blSOj8KuL8-A"}}
+          center={this.state.center}
+          zoom={this.state.zoom}
+          hoverDistance={40 / 2}
+          onBoundsChange={this._onBoundsChange}
+          onChildClick={this._onChildClick}
+          onChildMouseEnter={this._onChildMouseEnter}
+          onChildMouseLeave={this._onChildMouseLeave}>
+          {places}
+        </GoogleMap>
+      </div>
     )
   }
 
+}
+MoolyaMapView.propTypes = {
+  center: PropTypes.array,
+  zoom: PropTypes.number,
+  // greatPlaceCoords: PropTypes.any
+};
+MoolyaMapView.shouldComponentUpdate = shouldPureComponentUpdate;
+
+MoolyaMapView._onBoundsChange = (center, zoom) => {
+  this.props.onCenterChange(center);
+  this.props.onZoomChange(zoom);
+}
+
+MoolyaMapView._onChildClick = (key, childProps) => {
+  this.props.onCenterChange([childProps.latitude, childProps.longitude]);
+}
+
+MoolyaMapView._onChildMouseEnter = (key) => {
+  this.props.onHoverKeyChange(key);
+}
+
+MoolyaMapView._onChildMouseLeave = () => {
+  this.props.onHoverKeyChange(null);
 }
 
 

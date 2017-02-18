def8be973309b55f184db832d4d4e598a84c8c6f
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-02-18 13:24:43 +0530

    SubChapter query commented out

--- client/admin/subChapter/config/mlSubChapterConfig.js
+++ client/admin/subChapter/config/mlSubChapterConfig.js
@@ -14,28 +14,28 @@ const mlSubChapterListConfig=new MlViewer.View({
   pagination:true,
   sort:true,
   viewComponent:<MlSubChapterList />,
-  graphQlQuery:gql`
-               query{
-                  data:fetchSubChapters($id: String){
-                     totalRecords
-                        data{
-                         ...on SubChapter{
-                                  clusterName
-                                  chapterName
-                                  subChapterName
-                                  subChapterDisplayName
-                                  aboutSubChapter
-                                  subChapterImageLink
-                                  subChapterEmail
-                                  isEmailNotified 
-                                  showOnMap
-                                  isActive
-                              }
-                         }
-                      
-                  }
-               }
-              `
+  // graphQlQuery:gql`
+  //              query{
+  //                 data:fetchSubChapters($id: String){
+  //                    totalRecords
+  //                       data{
+  //                        ...on SubChapter{
+  //                                 clusterName
+  //                                 chapterName
+  //                                 subChapterName
+  //                                 subChapterDisplayName
+  //                                 aboutSubChapter
+  //                                 subChapterImageLink
+  //                                 subChapterEmail
+  //                                 isEmailNotified
+  //                                 showOnMap
+  //                                 isActive
+  //                             }
+  //                        }
+  //
+  //                 }
+  //              }
+  //             `
 });
 
 export { mlSubChapterListConfig};

1828d51c74edb2e0950ca745ea72cb913b300453
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-02-23 19:55:29 +0530

    Map Marker implemented for cluster/chapter/subChapter modules

--- client/commons/components/map/mapMarkers.jsx
+++ client/commons/components/map/mapMarkers.jsx
@@ -1,8 +1,10 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import controllable from 'react-controllables';
+import MapDetails from './mapDetails'
+import {render} from 'react-dom';
 
-const K_SIZE = 40;
+const K_SIZE = 80;
 
 const mapMarkerStyle = {
   // initially any map object has left top corner at lat lng coordinates
@@ -32,31 +34,33 @@ const mapMarkerStyleHover = {
 };
 
 export default class MapMarkers extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    console.log(this.props);
+    this.state = {data: {}, isClick: false};
     this.markerClickHandler.bind(this);
   }
 
-  info(){
+  info() {
     $(".isDisplay").show();
     $(".hoverContent").hide();
   }
 
-  onMouseEnterContent(){
-    if(!$(".isDisplay").is(':visible')){
-      $(".hoverContent").show();
-    }
-
+  onMouseEnterContent() {
+    this.setState({isHover: true});
+    // if(!$(".isDisplay").is(':visible')){
+    //   $(".hoverContent").show();
+    // }
   }
 
-  onMouseLeaveContent(){
-    $(".isDisplay").hide();
+  onMouseLeaveContent() {
+    this.setState({isHover: false});
+    // $(".isDisplay").hide();
   }
 
-  markerClickHandler(data){
+  markerClickHandler(data) {
     alert(data.text);
+    console.log(data);
+    // this.setState({isHover: true});
     //this.actionHandler(data);
     //Action Handler Gives the data.
     //Use session to show and hide the Tab
@@ -65,66 +69,17 @@ export default class MapMarkers extends Component {
   render() {
     const style = this.props.hover ? mapMarkerStyleHover : mapMarkerStyle;
     return (
-      <div className="map_view_block" id={this.props.markerId} onClick={this.markerClickHandler.bind(this,this.props)}>
-        <div className="cluster_map inactive" style={{'top': '30%', 'left': '35%'}}>
-          <div className="hex_btn hex_btn_in">
-            <span><img src="/images/hover_image.png" /><b>{this.props.text}</b></span>
-            <div className="indec"></div>
-          </div>
-        </div>
 
-        <div className="chapter_map_hover" style={{'top': '40%', 'left': '30%','display':'none'}}>
-          <div className="chapter_map_hover_content">
-            <h2>USA</h2>
-            <ul>
-              <li>
-                <img src="/images/hover_image.png" />
-                <div className="hex_btn2 hex_btn_in2">123</div>
-              </li>
-              <li>
-                <img src="/images/hover_image.png" />
-                <div className="hex_btn2 hex_btn_in2">123</div>
-              </li>
-              <li>
-                <img src="/images/hover_image.png" />
-                <div className="hex_btn2 hex_btn_in2">123</div>
-              </li>
-              <li>
-                <img src="/images/hover_image.png" />
-                <div className="hex_btn2 hex_btn_in2">123</div>
-              </li>
-              <li>
-                <img src="/images/hover_image.png" />
-                <div className="hex_btn2 hex_btn_in2">123</div>
-              </li>
-              <li>
-                <img src="/images/hover_image.png" />
-                <div className="hex_btn2 hex_btn_in2">123</div>
-              </li>
-              <li>
-                <img src="/images/hover_image.png" />
-                <div className="hex_btn2 hex_btn_in2">123</div>
-              </li>
-              <li>
-                <img src="/images/hover_image.png" />
-                <div className="hex_btn2 hex_btn_in2">123</div>
-              </li>
-            </ul>
-          </div>
+      <div style={{'width': '200px'}} className="cluster_map inactive" id={this.props.markerId}
+           onMouseEnter={this.onMouseEnterContent.bind(this)} onMouseLeave={this.onMouseLeaveContent.bind(this)}
+           onClick={this.markerClickHandler.bind(this, this.props)}>
+        <div className="hex_btn hex_btn_in">
+          <span><img src="/images/hover_image.png"/><b>{this.props.text}</b></span>
+          <div className="indec"></div>
         </div>
-
+        {this.state.isHover ? (<div><MapDetails allData={this.props}/></div>) : ""}
 
       </div>
-
-      // <div className="hint hint--html hint--info hint--top" style={style} onClick={this.info} onMouseEnter={this.onMouseEnterContent}
-      //      onMouseLeave={this.onMouseLeaveContent}>
-      //   <div>{this.props.text}</div>
-      //   <div style={{width: 80}} className="hoverContent hint__content">{this.props.text}</div>
-      //
-      //   <div className="isDisplay hint__content">
-      //     {this.props.desc}
-      //   </div>
-      // </div>
     );
   }
 }
@@ -133,3 +88,4 @@ MapMarkers.propTypes = {
 }
 MapMarkers.defaultProps = {};
 MapMarkers.shouldComponentUpdate = shouldPureComponentUpdate;
+

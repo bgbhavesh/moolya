c0e59f256ac5f29ebe04bd1da0a0d1e08c21ad7f
pavani <pavani.pasula@raksan.in>
2017-02-23 13:32:03 +0530

    created setting routes ,added seeddata for clusterAdmin

---
+++ server/MlInternalUsers/admin/seedData/mlClusterAdminMenus-seeddata.js
@@ -0,0 +1,202 @@
+if(Meteor.isServer){
+  MlMenus.upsert({name:"mlClusterAdminMenu"},{$set:{
+    "name" : "mlClusterAdminMenu",
+    "menu" : [
+      {
+        "image" : "/images/db_icon.png",
+        "link" : "/admin/dashboard/clusters",
+        "name" : "dashboard",
+        "uniqueId" : "dashboard",
+        "isLink" : true,
+        "isMenu" : true,
+        "subMenu" : [
+          {
+            "link" : "/admin/dashboard/clusters",
+            "name" : "Clusters",
+            "uniqueId" : "dashboard_clusters",
+            "subMenuMappingId":"dashboard_clusters",
+            "subMenusId":"dashboard",
+            "isLink" : true,
+            "isMenu" : true,
+            "image" : ""
+          },
+          {
+            "link" : "/admin/dashboard/chapters",
+            "name" : "Chapters",
+            "dynamicLink" : true,
+            "dynamicLinkHandler" : "",
+            "uniqueId" : "dashboard_chapters",
+            "subMenuMappingId":"dashboard_chapters",
+            "subMenusId":"dashboard",
+            "isLink" : true,
+            "image" : "",
+            "subMenu":[
+              {
+                "link" : "/admin/dashboard/clusters",
+                "name" : "SpecChapters",
+                "uniqueId" : "dashboard_specChapters",
+                "subMenuMappingId":"dashboard_chapters",
+                "subMenusId":"dashboard",
+                "isLink" : true,
+                "isMenu" : true,
+                "image" : ""
+
+              },
+              {
+                "link" : "/admin/dashboard/clusters",
+                "name" : "SpecSubChapters",
+                "uniqueId" : "dashboard_specSubChapters",
+                "subMenuMappingId":"dashboard_chapters",
+                "subMenusId":"dashboard",
+                "isLink" : true,
+                "isMenu" : true,
+                "image" : ""
+
+              }]
+          },
+          {
+            "link" : "/admin/dashboard/communities",
+            "name" : "Communities",
+            "uniqueId" : "dashboard_communities",
+            "subMenuMappingId":"dashboard_communities",
+            "subMenusId":"dashboard",
+            "isLink" : true,
+            "image" : ""
+          },
+
+        ]
+      },
+      {
+        "image" : "/images/cluster_icon.png",
+        "link" : "/admin/clusters",
+        "name" : "cluster",
+        "uniqueId"   :"cluster",
+        "isLink" : true,
+        "isMenu" : true
+      },
+
+      {
+        "link" : "/admin/clusters/clusterview/",
+        "name" : "cluster",
+        "uniqueId"   :"cluster",
+        "isLink" : true,
+        "isMenu" : true,
+        "isdynamic":true,
+        "subMenu":[
+          {
+            "link" : "/admin/cluster/clusterDetails",
+            "name" : "clusterDetails",
+            "uniqueId" : "cluster",
+            "isLink" : true,
+            "isMenu" : true,
+            "image" : "",
+          }
+        ]
+      },
+
+      {
+        "image" : "/images/chapter_icon.png",
+        "link" : "/admin/chapter",
+        "uniqueId" : "chapter",
+        "name" : "chapter",
+        "isLink" : "true",
+        "isMenu" : true,
+        "subMenu" : [
+          {
+            "link" : "/admin/chapter/chapters",
+            "name" : "Chapters",
+            "uniqueId" : "chapter_chapters",
+            "isLink" : true,
+            "isMenu" : true,
+            "image" : "",
+          },
+        ]
+      },
+      {
+        "image" : "/images/community_icon.png",
+        "link" : "/admin/community",
+        "uniqueId" : "community",
+        "name" : "community",
+        "isLink" : true,
+        "isMenu" : true,
+        "subMenu" : [
+          {
+            "link" : "/admichapters/comminty/communities",
+            "name" : "communities",
+            "uniqueId" : "community_communities",
+            "isLink" : true,
+            "isMenu" : true,
+            "image" : "",
+          },
+        ]
+      },
+      {
+        "image" : "/images/documents_icon.png",
+        "link" : "/admin/documents",
+        "name" : "documents",
+        "isLink" : true,
+        "isMenu" : true
+      },
+      {
+        "image" : "/images/services_icon.png",
+        "link" : "/admin/services",
+        "name" : "services",
+        "isLink" : true,
+        "isMenu" : true
+      },
+      {
+        "image" : "/images/settings_icon.png",
+        "link" : "/admin/settings/departmentsList",
+        "name" : "settings",
+        "uniqueId" : "settings",
+        "isLink" : true,
+        "isMenu" : true,
+        "subMenu" : [
+          {
+            "link" : "/admin/settings/departmentsList",
+            "name" : "Departments",
+            "uniqueId" : "settings_Departments",
+            "isLink" : true,
+            "isMenu" : true,
+            "image" : "",
+            "subMenu": [
+
+              {
+                "link": "/admin/settings/addDepartment",
+                "name": "Add Departments",
+                "uniqueId": "settings_AddDepartment",
+                "subMenuMappingId":"settings_Departments",
+                "subMenusId":"settings",
+                "isLink": true,
+                "isMenu": false,
+                "image": ""
+              },
+              {
+                "link": "/admin/settings/departmentsList",
+                "name": "List Department",
+                "uniqueId": "settings_DepartmentList",
+                "subMenuMappingId":"settings_Departments",
+                "subMenusId":"settings",
+                "isLink": true,
+                "isMenu": false,
+                "image": ""
+              },
+              {
+                "link": "/admin/settings/editDepartment",
+                "name": "Edit Department",
+                "uniqueId": "settings_EditDepartment",
+                "subMenuMappingId":"settings_Departments",
+                "subMenusId":"settings",
+                "isLink": true,
+                "isMenu": true,
+                "image": ""
+              }
+            ]
+          },
+
+
+        ]
+      }
+    ]
+  }});
+}

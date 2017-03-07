fba0ff49250a964ab07b1b71781e1b237383ce69
sireesha <sireesha.dhavala@raksanconsulting.com>
2017-03-07 17:08:14 +0530

    SocialLinkType Master Settings for CLuster Admin are done

--- client/admin/settings/socialLinks/actions/addSocialLinkTypeAction.js
+++ client/admin/settings/socialLinks/actions/addSocialLinkTypeAction.js
@@ -3,12 +3,12 @@ import {client} from '../../../core/apolloConnection';
 
 export async function addSocialLinkTypeActionHandler(Details)
 {
-  const result = await client.mutate({
+/*  const result = await client.mutate({
     mutation: gql`
         mutation ($socialLinksType:socialLinksTypeObject){
             createSocialLinksType(
                 socialLinksType: $socialLinksType
-            ) 
+            )
          }
         `,
     variables: {
@@ -17,5 +17,29 @@ export async function addSocialLinkTypeActionHandler(Details)
   })
   console.log(result)
   const id = result.data.createSocialLinksType;
+  return id*/
+
+  let socialName = Details.socialName;
+  let socialDisplayName = Details.socialDisplayName;
+  let aboutSocial = Details.aboutSocial;
+  let socialUploadIcon = Details.socialUploadIcon;
+  let isActive = Details.isActive;
+  let socialLinksInfo={socialName,socialDisplayName,aboutSocial,socialUploadIcon};
+  const result = await client.mutate({
+    mutation: gql`
+    mutation  ($masterData:MasterSettingsRequest){
+        createMasterSetting(
+          moduleName:"MASTER_SETTINGS",
+          actionName:"CREATE",
+          type:SOCIALLINKS,
+          masterData:$masterData
+        ) 
+      }
+    `,
+    variables: {
+      masterData:{"socialLinksInfo":socialLinksInfo,"isActive":isActive},
+    }
+  })
+  const id = result.data.createMasterSetting;
   return id
 }

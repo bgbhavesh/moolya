c6468edaaa8b1dfd72a4f923568b1e36fc122625
murali526 <muralidharreddy.kunduru@raksan.in>
2017-03-10 02:54:25 +0530

    password reset functionality

--- client/login/actions/loginActions.js
+++ client/login/actions/loginActions.js
@@ -10,12 +10,15 @@ export let loginActionHandler = {
                 console.log(result.reason)
                 callback(result.reason)
             }
-            else if(result && result.profile && result.profile.isInternaluser == true&&result.profile.InternalUprofile.moolyaProfile.isActive==true&&result.profile.InternalUprofile.moolyaProfile.assignedDepartment!=undefined&&result.profile.InternalUprofile.moolyaProfile.assignedDepartment[0].department!=null&&result.profile.InternalUprofile.moolyaProfile.userProfiles[0]!=undefined&&result.profile.InternalUprofile.moolyaProfile.userProfiles[0].userRoles!=undefined&&result.profile.InternalUprofile.moolyaProfile.userProfiles[0].userRoles[0].roleId!=null) {
+            else if(result && result.profile && result.profile.isInternaluser == "yes"){
+              FlowRouter.redirect("/admin");
+            }
+           /* else if(result && result.profile && result.profile.isInternaluser == true&&result.profile.InternalUprofile.moolyaProfile.isActive==true&&result.profile.InternalUprofile.moolyaProfile.assignedDepartment!=undefined&&result.profile.InternalUprofile.moolyaProfile.assignedDepartment[0].department!=null&&result.profile.InternalUprofile.moolyaProfile.userProfiles[0]!=undefined&&result.profile.InternalUprofile.moolyaProfile.userProfiles[0].userRoles!=undefined&&result.profile.InternalUprofile.moolyaProfile.userProfiles[0].userRoles[0].roleId!=null) {
               FlowRouter.redirect("/admin");
             }
             else{
               callback('login failed')
-            }
+            }*/
         });
     },
     onLogout(callback){

44c75ed5fbdd940323a54f2c5b8ddecd4a23cdfe
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-03-11 20:38:22 +0530

    images added for chapters

--- client/admin/chapter/components/MlChapterList.jsx
+++ client/admin/chapter/components/MlChapterList.jsx
@@ -6,14 +6,22 @@ export default class MlChapterList extends Component {
 
   render(){
     const data=this.props.data||[];
-    const list=  data.map((prop) =>
+    const list=  data.map((prop) => {
+      let image=prop.chapterImage&&prop.chapterImage.trim()!==""?<img src={`${prop.chapterImage}`}/>:<span className="ml ml-chapter"></span>;
+      return (
       <div className="col-md-2" key={prop._id}>
         <div className="list_block">
-          <div className={`cluster_status ${prop.statusField|| ""}_cl `}></div>
-          <a href={chapterRoutes.subChapterListRoute(prop.clusterId,prop._id)}> <div className={"hex_outer"}><img src={prop.subChapterImageLink}/></div></a>
+          <div className={`cluster_status ${prop.statusField || ""}_cl `}></div>
+          <a href={chapterRoutes.subChapterListRoute(prop.clusterId, prop._id)}>
+            <div className={"hex_outer"}>
+              {/*<img src={prop.subChapterImageLink}/>*/}
+              {image}
+            </div>
+          </a>
           <h3>{prop.chapterName} </h3>
         </div>
-      </div>
+      </div>)
+    }
   );
 
     return (
@@ -21,8 +29,6 @@ export default class MlChapterList extends Component {
           {list}
       </div>
     );
-
   }
-
 }
 
--- client/admin/cluster/components/MlClusterChapterList.jsx
+++ client/admin/cluster/components/MlClusterChapterList.jsx
@@ -13,14 +13,20 @@ export default class MlClusterChapterList extends Component
 
   render(){
     const data=this.props.data||[];
-    const list=  data.map((prop) =>
-      <div className="col-md-2" key={prop.chapterName}>
-        <div className="list_block">
-          <div className={`cluster_status ${prop.isActive|| ""}_cl `}></div>
-          <a href={clusterRoutes.subChapterListRoute(prop.clusterId,prop._id)}> <div className={"hex_outer"}><img src={prop.countryFlag}/></div></a>
-          <h3>{prop.chapterName}</h3>
+    const list=  data.map((prop) =>{
+      let image=prop.countryFlag&&prop.countryFlag.trim()!==""?<img src={`${prop.countryFlag}`}/>:<span className="ml ml-chapter"></span>;
+      return (
+        <div className="col-md-2" key={prop.chapterName}>
+          <div className="list_block">
+            <div className={`cluster_status ${prop.isActive|| ""}_cl `}></div>
+            <a href={clusterRoutes.subChapterListRoute(prop.clusterId,prop._id)}> <div className={"hex_outer"}>
+              {/*<img src={prop.countryFlag}/>*/}
+              {image}
+            </div></a>
+            <h3>{prop.chapterName}</h3>
+          </div>
         </div>
-      </div>
+    )}
     );
 
     return (<div className="row">{list}</div>);

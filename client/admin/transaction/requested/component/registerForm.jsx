27e0aa9c69e78795886147318fb6f25642dc8ea2
murali526 <muralidharreddy.kunduru@raksan.in>
2017-03-21 20:51:55 +0530

    Merge conflict

--- client/admin/transaction/requested/component/registerForm.jsx
+++ client/admin/transaction/requested/component/registerForm.jsx
@@ -60,7 +60,7 @@ export default class RegisterForm extends React.Component{
     const steps =
     [
       {name: 'Basic info','icon':<span className="ml ml-basic-Information"></span>, component: <Step1 getRegistrationDetails={this.getRegistrationDetails.bind(this)} registrationInfo={this.state.registrationDetails.registrationInfo} registrationId={registrationId}/>},
-      {name: 'Additional info','icon':<span className="ml ml-additional-Information"></span>,  component: <Step2 registrationId={registrationId} community={'institution'}/>},
+      {name: 'Additional info','icon':<span className="ml ml-additional-Information"></span>,  component: <Step2 registrationId={registrationId} registrationDetails={this.state.registrationDetails.registrationDetails} community={this.state.registrationDetails&&this.state.registrationDetails.registrationInfo.communityName}/>},
       {name: 'Contact details','icon':<span className="ml ml-moolya-symbol"></span>, component: <Step3 getRegistrationContactDetails={this.getRegistrationContactDetails.bind(this)} registrationInfo={this.state.registrationDetails}/>},
       {name: 'Social links','icon':<span className="ml ml-social-Links"></span>,  component: <Step4 getRegistrationSocialLinks={this.getRegistrationSocialLinks.bind(this)} registrationInfo={this.state.registrationDetails}/>},
       {name: 'KYC\'s Documents','icon':<span className="ml ml-kyc-document"></span>,  component: <Step5 getRegistrationKYCDetails={this.getRegistrationKYCDetails.bind(this)} registrationInfo={this.state.registrationDetails}/>},

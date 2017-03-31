70d6a4c656bd7fbf634102c219bb6def25ac652c
sireesha <sireesha.dhavala@raksanconsulting.com>
2017-03-31 18:41:04 +0530

    Delete functionality changes in step3  and step4

--- client/admin/transaction/requested/component/contactDetails.jsx
+++ client/admin/transaction/requested/component/contactDetails.jsx
@@ -21,7 +21,8 @@ export default class ContactDetails extends React.Component{
       selectedContactTab: null,
       selectedNumberTypeLabel: null,
       contactNumberObject:{numberType : "",numberTypeName: "",countryCode: "",contactNumber: ""},
-      contactNumberArray : this.props.registrationInfo.contactInfo || []
+      contactNumberArray : this.props.registrationInfo.contactInfo || [],
+      activeTab : "active"
 
     }
     this.findRegistration.bind(this);
@@ -125,6 +126,7 @@ export default class ContactDetails extends React.Component{
   }
   tabSelected(index,value){
     this.setState({selectedContactTab : true});
+    this.setState({activeTab : ""});
     this.findRegistration();
   }
 
@@ -144,6 +146,7 @@ export default class ContactDetails extends React.Component{
     const response = await updateRegistrationInfoDetails(listArray,detailsType,registerid);
     this.setState({loading:false,contactNumberArray:response.contactInfo});
     this.findRegistration();
+    this.setState({activeTab : "active"});
   }
 
 
@@ -164,70 +167,10 @@ export default class ContactDetails extends React.Component{
     return (
 
       <div className="panel-body">
-        {/*div className="ml_tabs">
-          <ul className="nav nav-pills">
-            <li className="active"> <a href="#1a" data-toggle="tab">Add New&nbsp;<b><FontAwesome name='minus-square'/></b></a> </li>
 
-            {that.state.contactNumberArray.map(function(options,key){
-            return(
-            <li  onClick={that.tabSelected.bind(that,key)}>
-              <a data-toggle="pill" href={'numberType'+key} className="add-contact">
-                <FontAwesome name='plus-square' />{options.numberTypeName}</a>
-            </li>)
-            })}
-
-          </ul>
-          <div className="tab-content clearfix">
-
-            <div className="tab-pane active" id="1a">
-              <div className="form-group">
-
-                <Moolyaselect multiSelect={false} ref="numberType" placeholder="Select NumberType"
-                              className="form-control float-label" selectedValue = {selectedNumId}
-                              valueKey={'value'} labelKey={'label'} queryType={"graphql"} query={numberTypeQuery}
-                              queryOptions={numberTypeOption} onSelect={this.optionsBySelectNumberType.bind(this)}
-                              isDynamic={true}/>
-              </div>
-              <div className="form-group">
-                <input type="text" placeholder="Enter Country Code" ref={'contryCode'} className="form-control float-label" id=""/>
-              </div>
-              <div className="form-group">
-                <input type="text" ref="{phoneNumber}" placeholder="Enter Number" id="phoneNumber" className="form-control float-label"/>
-              </div>
-              <div className="ml_icon_btn">
-                <a href="#"  onClick={this.onSavingContact.bind(this)}  className="save_btn" ><span
-                  className="ml ml-save"></span></a>
-                <a href="#" id="cancel_contact" className="cancel_btn"><span className="ml ml-delete"></span></a>
-              </div>
-            </div>
-            {that.state.contactNumberArray.map(function(options,key) {
-              return (<div className="tab-pane active" id={"numberType"+key}>
-                <div className="form-group">
-
-                  <Moolyaselect multiSelect={false} ref={"numberType"+key} placeholder="Select NumberType"
-                                className="form-control float-label" selectedValue={selectedNumId}
-                                valueKey={'value'} labelKey={'label'} queryType={"graphql"} query={numberTypeQuery}
-                                queryOptions={numberTypeOption} onSelect={that.updateContactOptions.bind(that,key)}
-                                isDynamic={true}/>
-                </div>
-                <div className="form-group">
-                  <input type="text" placeholder="Enter Country Code" ref={'contryCode'+key}
-                         className="form-control float-label" id=""/>
-                </div>
-                <div className="form-group">
-                  <input type="text" ref={'phoneNumber'+key} placeholder="Enter Number" id="phoneNumber"
-                         className="form-control float-label"/>
-                </div>
-                <div className="ml_icon_btn">
-                  <a href="#" onClick={that.onSavingContact.bind(that,key)} className="save_btn"><span
-                    className="ml ml-save"></span></a>
-                  <a href="#" id="cancel_contact" className="cancel_btn"><span className="ml ml-delete"></span></a>
-                </div>
-              </div>)
-            })}*/}
         <div className="ml_tabs">
           <ul  className="nav nav-pills">
-            <li className="active">
+            <li className={this.state.activeTab}>
               <a  href="#contactA" data-toggle="tab">Add New&nbsp;<b><FontAwesome name='plus-square'/></b></a>
             </li>
 
@@ -244,7 +187,7 @@ export default class ContactDetails extends React.Component{
           </ul>
 
           <div className="tab-content clearfix">
-            <div className="tab-pane active" id="contactA">
+            <div className={"tab-pane"+this.state.activeTab} id="contactA">
               <div className="form-group">
                 <Moolyaselect multiSelect={false} ref="numberType" placeholder="Select NumberType"
                               className="form-control float-label" selectedValue = {this.state.selectedNumberTypeValue}
@@ -265,7 +208,7 @@ export default class ContactDetails extends React.Component{
               </div>
             </div>
             {that.state.contactNumberArray && (that.state.contactNumberArray.map(function(options,key) {
-              return(<div className="tab-pane" id={'numberType'+key} >
+              return(<div className="tab-pane" id={'numberType'+key} key={key} >
                 <div className="form-group">
                   <Moolyaselect multiSelect={false} ref={"numberType"+key} placeholder="Select NumberType"
                                 className="form-control float-label" selectedValue={options.numberType}

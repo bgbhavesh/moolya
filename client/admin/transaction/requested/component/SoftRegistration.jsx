9745d7a4d9980be0174b47625ba24611a5fef7c8
mohasinraksan <mohammed.mohasin@raksan.in>
2017-04-04 12:47:43 +0530

    Registration Components added based on Templates Design.
    1)Each template will have its own set of steps which drives the soft/hard registration process.

---
+++ client/admin/transaction/requested/component/SoftRegistration.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Step1 from './step1';
+export default class SoftRegistration extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.getRegistrationSteps.bind(this);
+  }
+
+  getRegistrationSteps(props){
+    let softRegSteps=[{name: 'Basic info','icon':<span className="ml ml-basic-Information"></span>, component: <Step1 {...props}/>}];
+
+      return softRegSteps;
+  }
+  componentWillMount(){
+    this.props.setSoftRegistrationSteps(this.getRegistrationSteps(this.props));
+  }
+
+  compareQueryOptions(a, b) {
+    return JSON.stringify(a) === JSON.stringify(b);
+  };
+
+  componentWillUpdate(nextProps){
+    if(!this.compareQueryOptions(this.props,nextProps)){
+      this.props.setSoftRegistrationSteps(this.getRegistrationSteps(nextProps));
+    }
+  }
+
+  render(){
+    return null;
+  }
+};

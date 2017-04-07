f212f4e0b3e6e3860b54e9dfcf7b52836c70a01b
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-04-07 15:38:58 +0530

    Portfolio tabs integrated

--- client/admin/transaction/portfolio/component/Ideator/MlIdeatorPortfolio.jsx
+++ client/admin/transaction/portfolio/component/Ideator/MlIdeatorPortfolio.jsx
@@ -5,6 +5,12 @@ import { render } from 'react-dom';
 
 import MlTabComponent from '../../../../../commons/components/tabcomponent/MlTabComponent'
 import MlIdeatorDetails from '../Ideator/MlIdeatorDetails'
+import MlIdeatorProblemsAndSolutions from '../Ideator/MlIdeatorProblemsAndSolutions'
+import MlIdeatorAudience from '../Ideator/MlIdeatorAudience'
+import MlIdeatorLibrary from '../Ideator/MlIdeatorLibrary'
+import MlIdeatorStrategyAndPlanning from '../Ideator/MlIdeatorStrategyAndPlanning'
+import MlIdeatorIntellectualPlanningAndTrademark from '../Ideator/MlIdeatorIntellectualPlanningAndTrademark'
+import MlIdeatorLookingFor from '../Ideator/MlIdeatorLookingFor'
 
 export default class MlIdeatorPortfolioTemplate extends React.Component{
     constructor(props){
@@ -14,9 +20,14 @@ export default class MlIdeatorPortfolioTemplate extends React.Component{
 
     getTabComponents(){
         let tabs = [
-          {tabClassName: 'tab', panelClassName: 'panel', title:"first tab" , component:<MlIdeatorDetails key="1"/>},
-          {tabClassName: 'tab', panelClassName: 'panel', title:"second tab", component:<div  key="2"> second </div>},
-          {tabClassName: 'tab', panelClassName: 'panel', title:"third tab", component:<div key="3"> third </div>}
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Ideator" , component:<MlIdeatorDetails key="1"/>},
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Ideas", component:<div  key="2"> second </div>},
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Problems and Solutions", component:<MlIdeatorProblemsAndSolutions key="3"/>},
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Audience" , component:<MlIdeatorAudience key="4"/>},
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Library", component:<MlIdeatorLibrary  key="5"/> },
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Strategy and Planning", component:<MlIdeatorStrategyAndPlanning key="6"/>},
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Intellectual Planning and Trademark" , component:<MlIdeatorIntellectualPlanningAndTrademark key="7"/>},
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Looking For", component:<MlIdeatorLookingFor  key="8"/>}
         ]
         return tabs;
     }

74a6fa2413a68b1752e4991ec6c1dc8e5f0911c0
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-04-08 14:30:45 +0530

    MOOLYA-875 fix

--- client/admin/settings/userTypes/actions/createUsertypeAction.js
+++ client/admin/settings/userTypes/actions/createUsertypeAction.js
@@ -2,10 +2,6 @@ import gql from 'graphql-tag'
 import {client} from '../../../core/apolloConnection';
 
 export async function createUserTypeActionHandler(userType) {
-  // let _id=UserTypeDetails.id;
-  // let displayName = UserTypeDetails.displayName;
-  // let userTypeDesc = UserTypeDetails.userTypeDesc;
-  // let isActive = UserTypeDetails.isActive
   const result = await client.mutate({
     mutation: gql`
     mutation ($userType:userTypeObject, $moduleName:String, $actionName:String){

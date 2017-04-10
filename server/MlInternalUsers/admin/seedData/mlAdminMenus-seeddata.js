6e0894a4f4c8fad727666d13523c99cf853535c3
pavani <pavani.pasula@raksan.in>
2017-04-10 15:14:50 +0530

    process mapping kyc based on cluster chater and subchapter created

--- server/MlInternalUsers/admin/seedData/mlAdminMenus-seeddata.js
+++ server/MlInternalUsers/admin/seedData/mlAdminMenus-seeddata.js
@@ -486,7 +486,7 @@ if(Meteor.isServer){
           },
           {
             "link" : "/admin/transactions/approvedList",
-            "name" : "Approved",
+            "name" : "Approvals",
             "uniqueId" : "transaction_Approved",
             "isLink" : true,
             "isMenu" : true,

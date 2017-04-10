59fd163d2d1326c4bbf267f25a10515d9761ec07
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-04-10 15:48:49 +0530

    ideator problem solution

--- client/admin/transaction/portfolio/component/Ideator/MlIdeatorPortfolio.jsx
+++ client/admin/transaction/portfolio/component/Ideator/MlIdeatorPortfolio.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
+import React from "react";
+import {render} from "react-dom";
+import MlTabComponent from "../../../../../commons/components/tabcomponent/MlTabComponent";
+import MlIdeatorDetails from "../Ideator/MlIdeatorDetails";
+import MlIdeatorProblemsAndSolutions from "../Ideator/MlIdeatorProblemsAndSolutions";
+import MlIdeatorAudience from "../Ideator/MlIdeatorAudience";
+import MlIdeatorLibrary from "../Ideator/MlIdeatorLibrary";
+import MlIdeatorStrategyAndPlanning from "../Ideator/MlIdeatorStrategyAndPlanning";
+import MlIdeatorIntellectualPlanningAndTrademark from "../Ideator/MlIdeatorIntellectualPlanningAndTrademark";
+import MlIdeatorLookingFor from "../Ideator/MlIdeatorLookingFor";
 // import '../../../../../stylesheets/tab.css'
-
-import MlTabComponent from '../../../../../commons/components/tabcomponent/MlTabComponent'
-import MlIdeatorDetails from '../Ideator/MlIdeatorDetails'
-import MlIdeatorProblemsAndSolutions from '../Ideator/MlIdeatorProblemsAndSolutions'
-import MlIdeatorAudience from '../Ideator/MlIdeatorAudience'
-import MlIdeatorLibrary from '../Ideator/MlIdeatorLibrary'
-import MlIdeatorStrategyAndPlanning from '../Ideator/MlIdeatorStrategyAndPlanning'
-import MlIdeatorIntellectualPlanningAndTrademark from '../Ideator/MlIdeatorIntellectualPlanningAndTrademark'
-import MlIdeatorLookingFor from '../Ideator/MlIdeatorLookingFor'
 
 export default class MlIdeatorPortfolioTemplate extends React.Component{
     constructor(props){
         super(props)
-        this.state =  {tabs: [], ideatorPortfolio:{}};
+        this.state =  {tabs: [], ideatorPortfolio:{}, problemSolution:{}};
         this.getIdeatorDetails.bind(this);
+        this.getProblemSolution.bind(this)
     }
 
     getTabComponents(){
         let tabs = [
           {tabClassName: 'tab', panelClassName: 'panel', title:"Ideator" , component:<MlIdeatorDetails key="1" getIdeatorDetails={this.getIdeatorDetails.bind(this)}/>},
           {tabClassName: 'tab', panelClassName: 'panel', title:"Ideas", component:<div  key="2"> second </div>},
-          {tabClassName: 'tab', panelClassName: 'panel', title:"Problems and Solutions", component:<MlIdeatorProblemsAndSolutions key="3"/>},
+          {tabClassName: 'tab', panelClassName: 'panel', title:"Problems and Solutions", component:<MlIdeatorProblemsAndSolutions key="3" getProblemSolution={this.getProblemSolution.bind(this)}/>},
           {tabClassName: 'tab', panelClassName: 'panel', title:"Audience" , component:<MlIdeatorAudience key="4"/>},
           {tabClassName: 'tab', panelClassName: 'panel', title:"Library", component:<MlIdeatorLibrary  key="5"/> },
           {tabClassName: 'tab', panelClassName: 'panel', title:"Strategy and Planning", component:<MlIdeatorStrategyAndPlanning key="6"/>},
@@ -38,6 +37,10 @@ export default class MlIdeatorPortfolioTemplate extends React.Component{
         this.props.getPortfolioDetails(this.state.ideatorPortfolio);
     }
 
+  getProblemSolution(details) {
+    this.state.ideatorPortfolio['problemSolution'] = details;
+    this.props.getPortfolioDetails(this.state.ideatorPortfolio);
+  }
 
     componentWillMount()
     {

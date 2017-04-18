4dc9fa7ade77d2bdddbe7649dea2bb6e4e495147
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-04-18 16:06:08 +0530

    MOOLYA-404 MOOLYA-978 and MOOLYA-562 are fixed

--- client/admin/cluster/components/MlAssignBackendUserList.jsx
+++ client/admin/cluster/components/MlAssignBackendUserList.jsx
@@ -42,10 +42,18 @@ export default class MlAssignBackendUserList extends React.Component{
         return(
           <div>
             {backendUsers.map(function (user) {
+              let status, icon;
+              if(user.profile.isActive){
+                status = "active";
+                icon = "active-User";
+              } else{
+                status = "inactive";
+                icon = "inactive-user"
+              }
                 return(
                     <div className="col-md-4 col-sm-4" key={user.username} onClick={that.onBackEndUserClick.bind(that,user)}>
                         <div className="list_block provider_block">
-                          <div className="cluster_status active_cl"><span className="ml ml-active-User"></span></div>
+                          <div className={`cluster_status ${status}_cl`}><span className={`ml ml-${icon}`}></span></div>
                             <div className="provider_mask"> <img src="/images/funder_bg.png" /> <img className="user_pic" src="/images/def_profile.png" /> </div>
                             {/*<h3>{user.username}<br />USA</h3>*/}
                             <h3>{user.username}</h3>

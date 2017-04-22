aef16cf2d9b9d101257425b3538e150331e9f0ba
pavani <pavani.pasula@raksan.in>
2017-04-22 13:17:48 +0530

    Assign user for transaction in registration is done

--- server/MlInternalUsers/admin/transactions/transactionResolver.js
+++ server/MlInternalUsers/admin/transactions/transactionResolver.js
@@ -24,34 +24,42 @@ MlResolver.MlMutationResolver['createTransaction'] = (obj, args, context, info) 
 }
 
 MlResolver.MlMutationResolver['createRegistrationTransaction'] = (obj, args, context, info) => {
-
+    let transaction={};
   let transact = MlTransactionTypes.findOne({"_id":"registration"})|| {};
-  args.transaction.transactionTypeName=transact.transactionType;
-  args.transaction.transactionTypeId=transact.transactionId;
-  let transactionDetails=args.transaction
+  transaction.transactionTypeName=transact.transactionName;
+  transaction.transactionTypeId=transact._id;
   //find hierarchy
-  let hierarchy = mlDBController.find('MlHierarchyAssignments', {
-    parentDepartment: args.department,
-    parentSubDepartment: args.subDepartment,
-    clusterId:args.cluster
-  }, context, {teamStructureAssignment: {$elemMatch: {roleId: args.params.role}}}).fetch();
+  let hierarchy = mlDBController.findOne('MlHierarchyAssignments', {
+    parentDepartment: args.params.department,
+    parentSubDepartment: args.params.subDepartment,
+    clusterId:args.params.cluster
+  }, context, {teamStructureAssignment: {$elemMatch: {roleId: args.params.role}}})
   //update hierarchy from hierarchy result
+  transaction.hierarchy=hierarchy._id
 
   //get user details iterate through profiles match with role and get department and update allocation details.
   let user = mlDBController.findOne('users', {_id: args.params.user}, context)
+  let userprofile=user.profile.InternalUprofile.moolyaProfile.userProfiles
+  userProfileRoles = _.find(userprofile, function (item) {
+    return item.clusterId == args.params.cluster
+  });
+  let roles=userProfileRoles.userRoles
+    roleDetails=_.find(roles, function (item) {
+      return item.roleId == args.params.role
+    });
+  let date=new Date();
+  let allocation={
+    assignee            : user.username,
+    assigneeId          : args.params.user,
+    assignedDate        : date.date,
+    department          : roleDetails.departmentName,
+    departmentId        : roleDetails.departmentId,
+    subDepartment       : roleDetails.subDepartmentName,
+    subDepartmentId     : roleDetails.subDepartmentId,
+  }
+  transaction.allocation = allocation;
 
-  /*let allocation{
-    assignee            : String
-    assigneeId          : String
-    assignedDate        : Date
-    department          : String
-    departmentId        : String
-    subDepartment       : String
-    subDepartmentId     : String
-  }*/
-  args.transaction.allocation = allocation;
-
-  let id = mlDBController.insert('MlTransactions', args.transaction, context)
+  let id = mlDBController.insert('MlTransactions',transaction, context)
   if(id){
     let code = 200;
     let result = {transactionId : id}

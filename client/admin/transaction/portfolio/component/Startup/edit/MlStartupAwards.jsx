b15a7c8d46c749794f99a622d1300c4ed0ec5ec2
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-04-23 14:24:52 +0530

    Startup - Data, Awards, MCL and Looking screen partially integrated

---
+++ client/admin/transaction/portfolio/component/Startup/edit/MlStartupAwards.jsx
@@ -0,0 +1,257 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import ScrollArea from 'react-scrollbar'
+import { Button, Popover, PopoverTitle, PopoverContent } from 'reactstrap';
+import {dataVisibilityHandler, OnLockSwitch} from '../../../../../utils/formElemUtil';
+var FontAwesome = require('react-fontawesome');
+import Moolyaselect from  '../../../../../../commons/components/select/MoolyaSelect';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import _ from 'lodash';
+import {multipartASyncFormHandler} from '../../../../../../commons/MlMultipartFormAction'
+import {findStartupInvestorDetailsActionHandler} from '../../../actions/findPortfolioStartupDetails'
+var Select = require('react-select');
+var options = [
+  { value: '1', label: '1' },
+  { value: '2', label: '2' }
+];
+function logChange(val) {
+  console.log("Selected: " + val);
+}
+
+
+export default class MlStartupAwards extends React.Component{
+  constructor(props, context){
+    super(props);
+    this.state={
+      loading: true,
+      data:{},
+      startupInvestor: [],
+      popoverOpen:false,
+      index:"",
+      startupInvestorList:[],
+      indexArray:[],
+      selectedVal:null,
+      selectedObject:"default"
+    }
+    this.handleBlur.bind(this);
+    this.fetchPortfolioDetails.bind(this);
+    return this;
+  }
+
+  componentDidUpdate(){
+    OnLockSwitch();
+    dataVisibilityHandler();
+  }
+
+  componentDidMount(){
+    OnLockSwitch();
+    dataVisibilityHandler();
+  }
+  componentWillMount(){
+    this.fetchPortfolioDetails();
+  }
+  async fetchPortfolioDetails() {
+    let that = this;
+    let portfoliodetailsId=that.props.portfolioDetailsId;
+    const response = await findStartupInvestorDetailsActionHandler(portfoliodetailsId);
+    if (response) {
+      this.setState({loading: false, startupInvestor: response, startupInvestorList: response});
+    }
+  }
+  addInvestor(){
+    this.setState({selectedObject : "default"})
+    this.setState({popoverOpen : !(this.state.popoverOpen)})
+    this.setState({data : {}})
+    if(this.state.startupInvestor){
+      this.setState({index:this.state.startupInvestor.length})
+    }else{
+      this.setState({index:0})
+    }
+  }
+  onSelect(index, e){
+    let details = this.state.startupInvestor[index]
+    details = _.omit(details, "__typename");
+    this.setState({index:index});
+    this.setState({data:details})
+    this.setState({selectedObject : index})
+    this.setState({popoverOpen : !(this.state.popoverOpen)});
+    this.setState({"selectedVal" : details.fundingType});
+    let indexes = this.state.indexArray;
+    let indexArray = _.cloneDeep(indexes)
+    indexArray.push(index);
+    indexArray = _.uniq(indexArray);
+    this.setState({indexArray: indexArray})
+  }
+
+  onLockChange(field, e){
+    let details = this.state.data||{};
+    let key = e.target.id;
+    details=_.omit(details,[key]);
+    let className = e.target.className;
+    if(className.indexOf("fa-lock") != -1){
+      details=_.extend(details,{[key]:true});
+    }else{
+      details=_.extend(details,{[key]:false});
+    }
+    this.setState({data:details}, function () {
+      this.sendDataToParent()
+    })
+  }
+
+  onStatusChangeNotify(e)
+  {
+    let updatedData = this.state.data||{};
+    let key = e.target.id;
+    updatedData=_.omit(updatedData,[key]);
+    if (e.currentTarget.checked) {
+      updatedData=_.extend(updatedData,{[key]:true});
+    } else {
+      updatedData=_.extend(updatedData,{[key]:false});
+    }
+    this.setState({data:updatedData}, function () {
+      this.sendDataToParent()
+    })
+  }
+
+  onOptionSelected(selectedIndex,handler,selectedObj){
+
+    let details =this.state.data;
+    details=_.omit(details,["fundingType"]);
+    details=_.extend(details,{["fundingType"]:selectedIndex});
+    this.setState({data:details}, function () {
+      this.setState({"selectedVal" : selectedIndex})
+      this.sendDataToParent()
+    })
+
+  }
+  handleBlur(e){
+    let details =this.state.data;
+    let name  = e.target.name;
+    details=_.omit(details,[name]);
+    details=_.extend(details,{[name]:e.target.value});
+    this.setState({data:details}, function () {
+      this.sendDataToParent()
+    })
+  }
+  sendDataToParent(){
+    let data = this.state.data;
+    let startupInvestor1 = this.state.startupInvestor;
+    let startupInvestor = _.cloneDeep(startupInvestor1);
+    startupInvestor[this.state.index] = data;
+    let arr = [];
+    _.each(startupInvestor, function (item) {
+      for (var propName in item) {
+        if (item[propName] === null || item[propName] === undefined) {
+          delete item[propName];
+        }
+      }
+      newItem = _.omit(item, "__typename")
+      arr.push(newItem)
+    })
+    startupInvestor = arr;
+    // startupManagement=_.extend(startupManagement[this.state.arrIndex],data);
+    this.setState({startupInvestor:startupInvestor})
+    let indexArray = this.state.indexArray;
+    this.props.getAwardsDetails(startupInvestor,indexArray);
+  }
+
+  render(){
+    let query=gql`query{
+      data:fetchTechnologies {
+        label:displayName
+        value:_id
+      }
+    }`;
+    let that = this;
+    let investorsArray = that.state.startupInvestorList || [];
+    return (
+      <div className="admin_main_wrap">
+        <div className="admin_padding_wrap portfolio-main-wrap">
+          <h2>Awards</h2>
+          <div className="requested_input main_wrap_scroll">
+
+            <ScrollArea
+              speed={0.8}
+              className="main_wrap_scroll"
+              smoothScrolling={true}
+              default={true}
+            >
+              <div className="col-lg-12">
+                <div className="row">
+                  <div className="col-lg-2 col-md-3 col-sm-3">
+                    <a href="#" id="create_clientdefault" data-placement="top" data-class="large_popover" >
+                      <div className="list_block notrans" onClick={this.addInvestor.bind(this)}>
+                        <div className="hex_outer"><span className="ml ml-plus "></span></div>
+                        <h3 onClick={this.addInvestor.bind(this)}>Add New Awards</h3>
+                      </div>
+                    </a>
+                  </div>
+                  {investorsArray.map(function (details, idx) {
+                    return(<div className="col-lg-2 col-md-3 col-sm-3">
+                      <a href="#" id={"create_client"+idx}>
+                        <div className="list_block">
+                          <FontAwesome name='unlock'  id="makePrivate" defaultValue={details.makePrivate}/><input type="checkbox" className="lock_input" id="isAssetTypePrivate" checked={details.makePrivate}/>
+                          <div className="cluster_status inactive_cl"><FontAwesome name='times'/></div>
+                          <div className="hex_outer" onClick={that.onSelect.bind(that, idx)}><img src="/images/meteor-logo.png"/></div>
+                          <h3>{details.description}</h3>
+                        </div>
+                      </a>
+                    </div>)
+                  })}
+
+
+
+                </div>
+              </div>
+
+            </ScrollArea>
+
+            <Popover placement="right" isOpen={this.state.popoverOpen}  target={"create_client"+this.state.selectedObject} toggle={this.toggle}>
+              {/* <PopoverTitle>Add Asset</PopoverTitle>*/}
+              <PopoverContent>
+                <div  className="ml_create_client">
+                  <div className="medium-popover"><div className="row">
+                    <div className="col-md-12">
+                      <div className="form-group">
+                        <Moolyaselect multiSelect={false} className="form-control float-label" valueKey={'value'}
+                                      labelKey={'label'} queryType={"graphql"} query={query}
+                                      isDynamic={true}
+                                      onSelect={this.onOptionSelected.bind(this)}
+                                      selectedValue={this.state.selectedVal}/>
+                      </div>
+                      <div className="form-group">
+                        <Moolyaselect multiSelect={false} className="form-control float-label" valueKey={'value'}
+                                      labelKey={'label'} queryType={"graphql"} query={query}
+                                      isDynamic={true}
+                                      onSelect={this.onOptionSelected.bind(this)}
+                                      selectedValue={this.state.selectedVal}/>
+                      </div>
+                      <div className="form-group">
+                        <input type="text" name="description" placeholder="About" className="form-control float-label" id="" defaultValue={this.state.data.description}  onBlur={this.handleBlur.bind(this)}/>
+                        <FontAwesome name='unlock' className="input_icon req_textarea_icon un_lock" id="isDescriptionPrivate" defaultValue={this.state.data.isDescriptionPrivate}  onClick={this.onLockChange.bind(this, "isDescriptionPrivate")}/><input type="checkbox" className="lock_input" id="makePrivate" checked={this.state.data.isDescriptionPrivate}/>
+                      </div>
+                      <div className="form-group">
+                        <div className="input_types"><input id="makePrivate" type="checkbox" checked={this.state.data.makePrivate&&this.state.data.makePrivate}  name="checkbox" onChange={this.onStatusChangeNotify.bind(this)}/><label htmlFor="checkbox1"><span></span>Make Private</label></div>
+                      </div>
+                      <div className="ml_btn" style={{'textAlign': 'center'}}>
+                        <a href="" className="save_btn">Save</a>
+                      </div>
+                    </div>
+                  </div></div>
+                </div>
+              </PopoverContent>
+            </Popover>
+
+
+
+
+          </div>
+        </div>
+
+
+      </div>
+    )
+  }
+}
--- client/admin/transaction/portfolio/component/Startup/edit/MlStartupEditTemplate.jsx
+++ client/admin/transaction/portfolio/component/Startup/edit/MlStartupEditTemplate.jsx
@@ -6,6 +6,10 @@ import MlStartupManagement from './MlStartupManagement'
 import _ from 'lodash'
 import MlStartupAboutUs from "./aboutUs/MlStartupAboutUsLandingPage"
 import MlStartupInvestor from "./MlStartupInvestor"
+import MlStartupData from "./MlStartupData"
+import MlStartupAwards from './MlStartupAwards'
+import MlStartupMCL from './MlStartupMCL'
+import MlStartupLookingFor from './MlStartupLookingFor'
 
 
 export default class MlStartupEditTemplate extends React.Component{
@@ -14,6 +18,8 @@ export default class MlStartupEditTemplate extends React.Component{
     this.state =  {tabs: [],aboutUs: {}, startupPortfolio:{}};
     this.getChildContext.bind(this)
     this.getManagementDetails.bind(this);
+    this.getAwardsDetails.bind(this);
+    this.getLookingForDetails.bind(this);
   }
 
   getChildContext(){
@@ -40,16 +46,15 @@ export default class MlStartupEditTemplate extends React.Component{
 
       {tabClassName: 'tab', panelClassName: 'panel', title:"About" , component:<MlStartupAboutUs key="1" getAboutus={this.getAboutus.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
       {tabClassName: 'tab', panelClassName: 'panel', title:"Management" , component:<MlStartupManagement key="2" getManagementDetails={this.getManagementDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
-      {tabClassName: 'tab', panelClassName: 'panel', title:"Investor" , component:<MlStartupInvestor key="2" getInvestorDetails={this.getInvestor.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
-     /* {tabClassName: 'tab', panelClassName: 'panel', title:"Investor" , component:<MlIdeatorDetails key="3" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
-      {tabClassName: 'tab', panelClassName: 'panel', title:"Data" , component:<MlIdeatorDetails key="4" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
-      {tabClassName: 'tab', panelClassName: 'panel', title:"Charts" , component:<MlIdeatorDetails key="5" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
-      {tabClassName: 'tab', panelClassName: 'panel', title:"Awards" , component:<MlIdeatorDetails key="6" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
-      {tabClassName: 'tab', panelClassName: 'panel', title:"Library" , component:<MlIdeatorDetails key="7" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
-      {tabClassName: 'tab', panelClassName: 'panel', title:"M C & L" , component:<MlIdeatorDetails key="8" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
-      {tabClassName: 'tab', panelClassName: 'panel', title:"Looking For" , component:<MlIdeatorDetails key="9" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
+      {tabClassName: 'tab', panelClassName: 'panel', title:"Investor" , component:<MlStartupInvestor key="3" getInvestorDetails={this.getInvestor.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
+      {tabClassName: 'tab', panelClassName: 'panel', title:"Data" , component:<MlStartupData key="4" portfolioDetailsId={this.props.portfolioDetailsId}/>},
+      // {tabClassName: 'tab', panelClassName: 'panel', title:"Charts" , component:<MlIdeatorDetails key="5" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
+      {tabClassName: 'tab', panelClassName: 'panel', title:"Awards" , component:<MlStartupAwards key="6" getAwardsDetails={this.getAwardsDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
+      // {tabClassName: 'tab', panelClassName: 'panel', title:"Library" , component:<MlIdeatorDetails key="7" getIdeatorDetails={this.getIdeatorDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
+      {tabClassName: 'tab', panelClassName: 'panel', title:"M C & L" , component:<MlStartupMCL key="8" portfolioDetailsId={this.props.portfolioDetailsId}/>},
+      {tabClassName: 'tab', panelClassName: 'panel', title:"Looking For" , component:<MlStartupLookingFor key="9" getLookingForDetails={this.getLookingForDetails.bind(this)} portfolioDetailsId={this.props.portfolioDetailsId}/>},
 
-*/
+
     ]
     return tabs;
   }
@@ -78,6 +83,28 @@ export default class MlStartupEditTemplate extends React.Component{
       data['investor']=[];
     }
     data['investor'] = details;
+    this.setState({startupPortfolio : data})
+    this.props.getPortfolioDetails({startupPortfolio:this.state.startupPortfolio}, indexArray);
+  }
+
+  getAwardsDetails(details,indexArray){
+
+    let data = this.state.startupPortfolio;
+    if(data && !data.investor){
+      data['awardsRecognition']=[];
+    }
+    data['awardsRecognition'] = details;
+    this.setState({startupPortfolio : data})
+    this.props.getPortfolioDetails({startupPortfolio:this.state.startupPortfolio}, indexArray);
+  }
+
+  getLookingForDetails(details,indexArray){
+
+    let data = this.state.startupPortfolio;
+    if(data && !data.investor){
+      data['lookingFor']=[];
+    }
+    data['lookingFor'] = details;
     this.setState({startupPortfolio : data})
     this.props.getPortfolioDetails({startupPortfolio:this.state.startupPortfolio}, indexArray);
   }

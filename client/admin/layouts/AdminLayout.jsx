be1e997563c86d4a0145f1ac7ef47d2fdaf3f399
venkatsrinag <venkatasrinag.pindiproli@raksanconsulting.com>
2017-04-24 23:05:26 +0530

    funder schema added

--- client/admin/layouts/AdminLayout.jsx
+++ client/admin/layouts/AdminLayout.jsx
@@ -15,16 +15,15 @@ export default class  AdminLayout extends Component {
       //To Provide Context for Theme,Language and Menu/Role
      <div>
        <MetaTags>
-          <title>{'moolya'}</title>
+          <title> {'moolya'}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
        </MetaTags>
-
-      <ApolloProvider client={client}>
-        <div className="moolya_admin">
-          <MlAdminApp {...this.props}/>
-        </div>
-      </ApolloProvider>
-       </div>
+        <ApolloProvider client={client}>
+          <div className="moolya_admin">
+            <MlAdminApp {...this.props}/>
+          </div>
+        </ApolloProvider>
+     </div>
     )
   }
 }

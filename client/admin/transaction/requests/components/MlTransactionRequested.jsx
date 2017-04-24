54a7912d0133ad92048e743f074b8ce9960c586e
pavani <pavani.pasula@raksan.in>
2017-04-24 19:30:55 +0530

    getting request details from server and displaying in table

--- client/admin/transaction/requests/components/MlTransactionRequested.jsx
+++ client/admin/transaction/requests/components/MlTransactionRequested.jsx
@@ -3,6 +3,9 @@ import {render} from 'react-dom';
 import ScrollArea from 'react-scrollbar';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import MlDetailsNotesComponent from './MlDetailsNotesComponent'
+import {findTransactionRequestActionHandler} from '../actions/findTransactionRequests'
+import moment from 'moment'
+
 export default class MlTransactionRequested extends Component {
   constructor(props){
     super(props);
@@ -12,10 +15,25 @@ export default class MlTransactionRequested extends Component {
     return this;
   }
   componentWillMount() {
-    this.setState({requetsInfo:[{transactionCreatedDate:'',transactionTypeId:'',transactionTypeName:'',status:''}]})
-
+   // this.setState({requetsInfo:[{transactionCreatedDate:'',transactionTypeId:'',transactionTypeName:'',status:''}]})
+    const resp=this.findRequestDetails();
+    return resp;
   }
-
+    async findRequestDetails(){
+      let requestDetails = await findTransactionRequestActionHandler('registration');
+      let requestInfo = []
+      for (let i = 0; i < requestDetails.length; i++) {
+        let json = {
+          transactionCreatedDate: requestDetails[i].transactionCreatedDate,
+          requestTypeId: requestDetails[i].requestTypeId,
+          transactionTypeName: requestDetails[i].transactionTypeName,
+          status:'pending',
+          transactionId:requestDetails[i]._id
+        }
+        requestInfo.push(json)
+      }
+      this.setState({'requetsInfo':requestInfo})
+    }
 
   isExpandableRow(row) {
     if (row.transactionCreatedDate!=undefined) return true;
@@ -28,6 +46,8 @@ export default class MlTransactionRequested extends Component {
      <MlDetailsNotesComponent/>
     )
   }
+
+
   render() {
     const options = {
       expandRowBgColor: 'rgb(242, 255, 163)'
@@ -37,6 +57,10 @@ export default class MlTransactionRequested extends Component {
       bgColor: '#feeebf',
       clickToSelect: true,  // click to select, default is false
       clickToExpand: true  // click to expand row, default is false// click to expand row, default is false
+    }
+    function dateFormatter (data){
+      let createdDateTime=data&&data.data&&data.data.date;
+      return <div>{moment(createdDateTime).format('MM/DD/YYYY HH:mm:ss')}</div>;
     }
     return (
       <div className="admin_main_wrap">
@@ -56,8 +80,9 @@ export default class MlTransactionRequested extends Component {
                              selectRow={ selectRow }
                              pagination
             >
-              <TableHeaderColumn dataField="transactionCreatedDate" isKey={true} dataSort={true} width='62px' dataAlign='center'>Date&Time</TableHeaderColumn>
-              <TableHeaderColumn dataField="transactionTypeId">RequestId</TableHeaderColumn>
+              <TableHeaderColumn dataField="transactionId" isKey={true} dataSort={true} width='62px' dataAlign='center' hidden={true}>Id</TableHeaderColumn>
+              <TableHeaderColumn dataField="transactionCreatedDate" customComponent={dateFormatter(this)}>Date&Time</TableHeaderColumn>
+              <TableHeaderColumn dataField="requestTypeId">RequestId</TableHeaderColumn>
               <TableHeaderColumn dataField="transactionTypeName">Type</TableHeaderColumn>
               <TableHeaderColumn dataField="status">Status</TableHeaderColumn>
             </BootstrapTable>

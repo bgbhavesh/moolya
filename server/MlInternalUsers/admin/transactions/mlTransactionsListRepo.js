bb15e5c7d24c5275dcec79afac513fe3cd07d987
pavani <pavani.pasula@raksan.in>
2017-04-24 18:29:11 +0530

    Edit backend user issue is fixed , transaction requested resolver issue fixed

--- server/MlInternalUsers/admin/transactions/mlTransactionsListRepo.js
+++ server/MlInternalUsers/admin/transactions/mlTransactionsListRepo.js
@@ -12,35 +12,44 @@ class MlTransactionsListRepo{
 
   fetchTransactions(transactionType,userId){
     let transasctions = [];
-    let templateuserType=null,templateidentity=null,templateclusterId=null,templatechapterId=null,templatesubChapterId=null,templatecommunityCode=null;
+    /*let templateuserType=null,templateidentity=null,templateclusterId=null,templatechapterId=null,templatesubChapterId=null,templatecommunityCode=null;
     let baseQuery={templateProcessName:process,templateSubProcessName:subProcess,assignedTemplates: { $elemMatch: { "stepCode":stepCode } }};
     let specificQuery=[];
     let query={};
-    let template=null;
+    let template=null;*/
 
     //find user default profile and all its associated roles
-    let user = mlDBController.findOne('users', {_id: args.params.user}, context)
+    let user = mlDBController.findOne('users', {_id: userId}, context)
     let userprofile=user.profile.InternalUprofile.moolyaProfile.userProfiles
-    let defaultProfile=_.filter(userprofile, [isDefault=true])
-    let roles=defaultProfile.userRoles
+    let defaultProfile=_.filter(userprofile,function(user){ return user.isDefault == true; })
+    let roles=defaultProfile[0].userRoles
 
     //for each role find get hierarchy id's that are available in hierarchy Mapping
     let hierarchyInfo = []
     roles.map(function (role, key){
-      let roleHierarchy=mlDBController.find('MlHierarchyAssignments',{parentDepartment:"",parentSubDepartment:""}, context, {teamStructureAssignment: {$elemMatch: {roleId: role.roleId}}})
-      hierarchyInfo.push(roleHierarchy)
+      roleHierarchy=mlDBController.find('MlHierarchyAssignments',{parentDepartment:role.departmentId,parentSubDepartment:role.subDepartmentId}, context, {teamStructureAssignment: {$elemMatch: {roleId: role.roleId}}}).fetch()
+      if(roleHierarchy){
+        for(let i=0;i<roleHierarchy.length;i++){
+          hierarchyInfo.push(roleHierarchy[i])
+        }
+      }
     })
 
     //get all transactions that are available with the hierarchy levelcode and above
     let transactionsInfo = []
     hierarchyInfo.map(function (hierarchy,key) {
-      let transactions = mlDBController.find('MlTransactions',{hierarchy:hierarchy._id})
-      transactionsInfo.push(transactions)
+       transactions = mlDBController.find('MlTransactions',{hierarchy:hierarchy._id}).fetch()
+      if(transactions){
+        for(let i=0;i<transactions.length;i++){
+          transactionsInfo.push(transactions[i])
+        }
+      }
     })
 
     switch(transactionType){
-          case 'Registration':
-            transasctions=_.filter(transactionsInfo, [transactionTypeName='Registration'])
+          case 'registration':
+            transasctions=_.filter(transactionsInfo,function(transc){ return transc.transactionTypeName == 'registration'; })
+            return transasctions
           break;
           case 'Portfolio':
             transasctions=_.filter(transactionsInfo, [transactionTypeName='Portfolio'])
@@ -49,7 +58,7 @@ class MlTransactionsListRepo{
             transasctions=_.filter(transactionsInfo, [transactionTypeName='Office'])
           break;
           case 'Others':
-          transasctions=_.filter(transactionsInfo, [transactionTypeName='Office'])
+          transasctions=_.filter(transactionsInfo, [transactionTypeName='Others'])
           break;
     }
    return null;

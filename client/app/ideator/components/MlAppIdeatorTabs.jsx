7c2a4df69155af7003527d4416679e3c98dae6d6
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-04-28 19:48:52 +0530

    App- Ideator - Tabs - changes made

--- client/app/ideator/components/MlAppIdeatorTabs.jsx
+++ client/app/ideator/components/MlAppIdeatorTabs.jsx
@@ -5,14 +5,13 @@ import ScrollArea from 'react-scrollbar';
 var FontAwesome = require('react-fontawesome');
 var Select = require('react-select');
 import MlTabComponent from "../../../commons/components/tabcomponent/MlTabComponent";
-import MlAppIdeatorDetails from './MlAppIdeatorDetails';
-import MlAppIdeatorIdeas from './MlAppIdeatorIdeas';
-import MlAppIdeatorProblemsSolutions from './MlAppIdeatorProblemsSolutions';
-import MlAppIdeatorAudience from './MlAppIdeatorAudience';
-import MlAppIdeatorLibrary from './MlAppIdeatorLibrary';
-import MlAppIdeatorStrategyAndProblems from './MlAppIdeatorStrategyAndProblems';
-import MlAppIdeatorIntellectual from './MlAppIdeatorIntellectual';
-import MlAppIdeatorLookingFor from './MlAppIdeatorLookingFor'
+import MlPortfolioIdeatorBasicDetailsView from '../../../admin/transaction/portfolio/component/IdeatorView/MlPortfolioIdeatorBasicDetailsView'
+import MlPortfolioIdeatorProblemsAndSolutionsView from '../../../admin/transaction/portfolio/component/IdeatorView/MlProblemsAndSolutionsView'
+import MlPortfolioIdeatorAudienceView from '../../../admin/transaction/portfolio/component/IdeatorView/MlAudienceView'
+import MlPortfolioIdeatorLibraryView from '../../../admin/transaction/portfolio/component/IdeatorView/MlPortfolioLibrary'
+import MlPortfolioIdeatorStrategyPlansView from '../../../admin/transaction/portfolio/component/IdeatorView/MlStartergyAndPlanningView'
+import MlPortfolioIdeatorLookingForView from '../../../admin/transaction/portfolio/component/IdeatorView/MlLookingForView'
+import MlPortfolioIdeatorPlanningTrademarkView from '../../../admin/transaction/portfolio/component/IdeatorView/MlInAndTrademarkView'
 
 
 export default class MlAppIdeatorTabs extends React.Component{
@@ -22,6 +21,7 @@ constructor(props){
   // this.getIdeatorDetails.bind(this);
   // this.getProblemSolution.bind(this)
   // this.getChildContext.bind(this)
+  this.getSelectedAnnotation.bind(this);
 }
 
 // getChildContext(){
@@ -42,65 +42,32 @@ componentDidMount(){
     $('.horizon-swiper').horizonSwiper();
   },300);
 }
+  // getSelectedAnnotation(selAnnotation){
+  //   if(!this.state.popoverOpen){
+  //     this.toggle();
+  //     $('.comment-input-box').slideToggle();
+  //   }
+  //   if(selAnnotation){
+  //     this.setState({annotationData : selAnnotation},function(){
+  //       this.fetchComments(selAnnotation.id);
+  //     })
+  //   }
+  //
+  //
+  // }
 
 getTabComponents(){
   let tabs = [
-    {tabClassName: 'tab', panelClassName: 'panel', title:"Ideator", component:<MlAppIdeatorDetails key="1"/>},
-    {tabClassName: 'tab', panelClassName: 'panel', title:"Ideas", component:<MlAppIdeatorIdeas key="2"/>},
-    {tabClassName: 'tab', panelClassName: 'panel', title:"Problems and Solutions", component: <MlAppIdeatorProblemsSolutions key="3"/>},
-    {tabClassName: 'tab', panelClassName: 'panel', title:"Audience", component:<MlAppIdeatorAudience key="4"/>},
-    {tabClassName: 'tab', panelClassName: 'panel', title:"Library", component: <MlAppIdeatorLibrary key="5"/>},
-    {tabClassName: 'tab', panelClassName: 'panel', title:"Strategy and Planning", component:<MlAppIdeatorStrategyAndProblems key="6"/>},
-    {tabClassName: 'tab', panelClassName: 'panel', title:"Intellectual Planning and Trademark", component:<MlAppIdeatorIntellectual key="7"/>},
-    {tabClassName: 'tab', panelClassName: 'panel', title:"Looking For", component:<MlAppIdeatorLookingFor key="8"/>}
+    {tabClassName: 'tab', panelClassName: 'panel', title:"Details" , component:<MlPortfolioIdeatorBasicDetailsView key="1"  portfolioDetailsId={this.props.portfolioDetailsId}/>},
+    {tabClassName: 'tab', panelClassName: 'panel', title:"Problems and Solutions" , component:<MlPortfolioIdeatorProblemsAndSolutionsView key="2"  portfolioDetailsId={this.props.portfolioDetailsId} getSelectedAnnotations={}/>},
+    {tabClassName: 'tab', panelClassName: 'panel', title:"Audience" , component:<MlPortfolioIdeatorAudienceView key="3"  portfolioDetailsId={this.props.portfolioDetailsId} getSelectedAnnotations={}/>},
+    {tabClassName: 'tab', panelClassName: 'panel', title:"Library" , component:<MlPortfolioIdeatorLibraryView key="4"  portfolioDetailsId={this.props.portfolioDetailsId} getSelectedAnnotations={}/>},
+    {tabClassName: 'tab', panelClassName: 'panel', title:"Strategy and Plans" , component:<MlPortfolioIdeatorStrategyPlansView key="4"  portfolioDetailsId={this.props.portfolioDetailsId} getSelectedAnnotations={}/>},
+    {tabClassName: 'tab', panelClassName: 'panel', title:"IntellectualPlanning and Trademark" , component:<MlPortfolioIdeatorPlanningTrademarkView key="5"  portfolioDetailsId={this.props.portfolioDetailsId} getSelectedAnnotations={}/>},
+    {tabClassName: 'tab', panelClassName: 'panel', title:"Looking For" , component:<MlPortfolioIdeatorLookingForView key="5"  portfolioDetailsId={this.props.portfolioDetailsId} getSelectedAnnotations={}/>},
   ]
   return tabs;
 }
-
-// getIdeatorDetails(details){
-//   let data = this.state.ideatorPortfolio;
-//   data['portfolioIdeatorDetails']=details;
-//   this.setState({ideatorPortfolio : data})
-//   // this.state.ideatorPortfolio['portfolioIdeatorDetails'] = details;
-//   // this.setState({ideatorDetails:details})
-//   this.props.getPortfolioDetails({ideatorPortfolio:this.state.ideatorPortfolio});
-// }
-// getIdeas(details) {
-//   let data = this.state.ideatorPortfolio;
-//   data['ideas']=details;
-//   this.setState({ideatorPortfolio : data})
-//   this.props.getPortfolioDetails({ideatorPortfolio:this.state.ideatorPortfolio});
-// }
-// getProblemSolution(details) {
-//   let data = this.state.ideatorPortfolio;
-//   data['problemSolution']=details;
-//   this.setState({ideatorPortfolio : data})
-//   this.props.getPortfolioDetails({ideatorPortfolio:this.state.ideatorPortfolio});
-// }
-// getStrategyAndPlanning(details) {
-//   let data = this.state.ideatorPortfolio;
-//   data['strategyAndPlanning']=details;
-//   this.setState({ideatorPortfolio : data})
-//   this.props.getPortfolioDetails({ideatorPortfolio:this.state.ideatorPortfolio});
-// }
-// getIntellectualPlanning(details) {
-//   let data = this.state.ideatorPortfolio;
-//   data['intellectualPlanning']=details;
-//   this.setState({ideatorPortfolio : data})
-//   this.props.getPortfolioDetails({ideatorPortfolio:this.state.ideatorPortfolio});
-// }
-// getAudience(details) {
-//   let data = this.state.ideatorPortfolio;
-//   data['audience']=details;
-//   this.setState({ideatorPortfolio : data})
-//   this.props.getPortfolioDetails({ideatorPortfolio:this.state.ideatorPortfolio});
-// }
-// getLookingFor(details) {
-//   let data = this.state.ideatorPortfolio;
-//   data['lookingFor']=details;
-//   this.setState({ideatorPortfolio : data})
-//   this.props.getPortfolioDetails({ideatorPortfolio:this.state.ideatorPortfolio});
-// }
 
 componentWillMount()
 {

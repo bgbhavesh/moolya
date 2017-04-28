8bdb6757b521f0432b69ac0b234af42eecdd7ba0
mohasinraksan <mohammed.mohasin@raksan.in>
2017-04-28 22:09:22 +0530

    Console log added to check for logs

--- server/commons/mlEmail.js
+++ server/commons/mlEmail.js
@@ -9,6 +9,8 @@ var from=Meteor.settings.private.fromEmailAddr;
 Meteor.startup(function () {
   //console.log(encodeURIComponent("qasmtp@moolya.in"));
   process.env.MAIL_URL = Meteor.settings.private.smtpMailUrl;
+  console.log("Email Url: "+ Meteor.settings.private.smtpMailUrl);
+  console.log(encodeURIComponent("qasmtp@moolya.in"));
 });
 
   class SmtpEmail{

ee0cffa41a1acc37914a69aa84e395003381b140
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-04-28 20:19:26 +0530

    external user resolver commented due to issues

--- server/MlExternalUsers/externaluser/externalUserResolver.js
+++ server/MlExternalUsers/externaluser/externalUserResolver.js
@@ -1,53 +1,57 @@
-/**
- * Created by venkatsrinag on 28/4/17.
- */
-import MlResolver from '../../commons/mlResolverDef'
-
-MlResolver.MlQueryResolver['fetchIdeatorUsers'] = (obj, args, context, info) =>
-{
-    let sample = [
-        {
-            username:"",
-            profile:{
-                isExternaluser:true,
-                city:"",
-                state:"",
-                country:"",
-                cluster:"",
-                chapter:"",
-                community:"",
-                externalUserProfile:{
-                    firstName:"",
-                    lastName:"",
-                    userProfiles:[
-                        {
-                            registrationId:"",
-                            portfolio:[
-                                {
-                                    portfolioId:"",
-                                    isDefault:true
-                                }
-                            ],
-                            countryName:"",
-                            countryId:"",
-                            cityName:"",
-                            mobileNumber:"",
-                            clusterId:"",
-                            clusterName:"",
-                            chapterId:"",
-                            chapterName:"",
-                            subChapterId:"",
-                            subChapterName:"",
-                            communityId:"",
-                            communityName:"",
-                            communityType:"",
-                            isDefault:true,
-                            isProfileActive:true,
-                            accountType:""
-                        }
-                    ]
-                }
-            }
-        }
-    ]
-}
+// /**
+//  * Created by venkatsrinag on 28/4/17.
+//  */
+// import MlResolver from '../../commons/mlResolverDef'
+// import MlRespPayload from '../../commons/mlPayload'
+//
+// MlResolver.MlQueryResolver['fetchIdeatorUsers'] = (obj, args, context, info) =>
+// {
+//     let sample = [
+//         {
+//             username:"",
+//             profile:{
+//                 isExternaluser:true,
+//                 city:"",
+//                 state:"",
+//                 country:"",
+//                 cluster:"",
+//                 chapter:"",
+//                 community:"",
+//                 externalUserProfile:{
+//                     firstName:"",
+//                     lastName:"",
+//                     userProfiles:[
+//                         {
+//                             registrationId:"",
+//                             portfolio:[
+//                                 {
+//                                     portfolioId:"",
+//                                     isDefault:true
+//                                 }
+//                             ],
+//                             countryName:"",
+//                             countryId:"",
+//                             cityName:"",
+//                             mobileNumber:"",
+//                             clusterId:"",
+//                             clusterName:"",
+//                             chapterId:"",
+//                             chapterName:"",
+//                             subChapterId:"",
+//                             subChapterName:"",
+//                             communityId:"",
+//                             communityName:"",
+//                             communityType:"",
+//                             isDefault:true,
+//                             isProfileActive:true,
+//                             accountType:""
+//                         }
+//                     ]
+//                 }
+//             }
+//         }
+//     ]
+//   let code = 200;
+//   let response = new MlRespPayload().successPayload(sample, code);
+//   return response;
+// }

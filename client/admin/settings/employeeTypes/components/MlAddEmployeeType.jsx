20fe9c81e179f427411c7d98fb65d9025f40aed1
Mukhil_Padmanabhan <mukhil.padmanabhan@raksan.in>
2017-04-30 18:50:36 +0530

    MOOLYA-1023

--- client/admin/settings/employeeTypes/components/MlAddEmployeeType.jsx
+++ client/admin/settings/employeeTypes/components/MlAddEmployeeType.jsx
@@ -71,7 +71,7 @@ class MlAddEmployeeType extends React.Component{
     return (
       <div className="admin_main_wrap">
         <div className="admin_padding_wrap">
-          <h2>Create Employee type</h2>
+          <h2>Create Employee Type</h2>
           <div className="col-md-6 nopadding-left">
             <div className="form_bg">
               <form>

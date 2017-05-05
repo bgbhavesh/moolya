83805ccf089771997c3edc794721e288c79fba19
pavani <pavani.pasula@raksan.in>
2017-05-05 19:47:37 +0530

    Moolya-1129 fixed

--- server/MlInternalUsers/admin/settings/currencyType/currencyResolver.js
+++ server/MlInternalUsers/admin/settings/currencyType/currencyResolver.js
@@ -1,9 +1,13 @@
 import MlResolver from '../../../../commons/mlResolverDef'
 import {createcluster} from '../../clusters/clusterResolver'
 import MlRespPayload from '../../../../commons/mlPayload'
-
+import _ from "lodash"
 MlResolver.MlQueryResolver['fetchCurrency'] = (obj, args, context, info) =>{
   let currency;
     currency =  MlCurrencyType.find({isActive: true}).fetch();
-  return currency;
+    currenctUniq=_.uniqBy(currency, function (e) {
+      return e.currencyName;
+    });
+  currencySort = _.sortBy(currenctUniq, 'currencyName');
+  return currencySort;
 }

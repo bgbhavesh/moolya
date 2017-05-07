1373fe8351dc2f50c439f831a35a341eaebfef29
mohasinraksan <mohammed.mohasin@raksan.in>
2017-05-08 00:53:11 +0530

    Switch Profile Swiper changes

--- client/app/profile/components/MlAppSwitchProfile.jsx
+++ client/app/profile/components/MlAppSwitchProfile.jsx
@@ -1,53 +1,78 @@
 import React from 'react';
 import { render } from 'react-dom';
 import {fetchExternalUserProfilesActionHandler} from '../actions/findUserProfiles';
-
+import _ from 'lodash';
 
 export default class MlAppSwitchProfile extends React.Component{
 
   constructor(props, context){
       super(props);
-      this.state= {loading: false,userProfiles:[]};
+      this.state= {loading: true,swiper:null,userProfiles:[],currentSlideIndex:0};
       this.fetchExternalUserProfiles.bind(this);
+      this.onSlideIndexChange.bind(this);
+      this.initializeSwiper.bind(this);
     return this;
   }
 
-  componentDidMount()
-  {
-    $(function() {
+  onSlideIndexChange(swiper){
+     console.log(swiper);
+    if(this.state.currentSlideIndex!==swiper.activeIndex){
+      this.setState({'currentSlideIndex':swiper.activeIndex});
+    }
+  }
+
+  initializeSwiper(){
+    if (!this.swiper) {
+      this.swiper = new Swiper('.profile_container', {
+        pagination: '.swiper-pagination',
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        initialSlide: this.state.currentSlideIndex,
+        slidesPerView: 'auto',
+        coverflow: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true
+        },
+        onSlideChangeEnd: this.onSlideIndexChange.bind(this)
+      });
+    }
+  };
+  componentDidMount() {
+    $(function () {
       $('.float-label').jvFloat();
     });
-
-    var swiper = new Swiper('.profile_container', {
-      pagination: '.swiper-pagination',
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      initialSlide:1,
-      slidesPerView: 'auto',
-      coverflow: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true
-      }
-    });
+   // this.initializeSwiper();
   }
+
+
 
   async fetchExternalUserProfiles(){
     const response = await fetchExternalUserProfilesActionHandler();
     if(response){
-      this.setState({loading: false, userProfiles: response});
+      let index=_.findIndex(response, {'isDefault':true })||0;
+      let initialSlideIndex=index>=0?index:0;
+      this.setState({loading: false, userProfiles: response,'currentSlideIndex':initialSlideIndex});
     }
   }
 
-  componentWillMount()
+  async componentWillMount()
   {
-    this.fetchExternalUserProfiles();
+    var resp=await this.fetchExternalUserProfiles();
+    this.initializeSwiper();
   }
 
   render(){
+    const showLoader=this.state.loading;
+    if (showLoader) {
+      return <div className="loader_wrap"></div>;
+    }
+
+    let profileDetails=this.state.userProfiles[this.state.currentSlideIndex]||{};
+
     return (
       <div className="app_main_wrap">
         <div className="app_padding_wrap portfolio-main-wrap">
@@ -58,51 +83,53 @@ export default class MlAppSwitchProfile extends React.Component{
             <div className="col-md-2"></div>
             <div className="col-md-8">
               <div className="swiper-container profile_container">
-
                 <div className="swiper-wrapper">
-                    {this.state.userProfiles.map(function (prf, idx) {
-                      return(
-                        <div className="swiper-slide profile_accounts">
-                          <img src={prf.countryFlag?prf.countryFlag:""}/><br />{prf.clusterName?prf.clusterName:""}
-                          <h2>{prf.communityDefName?prf.communityDefName:""}</h2>
-                        </div>
-                      )
-                    })}
 
+                  {this.state.userProfiles.map(function (prf, idx) {
+                    return(
+                      <div className="swiper-slide profile_accounts" key={idx} name={idx}>
+                        <img src={prf.countryFlag?prf.countryFlag:""}/><br />{prf.clusterName?prf.clusterName:""}
+                        <h2>{prf.communityDefName?prf.communityDefName:""}</h2>
+                      </div>
+                    )
+                  })}
 
                 </div>
-
                 <div className="swiper-pagination"></div>
               </div>
             </div>
+
             <div className="col-md-12">
               <br />
               <br />
               <div className="clearfix"></div>
             </div>
+
             <div className="col-md-6">
               <div className="form-group">
-                <input type="text" placeholder="Community" className="form-control float-label" id="" value="Ideator"  disabled/>
+                  <input type="text" placeholder="Community" className="form-control float-label"  value={profileDetails.communityDefName}  disabled/>
               </div>
               <div className="form-group">
-                <input type="text" placeholder="Country" className="form-control float-label" id="" value="India" disabled/>
+                <input type="text" placeholder="Country" className="form-control float-label"  value={profileDetails.clusterName} disabled/>
               </div>
 
               <div className="form-group">
-                <input type="text" placeholder="Subscription Type" className="form-control float-label" id="" value="Stater" disabled/>
+                <input type="text" placeholder="Subscription Type" className="form-control float-label"  value={profileDetails.accountType} disabled/>
               </div>
             </div>
+
             <div className="col-md-6">
               <div className="form-group">
-                <input type="text" placeholder="Identity" className="form-control float-label" id="" value="As individual" disabled/>
+                <input type="text" placeholder="Identity" className="form-control float-label"  value={profileDetails.identityType} disabled/>
               </div>
               <div className="form-group">
-                <input type="text" placeholder="City" className="form-control float-label" id="" value="Hyderabad" disabled/>
+                <input type="text" placeholder="City" className="form-control float-label"  value={profileDetails.chapterName} disabled/>
               </div>
               <div className="form-group">
-                <input type="text" placeholder="Status" className="form-control float-label" id="" value="Active" disabled/>
+                <input type="text" placeholder="Status" className="form-control float-label"  value={profileDetails.isProfileActive} disabled/>
               </div>
             </div>
+
             <div className="col-md-12 text-center">
               <div className="col-md-4">
                 <a href="#" className="fileUpload mlUpload_btn">Make Default</a>

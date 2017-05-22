f30661e98550cb39619afb6f147253a442b9243e
pavani <pavani.pasula@raksan.in>
2017-05-22 17:32:32 +0530

    Created Date added to registration and portfolio

--- client/admin/transaction/requested/component/step1.jsx
+++ client/admin/transaction/requested/component/step1.jsx
@@ -14,7 +14,7 @@ import _ from 'lodash';
 import {mlFieldValidations} from '../../../../commons/validations/mlfieldValidation';
 import MlLoader from '../../../../commons/components/loader/loader'
 import {findAccountTypeActionHandler} from '../../../settings/accountType/actions/findAccountTypeAction'
-
+import moment from 'moment'
 var FontAwesome = require('react-fontawesome');
 var options3 = [
   {value: 'Yes', label: 'Yes'},
@@ -220,7 +220,7 @@ export default class step1 extends React.Component{
       let Details = {
         registrationId: this.props.registrationId,
         registrationDetail: {
-          registrationDate :this.refs.datetime.value,
+        //  registrationDate :this.refs.datetime.value,
           registrationId: this.state.registrationId,
           firstName: this.refs.firstName.value,
           lastName: this.refs.lastName.value,
@@ -413,7 +413,7 @@ export default class step1 extends React.Component{
                 <div className="form_bg">
                   <form>
                     <div className="form-group">
-                      <input type="text" ref="datetime" placeholder="Date & Time" defaultValue={that.state.registrationDetails&&that.state.registrationDetails.registrationDate} className="form-control float-label" id="" disabled="true"/>
+                      <input type="text" ref="datetime" placeholder="Date & Time" defaultValue={moment(that.state.registrationDetails&&that.state.registrationDetails.registrationDate).format('MM/DD/YYYY hh:mm:ss')} className="form-control float-label" id="" disabled="true"/>
                     </div>
                     <div className="form-group">
                       <input type="text" placeholder="Request ID"  defaultValue={that.state.registrationId} className="form-control float-label" id="" disabled="true"/>

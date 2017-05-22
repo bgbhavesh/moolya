c706b650119965948f837023645537cef2b687d4
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-05-22 16:00:12 +0530

    MOOLYA-710 is fixed

--- client/admin/cluster/components/MlClustersList.jsx
+++ client/admin/cluster/components/MlClustersList.jsx
@@ -7,8 +7,8 @@ export default class MlClustersList extends Component {
 
   render(){
     const data=this.props.data||[];
-    const list=  data.map((prop) =>
-      <div className="col-lg-2 col-md-4 col-sm-4" key={prop.displayName}>
+    const list=  data.map((prop, idx) =>
+      <div className="col-lg-2 col-md-4 col-sm-4" key={idx}>
         <div className="list_block">
           <div className={`cluster_status ${prop.statusField|| ""}_cl `}></div>
           <a href={clusterRoutes.clusterDetailsRoute(prop.id)}> <div className={"hex_outer"}><img src={prop.countryFlag}/></div></a>

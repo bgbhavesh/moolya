b85b10f833b27dab87d7d72b21f2650b7c573ad1
Mukhil.Padmanabhan <mukhil.padmanabhan@raksan.in>
2017-05-24 17:23:35 +0530

    Systems Accordian update

--- client/admin/transaction/transactionsLog/component/MlTransactionDetailsComponent.jsx
+++ client/admin/transaction/transactionsLog/component/MlTransactionDetailsComponent.jsx
@@ -5,10 +5,7 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 var Select = require('react-select');
 var FontAwesome = require('react-fontawesome');
 import ScrollArea from 'react-scrollbar';
-var options = [
-  { value: 'role', label: 'Role' },
-  { value: 'role', label: 'Role' }
-];
+
 function logChange(val) {
   console.log("Selected: " + val);
 }
@@ -16,7 +13,6 @@ function logChange(val) {
 
 export default class MlTransactionDetailsComponent extends React.Component {
   render() {
-    console.log(this.props.data);
     return (
       <div className="ml_tabs">
         <ul  className="nav nav-pills">
@@ -29,9 +25,9 @@ export default class MlTransactionDetailsComponent extends React.Component {
           <li >
             <a  href={`#DeviceDetails${this.props.data._id}`} data-toggle="tab">Device Details</a>
           </li>
-          <li >
-            <a  href="#4a" data-toggle="tab">History</a>
-          </li>
+          {/*<li >*/}
+            {/*<a  href="#4a" data-toggle="tab">History</a>*/}
+          {/*</li>*/}
           <li >
             <a  href={`#notes${this.props.data._id}`} data-toggle="tab">Notes</a>
           </li>
@@ -58,9 +54,9 @@ export default class MlTransactionDetailsComponent extends React.Component {
                 </div>
               </div>
               <div className="col-md-6">
-                <div className="form-group">
-                  <input type="text" placeholder="Phone no"  className="form-control float-label" id=""/>
-                </div>
+                {/*<div className="form-group">*/}
+                  {/*<input type="text" placeholder="Phone no"  className="form-control float-label" id=""/>*/}
+                {/*</div>*/}
                 <div className="form-group">
                   <input type="text" placeholder="Cluster"  defaultValue = {this.props.data.clusterName} disabled={true} className="form-control float-label" id=""/>
                 </div>
@@ -97,9 +93,9 @@ export default class MlTransactionDetailsComponent extends React.Component {
                 <div className="form-group">
                   <input type="text" placeholder="Duration" disabled={true} className="form-control float-label" id=""/>
                 </div>
-                <div className="form-group">
-                  <input type="text" placeholder="Triggered Location" disabled={true} className="form-control float-label" id=""/>
-                </div>
+                {/*<div className="form-group">*/}
+                  {/*<input type="text" placeholder="Triggered Location" disabled={true} className="form-control float-label" id=""/>*/}
+                {/*</div>*/}
               </div>
 
             </div>
@@ -141,18 +137,6 @@ export default class MlTransactionDetailsComponent extends React.Component {
           </div>
 
 
-
-
-          {/*<div className="tab-pane active" id="4a">*/}
-
-
-          {/*</div>*/}
-
-
-
-
-
-
         </div>
 
       </div>
@@ -168,80 +152,3 @@ const selectRow = {
   clickToExpand: true // click to expand row, default is false
 };
 
-//  class EditTaxation extends React.Component {
-//   componentDidMount() {
-//     $('.switch input').change(function() {
-//       if ($(this).is(':checked')) {
-//         $(this).parent('.switch').addClass('on');
-//       } else {
-//         $(this).parent('.switch').removeClass('on');
-//       }
-//     });
-//   }
-//   constructor(props) {
-//     super(props);
-//   }
-//
-//   isExpandableRow(row) {
-//     if (row.id <= 1) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-//
-//   expandComponent(row) {
-//     return (
-//       <InnerTable data={ row.expand } />
-//     );
-//   }
-//   render() {
-//     const options = {
-//       expandRowBgColor: 'rgb(242, 255, 163)'
-//     };
-//     return (
-//       <div className="admin_main_wrap">
-//         <div className="admin_padding_wrap">
-//           <h2>Edit Taxation</h2>
-//           <div className="main_wrap_scroll">
-//             <ScrollArea
-//               speed={0.8}
-//               className="main_wrap_scroll"
-//               smoothScrolling={true}
-//               default={true}
-//             >
-//
-//
-//               <br className="brclear"/>
-//               <BootstrapTable  data={ taxes }
-//                                options={ options }
-//                                expandableRow={ this.isExpandableRow }
-//                                expandComponent={ this.expandComponent }
-//                                selectRow={ selectRow }
-//                                pagination
-//                                search
-//               >
-//                 <TableHeaderColumn dataField="id" isKey={true} dataSort={true} width='62px' dataAlign='center'>S.No</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="dateTime">Date & Time</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="userId">User ID</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="name">Name</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="tarId">Tra ID</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="cluster">Cluster</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="chapter">Chapter</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="subChater">Sub Chapter</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="community">Community</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="payment">Payment</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="status">Status</TableHeaderColumn>
-//                 <TableHeaderColumn dataField="action">Action</TableHeaderColumn>
-//               </BootstrapTable>
-//
-//             </ScrollArea>
-//           </div>
-//         </div>
-//
-//
-//       </div>
-//     )
-//   }
-// }
-// ;

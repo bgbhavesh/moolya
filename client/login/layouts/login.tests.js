97d93b9b41e096963aa5118abba5d243bcf3834b
Anakha <anakha.ramachandran@raksan.in>
2017-05-29 19:22:12 +0530

    Adds test for login button

--- client/login/layouts/login.tests.js
+++ client/login/layouts/login.tests.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {mlValidations} from "../../commons/validations/formValidation";
 import {
   renderIntoDocument,
   findRenderedDOMComponentWithTag,
@@ -7,18 +8,37 @@ import {
 } from 'react-addons-test-utils';
 
 // Need to import to get the value of the MlLoginContent from the file.
+import {spy, stub} from 'sinon';
 import {} from './login.jsx';
 import { expect } from 'chai';
 
-describe('TodoInput', () => {
+describe('Client Side Test: Login Module', () => {
+
   it('should render an input field and button', () => {
-    const component = renderIntoDocument(
-      <MlLoginContent />
-    );
+    formSubmits = () => { };
+    component = renderIntoDocument(
+      <MlLoginContent formSubmit={formSubmits}/>);
     const inputField = scryRenderedDOMComponentsWithClass(component, 'input');
     const button = scryRenderedDOMComponentsWithClass(component, 'button');
 
     expect(inputField).to.be.ok;
     expect(button).to.be.ok;
   });
+  /*
+    For performing the second test case, add ref="signIn" to the sign in button in login.jsx
+  */
+  it('should validate user on button click', function () {
+    mlValidations.formValidations = spy();
+    const user = component.refs.username;
+    const password = component.refs.password;
+    const button = component.refs.signIn;
+    user.value = 'anakhar.22@gmail.com';
+    password.value = '1234!@#';
+    Simulate.change(user);
+    Simulate.change(password);
+    Simulate.click(button);
+    console.log(component.refs.username.value);
+    expect(mlValidations.formValidations.calledOnce).to.be.true;
+  });
+
 });

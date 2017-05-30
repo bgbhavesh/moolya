07645c015094fce3a60d4bc6942179ad3f7eadd4
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-05-30 15:06:45 +0530

    Cluster Admin Settings Autherisation

--- client/admin/settings/socialLinks/config/mlSocialLinkConfig.js
+++ client/admin/settings/socialLinks/config/mlSocialLinkConfig.js
@@ -74,7 +74,7 @@ const mlSocialLinksTypeTableConfig=new MlViewer.View({
               `*/
   graphQlQuery:gql`
               query ContextSpecSearch($context:ContextParams,$offset: Int, $limit: Int,$searchSpec:SearchSpec,$fieldsData: [GenericFilter], $sortData: [SortFilter]){
-                   data:ContextSpecSearch(module:"socialLinks",context:$context,offset:$offset,limit:$limit,searchSpec:$searchSpec,fieldsData: $fieldsData, sortData: $sortData){
+                   data:ContextSpecSearch(module:"MASTERSETTINGS",context:$context,offset:$offset,limit:$limit,searchSpec:$searchSpec,fieldsData: $fieldsData, sortData: $sortData){
                         totalRecords
                            data{
                             ...on MasterSettings{

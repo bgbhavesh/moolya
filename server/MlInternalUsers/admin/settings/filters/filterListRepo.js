bb5a64d75549f14e389f9481b68aa1f10beeb999
sireesha <sireesha.dhavala@raksanconsulting.com>
2017-05-31 16:11:35 +0530

    MOOLYA-1701 issue is done

--- server/MlInternalUsers/admin/settings/filters/filterListRepo.js
+++ server/MlInternalUsers/admin/settings/filters/filterListRepo.js
@@ -133,7 +133,7 @@ export default class MlFilterListRepo{
       case "Gen_Chapters":
 
         if(listData.length < 1){
-          if(userProfile.hierarchyLevel == 4 || userProfile.hierarchyLevel == 3){
+          if(userProfile.hierarchyLevel == 4 || userProfile.hierarchyLevel == 3 || userProfile.hierarchyLevel == 0){
             let arrayOfChapters = _.pluck(requestParams.filteredListId, 'value') || [];
             result= MlChapters.find({ clusterId: {$in : arrayOfChapters},isActive : true}).fetch();
           }else{

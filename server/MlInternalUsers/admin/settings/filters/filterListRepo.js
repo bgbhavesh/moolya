9201c544106050e75c55d87b59e7e9099368d714
sireesha <sireesha.dhavala@raksanconsulting.com>
2017-05-31 16:14:26 +0530

    Added community hierarchy in filters

--- server/MlInternalUsers/admin/settings/filters/filterListRepo.js
+++ server/MlInternalUsers/admin/settings/filters/filterListRepo.js
@@ -162,7 +162,7 @@ export default class MlFilterListRepo{
 
         if(listData.length < 1){
 
-          if(userProfile.hierarchyLevel == 4 || userProfile.hierarchyLevel == 3 || userProfile.hierarchyLevel == 2){
+          if(userProfile.hierarchyLevel == 4 || userProfile.hierarchyLevel == 3 || userProfile.hierarchyLevel == 2 || userProfile.hierarchyLevel == 0){
             let arrayOfGenSubChapter = _.pluck(requestParams.filteredListId, 'value') || [];
             result= MlSubChapters.find({clusterId: {$in : arrayOfGenSubChapter}, chapterId: {$in : arrayOfGenSubChapter},isActive : true}).fetch();
           }else{

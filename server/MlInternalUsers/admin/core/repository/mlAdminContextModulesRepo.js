f31a2383b37304396d4f48dfc40f6d5fc784d3d4
sireesha <sireesha.dhavala@raksanconsulting.com>
2017-06-12 13:11:28 +0530

    Filter community changes in all modules

--- server/MlInternalUsers/admin/core/repository/mlAdminContextModulesRepo.js
+++ server/MlInternalUsers/admin/core/repository/mlAdminContextModulesRepo.js
@@ -141,10 +141,19 @@ let CoreModules = {
     }
     let serverQuery = {};
     let query = {};
+    let userContextQuery = {};
     requestParams = requestParams ? requestParams : null;
     let reqArray=requestParams.moduleName.split(',');
     serverQuery={moduleName:{$in:reqArray}}
-    query = mergeQueries(userFilterQuery, serverQuery);
+    let userProfile=new MlAdminUserContext().userProfileDetails(context.userId)||{};
+    if(userProfile.hierarchyLevel == 4){
+      userContextQuery = {}
+    }else if(userProfile.hierarchyLevel == 3){
+      let clusterIds = userProfile && userProfile.defaultProfileHierarchyRefId?userProfile.defaultProfileHierarchyRefId:[];
+      userContextQuery = {clusterId:{$in:clusterIds}}
+    }
+    query = mergeQueries(userContextQuery,userFilterQuery, serverQuery);
+
     const data = MlAudit.find(query, fieldsProj).fetch();
     const totalRecords = mlDBController.find('MlAudit', query, context, fieldsProj).count();
 

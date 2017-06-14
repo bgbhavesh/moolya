b7e049ed8f6596c1d7fda94a637c63cb654dcec5
venkatsrinag <venkatasrinag.pindiproli@raksanconsulting.com>
2017-06-14 19:26:01 +0530

    activity task graphql shcema

---
+++ server/MlExternalUsers/activity/activitySchema.js
@@ -0,0 +1,27 @@
+/**
+ * Created by venkatsrinag on 14/6/17.
+ */
+
+import {mergeStrings} from 'gql-merge';
+import MlSchemaDef from '../../commons/mlSchemaDef'
+
+// let activity=`
+//     type Activity{
+//     }
+//
+//     input activity{
+//     }
+//
+//     type Query{
+//         fetchActivities:[Activity]
+//         fetchActivity:Activity
+//     }
+//
+//     type Mutation{
+//         createActivity:response
+//         updateActivity:response
+//     }
+// `
+//
+//
+// MlSchemaDef['schema'] = mergeStrings([MlSchemaDef['schema'], activity]);

a7d938a74b174566d0057bce2cc4bb150b4acbcf
Mukhil.Padmanabhan <mukhil.padmanabhan@raksan.in>
2017-06-15 13:35:47 +0530

    service task and activity schema and resolver update

--- server/MlExternalUsers/Service/serviceSchema.js
+++ server/MlExternalUsers/Service/serviceSchema.js
@@ -1,134 +1,134 @@
-// /**
-//  * Created by Mukhil on 14/6/17.
-//  */
-//
-// import {mergeStrings} from 'gql-merge';
-// import MlSchemaDef from '../../commons/mlSchemaDef'
-// import MlResolver from "../../commons/mlResolverDef";
-//
-//
-// let service=`
-//   type facilitationCharge {
-//      amount: Int
-//      percentage: Int
-//      derivedAmount: Int
-//   }
-//   type attachments {
-//      name: String
-//      info: String
-//      isMandatory: Boolean
-//   }
-//   type payment {
-//      amount: Int
-//      isDiscount: Boolean
-//      discountAmount: Boolean
-//      discountPercentage: Int
-//      isTaxInclusive: Boolean
-//      isPromoCodeApplicable: Boolean
-//   }
-//   type termsAndCondition {
-//      isCancelable: Boolean
-//      isRefundable: Boolean
-//      isReschedulable: Boolean
-//      noOfReschedulable: Int
-//   }
-//   type duration{
-//           hours: Int
-//           minutes: Int
-//         }
-//
-//   type Service {
-//     userId: String
-//     profileId: String
-//     name: String
-//     displayName: String
-//     noOfSession: Int
-//     sessionFrequency: Int
-//     duration: duration
-//     status: Boolean
-//     termsAndCondition: termsAndCondition
-//     attachments: attachments
-//     payment: payment
-//     tasks: [String]
-//     facilitationCharge : facilitationCharge
-//     note: String
-//     createdAt: Date
-//     updatedAt: Date
-//
-//   }
-//
-//    input facilitationCharge {
-//        amount: Int
-//        percentage: Int
-//        derivedAmount: Int
-//    }
-//
-//    input attachments {
-//        name: String
-//        info: String
-//        isMandatory: Boolean
-//    }
-//
-//    input payment {
-//        amount: Int
-//        isDiscount: Boolean
-//        discountAmount: Boolean
-//        discountPercentage: Int
-//        isTaxInclusive: Boolean
-//        isPromoCodeApplicable: Boolean
-//    }
-//
-//    input termsAndCondition {
-//        isCancelable: Boolean
-//        isRefundable: Boolean
-//        isReschedulable: Boolean
-//        noOfReschedulable: Int
-//    }
-//
-//    input duration{
-//       hours: Int
-//       minutes: Int
-//    }
-//
-//    input Service {
-//         userId: String
-//         profileId: String
-//         name: String
-//         displayName: String
-//         noOfSession: Int
-//         sessionFrequency:Int
-//         duration: duration
-//         status: Boolean
-//         termsAndCondition: termsAndCondition
-//         attachments: attachments
-//         payment: payment
-//         tasks: [String]
-//         facilitationCharge : facilitationCharge
-//         note: String
-//         createdAt: Date
-//         updatedAt: Date
-//     }
-//
-//     type Query {
-//         fetchServices:[Service]
-//         fetchService(id:String):Service
-//     }
-//
-//     type Mutation {
-//         createService:response
-//         updateService:response
-//     }
-// `;
-//
-//
-// MlSchemaDef['schema'] = mergeStrings([MlSchemaDef['schema'], service]);
-//
-// let supportedApi = [
-//   {api:'fetchServices', actionName:'READ', moduleName:"OFFICE"},
-//   {api:'fetchService', actionName:'READ', moduleName:"OFFICE"},
-//
-//   {api:'createService', actionName:'CREATE', moduleName:"OFFICE"},
-//   {api:'updateService', actionName:'UPDATE', moduleName:"OFFICE"},
-// ]
-//
-// MlResolver.MlModuleResolver.push(supportedApi)
+/**
+ * Created by Mukhil on 14/6/17.
+ */
+
+import {mergeStrings} from 'gql-merge';
+import MlSchemaDef from '../../commons/mlSchemaDef'
+import MlResolver from "../../commons/mlResolverDef";
+
+
+let service=`
+  type FacilitationCharge {
+     amount: Int
+     percentage: Int
+     derivedAmount: Int
+  }
+  type Attachments {
+     name: String
+     info: String
+     isMandatory: Boolean
+  }
+  type Payment {
+     amount: Int
+     isDiscount: Boolean
+     discountAmount: Boolean
+     discountPercentage: Int
+     isTaxInclusive: Boolean
+     isPromoCodeApplicable: Boolean
+  }
+  type TermsAndCondition {
+     isCancelable: Boolean
+     isRefundable: Boolean
+     isReschedulable: Boolean
+     noOfReschedulable: Int
+  }
+  type Duration{
+     hours: Int
+     minutes: Int
+  }
+
+  type Service {
+    userId: String
+    profileId: String
+    name: String
+    displayName: String
+    noOfSession: Int
+    sessionFrequency: Int
+    duration: Duration
+    status: Boolean
+    termsAndCondition: TermsAndCondition
+    attachments: Attachments
+    payment: Payment
+    tasks: [String]
+    facilitationCharge : FacilitationCharge
+    note: String
+    createdAt: Date
+    updatedAt: Date
+
+  }
+
+   input facilitationCharge {
+       amount: Int
+       percentage: Int
+       derivedAmount: Int
+   }
+
+   input attachments {
+       name: String
+       info: String
+       isMandatory: Boolean
+   }
+
+   input payment {
+       amount: Int
+       isDiscount: Boolean
+       discountAmount: Boolean
+       discountPercentage: Int
+       isTaxInclusive: Boolean
+       isPromoCodeApplicable: Boolean
+   }
+
+   input termsAndCondition {
+       isCancelable: Boolean
+       isRefundable: Boolean
+       isReschedulable: Boolean
+       noOfReschedulable: Int
+   }
+
+   input duration{
+      hours: Int
+      minutes: Int
+   }
+
+   input service {
+        userId: String
+        profileId: String
+        name: String
+        displayName: String
+        noOfSession: Int
+        sessionFrequency:Int
+        duration: duration
+        status: Boolean
+        termsAndCondition: termsAndCondition
+        attachments: attachments
+        payment: payment
+        tasks: [String]
+        facilitationCharge : facilitationCharge
+        note: String
+        createdAt: Date
+        updatedAt: Date
+    }
+
+    type Query {
+        fetchServices:[Service]
+        fetchService(id:String):Service
+    }
+
+    type Mutation {
+        createService:response
+        updateService:response
+    }
+`;
+
+
+MlSchemaDef['schema'] = mergeStrings([MlSchemaDef['schema'], service]);
+
+let supportedApi = [
+  {api:'fetchServices', actionName:'READ', moduleName:"OFFICE"},
+  {api:'fetchService', actionName:'READ', moduleName:"OFFICE"},
+
+  {api:'createService', actionName:'CREATE', moduleName:"OFFICE"},
+  {api:'updateService', actionName:'UPDATE', moduleName:"OFFICE"},
+]
+
+MlResolver.MlModuleResolver.push(supportedApi)
--- server/MlExternalUsers/activity/activityResolver.js
+++ server/MlExternalUsers/activity/activityResolver.js
@@ -1,42 +1,42 @@
-// /**
-//  * Created by Mukhil on 14/6/17.
-//  */
-//
-// import MlResolver from '../../commons/mlResolverDef'
-// import MlRespPayload from '../../commons/mlPayload'
-// import MlUserContext from '../../MlExternalUsers/mlUserContext'
-// var extendify = require('extendify');
-// var _ = require('lodash')
-//
-// MlResolver.MlQueryResolver['fetchActivities'] = (obj, args, context, info) => {
-//   let query = {
-//     userId:context.userId  // to do for query
-//   };
-//   let result = mlDBController.find('MlActivity', query , context).fetch()
-//
-// }
-//
-// MlResolver.MlQueryResolver['fetchActivity'] = (obj, args, context, info) => {
-//   let result = mlDBController.findOne('MlActivity', {_id:args.activityId} , context);
-// }
-//
-//
-// MlResolver.MlMutationResolver['createActivity'] = (obj, args, context, info) => {
-//   let result = mlDBController.insert('MlActivity', args.activity , context).fetch()
-//   if(result){
-//     let code = 200;
-//     let result = {userId: userId}
-//     let response = new MlRespPayload().successPayload(result, code);
-//     return response
-//   }
-// }
-//
-// MlResolver.MlMutationResolver['updateActivity'] = (obj, args, context, info) => {
-//   let result = mlDBController.update('MlActivity', {_id:args.activityId}, args.activity, {'$set':1}, context).fetch();
-//   if(result){
-//     let code = 200;
-//     let result = {userId: userId}
-//     let response = new MlRespPayload().successPayload(result, code);
-//     return response
-//   }
-// }
+/**
+ * Created by Mukhil on 14/6/17.
+ */
+
+import MlResolver from '../../commons/mlResolverDef'
+import MlRespPayload from '../../commons/mlPayload'
+import MlUserContext from '../../MlExternalUsers/mlUserContext'
+var extendify = require('extendify');
+var _ = require('lodash')
+
+MlResolver.MlQueryResolver['fetchActivities'] = (obj, args, context, info) => {
+  let query = {
+    userId:context.userId  // to do for query
+  };
+  let result = mlDBController.find('MlActivity', query , context).fetch()
+
+}
+
+MlResolver.MlQueryResolver['fetchActivity'] = (obj, args, context, info) => {
+  let result = mlDBController.findOne('MlActivity', {_id:args.activityId} , context);
+}
+
+
+MlResolver.MlMutationResolver['createActivity'] = (obj, args, context, info) => {
+  let result = mlDBController.insert('MlActivity', args.activity , context).fetch()
+  if(result){
+    let code = 200;
+    let result = {userId: userId}
+    let response = new MlRespPayload().successPayload(result, code);
+    return response
+  }
+}
+
+MlResolver.MlMutationResolver['updateActivity'] = (obj, args, context, info) => {
+  let result = mlDBController.update('MlActivity', {_id:args.activityId}, args.activity, {'$set':1}, context).fetch();
+  if(result){
+    let code = 200;
+    let result = {userId: userId}
+    let response = new MlRespPayload().successPayload(result, code);
+    return response
+  }
+}

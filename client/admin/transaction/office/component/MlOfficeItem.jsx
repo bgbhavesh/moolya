98588835fbbf28520b933700331bcbcdbe34cebe
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-06-17 15:48:07 +0530

    MOOLYA-1932 fix

--- client/admin/transaction/office/component/MlOfficeItem.jsx
+++ client/admin/transaction/office/component/MlOfficeItem.jsx
@@ -202,7 +202,8 @@ export default class MlOfficeItem extends React.Component {
             <div className="row">
               <div className="col-md-6">
                 <div className="form-group">
-                  <input type="text" placeholder="User Id" defaultValue=' ' value={this.state.transInfo.userId} className="form-control float-label" id=""/>
+                  {/*<input type="text" placeholder="User Id" defaultValue=' ' value={this.state.transInfo.userId} className="form-control float-label"/>*/}
+                  <input type="text" placeholder="User Id" defaultValue=' ' value={this.state.userInfo.profileId?this.state.userInfo.profileId:''} className="form-control float-label"/>
                 </div>
                 <div className="form-group">
                   <input type="text" placeholder="Transaction Id" defaultValue=" " value={this.state.transInfo.transactionId} className="form-control float-label" id=""/>

278c4aac7ea9d1b1071377829f50bea9cf81af29
mohasinraksan <mohammed.mohasin@raksan.in>
2017-06-20 01:40:35 +0530

    Action component changes for portfolio view/edit type

--- client/app/commons/components/MlAppPortfolioAccordion.jsx
+++ client/app/commons/components/MlAppPortfolioAccordion.jsx
@@ -1,9 +1,10 @@
 import React, { Component, PropTypes }  from "react";
 import { render } from 'react-dom';
+import _ from 'lodash';
 import MlAppActionComponent from './MlAppActionComponent';
 import MlAccordion from './MlAccordion';
 import MlIdeatorRelatedIdeas from '../../../app/ideators/components/MlAppIdeatorRelatedIdeas';
-
+import {GenericPortfolioActionsConfig} from '../config/mlPortfolioConfig';
 export default class MlAppPortfolioAccordionContainer extends React.Component{
   constructor(props){
     super(props);
@@ -13,27 +14,29 @@ export default class MlAppPortfolioAccordionContainer extends React.Component{
   componentDidMount(){
   }
 
-
   render(){
-    export const genericPortfolioAccordionConfig= {
-      id:'portfolioAccordion',
-      panelItems:[
-        {'title':'Related',isText:true,contentComponent:'upcoming',style:{'background': '#273545'}},
-        {'title':'Actions',isText:false,style:{'background': '#ef4647'},contentComponent:<MlAppActionComponent resourceDetails={{resourceId:this.props.resourceId,resourceType:'portfolio'}} actionOptions={this.props.actionOptions}  />}
-        ]
-    };
 
+    var that=this;
+    var viewMode=this.props.viewMode===false?"edit":"view";
+    //resolve the actions for the portfolio
+    var actionOptions= _.filter(GenericPortfolioActionsConfig,{actionType:viewMode})||[];
+       actionOptions.forEach(function (action) {
+                action.showAction=true;
+                that.props.assignActionHandler(action);
+       });
+
+    //resolve the accordions for the portfolio
+    var accordionOptions=
+       {id:'portfolioAccordion',
+        panelItems:[{'title':'Actions',isText:false,style:{'background': '#ef4647'},contentComponent:<MlAppActionComponent resourceDetails={{resourceId:this.props.resourceId,resourceType:'portfolio'}} actionOptions={actionOptions}/>}]};
     //todo: dynamic check for accordion configuration
        if(this.props.communityType==="Ideators"||this.props.communityType==="ideator"){
-         genericPortfolioAccordionConfig["panelItems"].push({title:'Related Ideas',isText:false,contentComponent:<MlIdeatorRelatedIdeas />,style:{'background': '#273545'}});
+         accordionOptions["panelItems"].push({title:'Related Ideas',isText:false,contentComponent:<MlIdeatorRelatedIdeas />,style:{'background': '#273545'}});
        }
 
     return(
-        <MlAccordion accordionOptions={genericPortfolioAccordionConfig} {...this.props} />
-    )
+        <MlAccordion accordionOptions={accordionOptions} {...this.props} />
+      )
+
   }
 }
-
-MlAppPortfolioAccordionContainer.contextTypes = {
-  userType: PropTypes.string
-};

c0a9ddbaa85059bad112e98dd311a1883ff5ace0
mohasinraksan <mohammed.mohasin@raksan.in>
2017-06-23 23:24:52 +0530

    Implemented Review functionality . changes to schema and resolver

--- server/MlExternalUsers/interactions/reviews/reviewSchema.js
+++ server/MlExternalUsers/interactions/reviews/reviewSchema.js
@@ -7,7 +7,39 @@ import MlSchemaDef from '../../../commons/mlSchemaDef'
 
 let reviewSchema = `
     type Mutation{
-        createReview(resourceId:String!,resourceType:String!,message:String!):response       
+        createReview(resourceId:String!,resourceType:String!,message:String!,rating:Float):response       
+    }
+    
+    type Review {
+     resourceId:String,
+     resourceType:String,
+     rating:Float,
+     message:String,
+     userId:String,
+     userEmail:String,
+     createdOn:Date,
+     updatedOn:Date,
+     updatedBy:Date,
+     status:String,
+     isActive:Boolean,
+     status:Int
+    }
+    
+     type ReviewDetails {
+     reviewId:String,
+     resourceId:String,
+     resourceType:String,
+     rating:Float,
+     message:String,
+     userId:String,
+     userEmail:String,
+     createdOn:Date,
+     userName:String,
+     userProfileImage:String 
+    }
+    
+    type Query{
+        fetchReviews(resourceId:String!,resourceType:String!,cursor:String):[ReviewDetails]
     }
 `
 

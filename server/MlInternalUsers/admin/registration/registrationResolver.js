1ee9883dd0e3717b52948e12d8f63d6b2a221fd5
murali526 <muralidharreddy.kunduru@raksan.in>
2017-06-27 12:34:12 +0530

    website registration issue fixed

--- server/MlInternalUsers/admin/registration/registrationResolver.js
+++ server/MlInternalUsers/admin/registration/registrationResolver.js
@@ -150,7 +150,7 @@ MlResolver.MlMutationResolver['registerAs'] = (obj, args, context, info) => {
 MlResolver.MlMutationResolver['createRegistrationAPI'] = (obj, args, context, info) => {
 
   var response=null;
-  var registrationExist = MlRegistration.findOne({"registrationInfo.email":registrationInfo.email,status: { $nin: [ 'Rejected' ] }})//MlRegistration.findOne({"registrationInfo.email":args.registration.email})
+  var registrationExist = MlRegistration.findOne({"registrationInfo.email":args.registration.email,status: { $nin: [ 'Rejected' ] }})//MlRegistration.findOne({"registrationInfo.email":args.registration.email})
   var userExist = mlDBController.findOne('users', {"profile.email":args.registration.email}, context) || {};
   if(registrationExist || userExist._id){
     let code = 400;

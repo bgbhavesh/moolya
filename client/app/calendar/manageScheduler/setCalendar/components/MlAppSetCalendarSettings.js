597d3492a52c9f77ce6fd897fc3a6a596c95436f
Pankaj <pankajkumar.jatav@raksan.in>
2017-06-29 15:08:58 +0530

    WIP :: Set Calendar Settings

--- client/app/calendar/manageScheduler/setCalendar/components/MlAppSetCalendarSettings.js
+++ client/app/calendar/manageScheduler/setCalendar/components/MlAppSetCalendarSettings.js
@@ -1,9 +1,11 @@
 /**
  * Created by pankaj on 28/6/17.
  */
+
 import React from 'react';
 import MlAppScheduleHead from "../../commons/components/MlAppScheduleHead";
 import {fetchCalendarSettingsActionHandler} from '../actions/fetchCalendarSettings';
+import StepZilla from "../../../../../commons/components/stepzilla/StepZilla";
 import MlAppSetCalendarPrimarySettings from './MlAppSetCalendarPrimarySettings';
 import MlAppSetCalendarTimmingSettings from './MlAppSetCalendarTimmingSettings';
 
@@ -43,15 +45,25 @@ export default class MlAppSetCalendarSettings extends React.Component {
   }
 
   render(){
+    const steps = [
+        {name: 'Primary', component: <MlAppSetCalendarPrimarySettings primarySettings={this.state.primarySettings} />,icon:<span className="ml fa fa-file-text-o "></span>},
+        {name: 'Manage', component: <MlAppSetCalendarTimmingSettings />,icon:<span className="ml fa fa-calendar-plus-o"></span>},
+        // {name: 'Vacation', component: <MlAppSetCalendarTimmingSettings />,icon:<span className="ml fa fa-plane"></span>},
+      ]
     return (
       <div className="app_main_wrap" style={{'overflow':'auto'}}>
         <div className="app_padding_wrap">
           <MlAppScheduleHead type="calendar"/>
-          <MlAppSetCalendarPrimarySettings primarySettings={this.state.primarySettings} />
-          {/*<MlAppSetCalendarTimmingSettings />*/}
+          <div className="clearfix" />
+          <div className="col-md-12">
+            <div className='step-progress' >
+              <div id="root" >
+                <StepZilla steps={steps} stepsNavigation={true} prevBtnOnLastStep={false} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
   }
-
 }

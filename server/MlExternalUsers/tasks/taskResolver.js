e26e4ed517c1df378f2aba4137a854cc06b5d9e4
Pankaj <pankajkumar.jatav@raksan.in>
2017-07-08 15:57:34 +0530

    FIXED :: MOOLYA-2381

--- server/MlExternalUsers/tasks/taskResolver.js
+++ server/MlExternalUsers/tasks/taskResolver.js
@@ -70,11 +70,11 @@ MlResolver.MlMutationResolver['updateTask'] = (obj, args, context, info) => {
           let details = mlDBController.find('MlActivity',{_id:{$in:item}}, context).fetch()
           let amount = _.map(details, 'payment.amount');
           _.each(amount, function (i,s) {
-            activitiesAmount.push(i)
+            activitiesAmount.push(i ? i : 0);
           });
           let derivedAmount = _.map(details, 'payment.derivedAmount');
           _.each(derivedAmount, function (i,s) {
-            activitiesDerivedAmount.push(i)
+            activitiesDerivedAmount.push(i ? i : 0);
           });
         });
         let finalActivitiesAmount = _.sum(activitiesAmount);

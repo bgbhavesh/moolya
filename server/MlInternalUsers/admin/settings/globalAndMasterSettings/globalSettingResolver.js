4ae1afcd8dd99eef04e76b4dae8d399f54ade29b
sravani_sistla <sravani.sistla@raksan.in>
2017-07-12 16:06:14 +0530

    Moolya-2103,472,477 Issues Fixed

--- server/MlInternalUsers/admin/settings/globalAndMasterSettings/globalSettingResolver.js
+++ server/MlInternalUsers/admin/settings/globalAndMasterSettings/globalSettingResolver.js
@@ -10,8 +10,16 @@ MlResolver.MlQueryResolver['fetchGlobalSettings'] = (obj, args, context, info) =
   // TODO : Authorization
   let userProfile = new MlAdminUserContext().userProfileDetails(context.userId) || {};
   let clusterId = userProfile && userProfile.defaultProfileHierarchyRefId ? userProfile.defaultProfileHierarchyRefId : '';
-     // return MlGlobalSettings.find({}).fetch();
-     return mlDBController.find('MlGlobalSettings', {hierarchyRefId:clusterId}, context).fetch();
+   let result = mlDBController.find('MlGlobalSettings', {hierarchyRefId:clusterId}, context).fetch();
+   // return MlGlobalSettings.find({}).fetch();
+  let cluster = mlDBController.findOne("MlClusters", {"_id": clusterId}, context)
+  if(cluster){
+    let countryCode = cluster.clusterCode
+    let countryCapital = mlDBController.findOne("MlCapital", {"countryCode": countryCode}, context)
+    result[0].regionalInfo.capitalName = countryCapital.capital
+    result[0].regionalInfo.regionalFlag = cluster.countryFlag
+  }
+     return result;
 }
 
 
@@ -49,7 +57,64 @@ MlResolver.MlQueryResolver['findTimeZones'] = (obj, args, context, info) => {
 
   return [];
 }
+MlResolver.MlQueryResolver['findRounding'] = (obj, args, context, info) => {
+  let userProfile = new MlAdminUserContext().userProfileDetails(context.userId) || {};
+  let clusterId = userProfile && userProfile.defaultProfileHierarchyRefId ? userProfile.defaultProfileHierarchyRefId : '';
+  if (clusterId) {
+      let cluster = mlDBController.findOne("MlClusters", {"_id": clusterId}, context)
+      if(cluster && cluster.clusterCode){
+        let roundings = mlDBController.find("MlCurrencyType", {"countryCode": cluster.clusterCode}, context).fetch();
+        if (roundings) {
+          let code = 200;
+          let response = new MlRespPayload().successPayload('', code);
+          return roundings
+        }
+      }
+  }
+}
+MlResolver.MlQueryResolver['findCurrencyNames'] = (obj, args, context, info) => {
+  let userProfile = new MlAdminUserContext().userProfileDetails(context.userId) || {};
+  let clusterId = userProfile && userProfile.defaultProfileHierarchyRefId ? userProfile.defaultProfileHierarchyRefId : '';
+  if (clusterId) {
+    let cluster = mlDBController.findOne("MlClusters", {"_id": clusterId}, context)
+    if(cluster && cluster.clusterCode){
+      let currencyName = mlDBController.find("MlCurrencyType", {"countryCode": cluster.clusterCode}, context).fetch();
+      if (currencyName) {
+        let code = 200;
+        let result = []
+        let response = new MlRespPayload().successPayload('', code);
+        currencyName.map(function (currency) {
+          let json={
+            regionalCurrencyName : currency.currencyName,
+            _id : currency._id
+          }
+          result.push(json)
+        })
+
+        return result
+      }
+    }
+  }
+}
+
 MlResolver.MlQueryResolver['findLanguages'] = (obj, args, context, info) => {
-  let languages = mlDBController.find("MlLanguages", {}, context).fetch();
-  return languages;
+  let userProfile = new MlAdminUserContext().userProfileDetails(context.userId) || {};
+  let clusterId = userProfile && userProfile.defaultProfileHierarchyRefId ? userProfile.defaultProfileHierarchyRefId : '';
+  if (clusterId) {
+    let languages = mlDBController.find("MlMasterSettings", {"hierarchyRefId": clusterId}, context).fetch();
+    if(languages){
+      let response = []
+      // data = languages.languageInfo
+      languages.map(function (lang) {
+        if(lang.languageInfo){
+          let json = {
+            lang_code : lang.languageInfo.languageName,
+            language_name : lang.languageInfo.languageName
+          }
+          response.push(json)
+        }
+      })
+      return response;
+    }
+  }
 }

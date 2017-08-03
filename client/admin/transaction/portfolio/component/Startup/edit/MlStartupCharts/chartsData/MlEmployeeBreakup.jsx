f4d0bffc986c241cad54645c8852dc3285d57bda
sireesha <sireesha.dhavala@raksanconsulting.com>
2017-08-03 19:04:55 +0530

    FIXED::MOOLYA-2737

--- client/admin/transaction/portfolio/component/Startup/edit/MlStartupCharts/chartsData/MlEmployeeBreakup.jsx
+++ client/admin/transaction/portfolio/component/Startup/edit/MlStartupCharts/chartsData/MlEmployeeBreakup.jsx
@@ -179,123 +179,133 @@ export default class MlEmployeeBreakup extends React.Component{
           smoothScrolling={true}
           default={true}
         >
-      <div className="office-members-detail">
+        <div className="panel panel-default">
+          <div className="panel-heading">Employee Breakup{
+            <div className="pull-right block_action" onClick={this.onSaveAction.bind(this,defaultIndex)}><img
+            src="/images/add.png"/></div>}
 
-        <div className="form_inner_block">
-          <div className="add_form_block" onClick={this.onSaveAction.bind(this,defaultIndex)}><img src="/images/add.png"/></div>
+          </div>
+          <div className="panel-body">
+            <div className="office-members-detail">
 
-          <div className="col-lg-12 col-md-12 col-sm-10">
-            <div className="row">
-              <div className="form-group col-lg-6 col-md-6 col-sm-6">
-                    <Moolyaselect multiSelect={false} placeholder="Select Department"
-                 className="form-control float-label" valueKey={'value'} labelKey={'label'}
-                 selectedValue={this.state.selectedVal} queryType={"graphql"}
-                 query={departmentQuery} onSelect={this.optionsBySelectTypeOfDepartment.bind(this,defaultIndex)}
-                 isDynamic={true}/>
+            <div className="form_inner_block">
+              {/*<div className="add_form_block" onClick={this.onSaveAction.bind(this,defaultIndex)}><img src="/images/add.png"/></div>*/}
 
-              </div>
-              <div className="form-group col-lg-6">
-                <div className="form-group col-md-6 col-sm-6">
-                  <Datetime dateFormat="YYYY" timeFormat={false} viewMode="years"
-                            inputProps={{placeholder: "Select From Year", className:"float-label form-control"}}
-                            closeOnSelect={true} ref={"ebdFromYear"+defaultIndex} onBlur={this.handleFromYearChange.bind(this,defaultIndex)}/>
+              <div className="col-lg-12 col-md-12 col-sm-10">
+                <div className="row">
+                  <div className="form-group col-lg-6 col-md-6 col-sm-6">
+                        <Moolyaselect multiSelect={false} placeholder="Select Department"
+                     className="form-control float-label" valueKey={'value'} labelKey={'label'}
+                     selectedValue={this.state.selectedVal} queryType={"graphql"}
+                     query={departmentQuery} onSelect={this.optionsBySelectTypeOfDepartment.bind(this,defaultIndex)}
+                     isDynamic={true}/>
+
+                  </div>
+                  <div className="form-group col-lg-6">
+                    <div className="form-group col-md-6 col-sm-6">
+                      <Datetime dateFormat="YYYY" timeFormat={false} viewMode="years"
+                                inputProps={{placeholder: "Select From Year", className:"float-label form-control"}}
+                                closeOnSelect={true} ref={"ebdFromYear"+defaultIndex} onBlur={this.handleFromYearChange.bind(this,defaultIndex)}/>
+                    </div>
+                    <div className="form-group col-md-6 col-sm-6">
+                      <Datetime dateFormat="MMMM" timeFormat={false} viewMode="months"
+                                inputProps={{placeholder: "Select From Month", className:"float-label form-control"}}
+                                closeOnSelect={true} ref={"ebdFromMonth"+defaultIndex} onBlur={this.handleFromMonthChange.bind(this,defaultIndex)}/>
+                    </div>
+
+                  </div>
+
+                  <div className="form-group col-lg-6">
+                    <div className="form-group col-md-6 col-sm-6">
+                      <Datetime dateFormat="YYYY" timeFormat={false} viewMode="years"
+                                inputProps={{placeholder: "Select To Year", className:"float-label form-control"}}
+                                closeOnSelect={true} ref={"ebdToYear"+defaultIndex} onBlur={this.handleToYearChange.bind(this,defaultIndex)}/>
+                    </div>
+                    <div className="form-group col-md-6 col-sm-6">
+                      <Datetime dateFormat="MMMM" timeFormat={false} viewMode="months"
+                                inputProps={{placeholder: "Select To Month", className:"float-label form-control"}}
+                                closeOnSelect={true} ref={"ebdToMonth"+defaultIndex} onBlur={this.handleToMonthChange.bind(this,defaultIndex)}/>
+                    </div>
+                  </div>
+
+                  <div className="form-group col-lg-6 col-md-6 col-sm-6">
+                    <input type="text" placeholder="Number of Employment" ref={"ebdNumberOfEmployment"+defaultIndex} className="form-control float-label"
+                           id="" name="ebdNumberOfEmployment" onBlur={this.employeementHandleBlur.bind(this,defaultIndex)}/>
+                  </div>
+
+                  <div className="form-group col-lg-6 col-md-6 col-sm-6">
+                  <textarea rows="1" placeholder="About" ref={"ebdAbout"+defaultIndex} className="form-control float-label"
+                            id="" name="ebdAbout" onBlur={this.aboutHandleBlur.bind(this,defaultIndex)}></textarea>
+                  </div>
                 </div>
-                <div className="form-group col-md-6 col-sm-6">
-                  <Datetime dateFormat="MMMM" timeFormat={false} viewMode="months"
-                            inputProps={{placeholder: "Select From Month", className:"float-label form-control"}}
-                            closeOnSelect={true} ref={"ebdFromMonth"+defaultIndex} onBlur={this.handleFromMonthChange.bind(this,defaultIndex)}/>
-                </div>
-
-              </div>
-
-              <div className="form-group col-lg-6">
-                <div className="form-group col-md-6 col-sm-6">
-                  <Datetime dateFormat="YYYY" timeFormat={false} viewMode="years"
-                            inputProps={{placeholder: "Select To Year", className:"float-label form-control"}}
-                            closeOnSelect={true} ref={"ebdToYear"+defaultIndex} onBlur={this.handleToYearChange.bind(this,defaultIndex)}/>
-                </div>
-                <div className="form-group col-md-6 col-sm-6">
-                  <Datetime dateFormat="MMMM" timeFormat={false} viewMode="months"
-                            inputProps={{placeholder: "Select To Month", className:"float-label form-control"}}
-                            closeOnSelect={true} ref={"ebdToMonth"+defaultIndex} onBlur={this.handleToMonthChange.bind(this,defaultIndex)}/>
-                </div>
-              </div>
-
-              <div className="form-group col-lg-6 col-md-6 col-sm-6">
-                <input type="text" placeholder="Number of Employment" ref={"ebdNumberOfEmployment"+defaultIndex} className="form-control float-label"
-                       id="" name="ebdNumberOfEmployment" onBlur={this.employeementHandleBlur.bind(this,defaultIndex)}/>
-              </div>
-
-              <div className="form-group col-lg-6 col-md-6 col-sm-6">
-              <textarea rows="1" placeholder="About" ref={"ebdAbout"+defaultIndex} className="form-control float-label"
-                        id="" name="ebdAbout" onBlur={this.aboutHandleBlur.bind(this,defaultIndex)}></textarea>
               </div>
             </div>
+            {that.state.dataList.map(function (details, idx) {
+
+
+              return(<div className="form_inner_block">
+
+                {/*<div className="add_form_block" onClick={that.onRemoveAction.bind(that,idx)}><img src="/images/remove.png"/></div>*/}
+                <div className="col-lg-12 col-md-12 col-sm-10">
+                  <div className="row">
+                    <div className="form-group col-lg-6 col-md-6 col-sm-6">
+                      <Moolyaselect multiSelect={false} placeholder="Select Type Of Entity"
+                                    className="form-control float-label" valueKey={'value'} labelKey={'label'}
+                                    selectedValue={details.ebdDepartment} queryType={"graphql"}
+                                    query={departmentQuery} onSelect={that.optionsBySelectTypeOfDepartment.bind(that,idx)}
+                                    isDynamic={true}/>
+
+                    </div>
+                    <div className="form-group col-lg-6">
+                      <div className="form-group col-md-6 col-sm-6">
+                        <Datetime dateFormat="YYYY" timeFormat={false} viewMode="years" defaultValue={details.ebdFromYear}
+                                  inputProps={{placeholder: "Select Year", className: "float-label form-control"}}
+
+                                  closeOnSelect={true} ref={"ebdFromYear"+idx} onBlur={that.handleFromYearChange.bind(that, idx)}/>
+                      </div>
+                      <div className="form-group col-md-6 col-sm-6">
+                        <Datetime dateFormat="MMMM" timeFormat={false} viewMode="months" defaultValue={details.ebdFromMonth}
+                                  inputProps={{placeholder: "Select Year", className: "float-label form-control"}}
+
+                                  closeOnSelect={true} ref={"ebdFromMonth"+idx} onBlur={that.handleFromMonthChange.bind(that, idx)}/>
+                      </div>
+
+                    </div>
+
+
+                    <div className="form-group col-lg-6">
+                      <div className="form-group col-md-6 col-sm-6">
+                        <Datetime dateFormat="YYYY" timeFormat={false} viewMode="years" defaultValue={details.ebdToYear}
+                                  inputProps={{placeholder: "Select Year", className: "float-label form-control"}}
+
+                                  closeOnSelect={true} ref={"ebdToYear"+idx} onBlur={that.handleToYearChange.bind(that, idx)}/>
+                      </div>
+                      <div className="form-group col-md-6 col-sm-6">
+                        <Datetime dateFormat="MMMM" timeFormat={false} viewMode="months" defaultValue={details.ebdToMonth}
+                                  inputProps={{placeholder: "Select Year", className: "float-label form-control"}}
+
+                                  closeOnSelect={true} ref={"ebdToMonth"+idx} onBlur={that.handleToMonthChange.bind(that, idx)}/>
+                      </div>
+                    </div>
+                    <div className="form-group col-lg-6 col-md-6 col-sm-6">
+                      <input type="text" placeholder="Number of Employment" ref={"ebdNumberOfEmployment"+idx}
+                             className="form-control float-label" id="" defaultValue={details.ebdNumberOfEmployment} name="ebdNumberOfEmployment" onBlur={that.employeementHandleBlur.bind(that,idx)}/>
+                    </div>
+
+                    <div className="form-group col-lg-6 col-md-6 col-sm-6">
+                    <textarea rows="1" placeholder="About" ref={"ebdAbout"+idx} className="form-control float-label"
+                              id="" name="ebdAbout"   defaultValue={details.ebdAbout} onBlur={that.aboutHandleBlur.bind(that,idx)}></textarea>
+                    </div>
+                  </div>
+                </div>
+
+              </div>)
+
+            })}
+
+          </div>
           </div>
         </div>
-        {that.state.dataList.map(function (details, idx) {
-
-
-          return(<div className="form_inner_block">
-
-            {/*<div className="add_form_block" onClick={that.onRemoveAction.bind(that,idx)}><img src="/images/remove.png"/></div>*/}
-            <div className="col-lg-12 col-md-12 col-sm-10">
-              <div className="row">
-                <div className="form-group col-lg-6 col-md-6 col-sm-6">
-                  <Moolyaselect multiSelect={false} placeholder="Select Type Of Entity"
-                                className="form-control float-label" valueKey={'value'} labelKey={'label'}
-                                selectedValue={details.ebdDepartment} queryType={"graphql"}
-                                query={departmentQuery} onSelect={that.optionsBySelectTypeOfDepartment.bind(that,idx)}
-                                isDynamic={true}/>
-
-                </div>
-                <div className="form-group col-lg-6">
-                  <div className="form-group col-md-6 col-sm-6">
-                    <Datetime dateFormat="YYYY" timeFormat={false} viewMode="years" defaultValue={details.ebdFromYear}
-                              inputProps={{placeholder: "Select Year", className: "float-label form-control"}}
-
-                              closeOnSelect={true} ref={"ebdFromYear"+idx} onBlur={that.handleFromYearChange.bind(that, idx)}/>
-                  </div>
-                  <div className="form-group col-md-6 col-sm-6">
-                    <Datetime dateFormat="MMMM" timeFormat={false} viewMode="months" defaultValue={details.ebdFromMonth}
-                              inputProps={{placeholder: "Select Year", className: "float-label form-control"}}
-
-                              closeOnSelect={true} ref={"ebdFromMonth"+idx} onBlur={that.handleFromMonthChange.bind(that, idx)}/>
-                  </div>
-
-                </div>
-
-
-                <div className="form-group col-lg-6">
-                  <div className="form-group col-md-6 col-sm-6">
-                    <Datetime dateFormat="YYYY" timeFormat={false} viewMode="years" defaultValue={details.ebdToYear}
-                              inputProps={{placeholder: "Select Year", className: "float-label form-control"}}
-
-                              closeOnSelect={true} ref={"ebdToYear"+idx} onBlur={that.handleToYearChange.bind(that, idx)}/>
-                  </div>
-                  <div className="form-group col-md-6 col-sm-6">
-                    <Datetime dateFormat="MMMM" timeFormat={false} viewMode="months" defaultValue={details.ebdToMonth}
-                              inputProps={{placeholder: "Select Year", className: "float-label form-control"}}
-
-                              closeOnSelect={true} ref={"ebdToMonth"+idx} onBlur={that.handleToMonthChange.bind(that, idx)}/>
-                  </div>
-                </div>
-                <div className="form-group col-lg-6 col-md-6 col-sm-6">
-                  <input type="text" placeholder="Number of Employment" ref={"ebdNumberOfEmployment"+idx}
-                         className="form-control float-label" id="" defaultValue={details.ebdNumberOfEmployment} name="ebdNumberOfEmployment" onBlur={that.employeementHandleBlur.bind(that,idx)}/>
-                </div>
-
-                <div className="form-group col-lg-6 col-md-6 col-sm-6">
-                <textarea rows="1" placeholder="About" ref={"ebdAbout"+idx} className="form-control float-label"
-                          id="" name="ebdAbout"   defaultValue={details.ebdAbout} onBlur={that.aboutHandleBlur.bind(that,idx)}></textarea>
-                </div>
-              </div>
-            </div>
-
-          </div>)
-
-        })}
-      </div>
 
         </ScrollArea>
       </div>

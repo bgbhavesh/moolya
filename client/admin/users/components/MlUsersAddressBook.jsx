0336b8ea044e1add0dbac1d6c64685640e1af5ea
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-08-04 13:36:54 +0530

    Admin -> Users -> ADDRESS BOOK IMPLEMENTATION

--- client/admin/users/components/MlUsersAddressBook.jsx
+++ client/admin/users/components/MlUsersAddressBook.jsx
@@ -1,19 +1,99 @@
 /**
- * Created by vishwadeep on 26/7/17.
+ * Created by Rajat
  */
-import React, {Component} from "react";
 
-export default class MlUsersAddressBook extends Component {
-  constructor(props, context) {
+import React from "react";
+import {Meteor} from "meteor/meteor";
+import {render} from "react-dom";
+import ScrollArea from "react-scrollbar";
+
+import AppContactDetails from "./MlUserContactDetails";
+import AppAddressDetails from "./MlUserAddressDetails";
+import AppEmailDetails from "./MlUserEmailDetails";
+import {findAddressBookActionHandler} from '../actions/findUserAddressBookHandler'
+import MlLoader from '../../../commons/components/loader/loader'
+
+export default class MlUsersAddressBook extends React.Component {
+  constructor(props) {
     super(props);
+    this.state = {
+      loading: true
+    }
+    this.findAddressBook.bind(this);
     return this;
   }
 
-  render() {
-    return (
-      <div className="row">
-        <h2> Address Book Landing </h2>
-      </div>
-    );
+  componentWillMount() {
+    const resp=this.findAddressBook();
+    return resp;
   }
-}
+
+  async findAddressBook(){
+    const response = await findAddressBookActionHandler(this.props.config.registrationId);
+    if(response){
+      this.setState({data:response, loading:false});
+    }else {
+      this.setState({loading:false, data:null})
+    }
+  }
+
+  componentDidMount() {
+    var WinHeight = $(window).height();
+    $('.left_wrap').height(WinHeight - (160 + $('.admin_header').outerHeight(true)));
+  }
+
+  render() {
+    const showLoader=this.state.loading;
+    let clusterId=this.state.data?this.state.data.clusterId:'';
+    let contactInfo= this.state.data?this.state.data.contactInfo:''
+    let emailInfo= this.state.data?this.state.data.emailInfo:''
+    let addressInfo = this.state.data?this.state.data.addressInfo:''
+    let registerId = this.state.data?this.state.data.registrationId:''
+    let profileId = this.state.data?this.state.data.profileId:''
+    return (
+      <div className="admin_main_wrap">
+        {showLoader === true ? (<MlLoader/>) : (
+          <div className="admin_padding_wrap">
+            <h2>Address Book</h2>
+            <div className="main_wrap_scroll">
+              <ScrollArea
+                speed={0.8}
+                className="main_wrap_scroll"
+                smoothScrolling={true}
+                default={true}
+              >
+                <div className="row">
+                  <div className="col-lg-6 ">
+                    <form>
+                      <div className="panel panel-default new_profile_tabs">
+                        <div className="panel-heading">
+                          Contact Number
+                        </div>
+                        <AppContactDetails clusterId={clusterId} contactInfoDetails={contactInfo}  registerId = {registerId} profileId={profileId} registrationDetails={this.findAddressBook.bind(this)}/>
+                      </div>
+                      <div className="panel panel-default new_profile_tabs">
+                        <div className="panel-heading">
+                          Email ID
+                        </div>
+                        <AppEmailDetails clusterId={clusterId} emailInfoDetails={emailInfo}  registerId = {registerId} profileId={profileId} registrationDetails={this.findAddressBook.bind(this)}/>
+                      </div>
+                    </form>
+                  </div>
+                  <div className="col-lg-6 ">
+                    <form>
+                      <div className="panel panel-default new_profile_tabs">
+                        <div className="panel-heading">
+                          Address
+                        </div>
+                        <AppAddressDetails clusterId={clusterId} addressInfoDetails={addressInfo} registerId = {registerId} profileId={profileId} registrationDetails={this.findAddressBook.bind(this)}/>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </ScrollArea>
+            </div>
+          </div>)}
+      </div>
+    )
+  }
+};

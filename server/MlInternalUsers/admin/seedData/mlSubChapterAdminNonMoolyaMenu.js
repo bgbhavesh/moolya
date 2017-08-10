1dda759687f78cac49cde0610c26363ad7fe7f73
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-08-10 17:31:43 +0530

    fixed:: MOOLYA-2813, MOOLYA-2803

--- server/MlInternalUsers/admin/seedData/mlSubChapterAdminNonMoolyaMenu.js
+++ server/MlInternalUsers/admin/seedData/mlSubChapterAdminNonMoolyaMenu.js
@@ -1010,17 +1010,6 @@ if (Meteor.isServer) {
                   "subMenusId": "portfolio",
                   "subMenuMappingId": "portfolio_approved"
                 },
-
-                {
-                  "link": "/admin/transactions/portfolio/createPortfolio",
-                  "name": "Create",
-                  "uniqueId": "portfolio_create",
-                  "isLink": true,
-                  "isMenu": true,
-                  "image": "",
-                  "subMenusId": "portfolio",
-                  //"subMenuMappingId":"transaction_registration_create"
-                },
                 {
                   "link": "/admin/transactions/portfolio/history",
                   "name": "History",

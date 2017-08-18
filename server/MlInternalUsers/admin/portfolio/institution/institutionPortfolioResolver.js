fbae131d0e9a876e68fad6f8f3745b7cc1316879
mohasinraksan <mohammed.mohasin@raksan.in>
2017-08-18 17:44:46 +0530

    Institution Portfolio schema/resolver added

---
+++ server/MlInternalUsers/admin/portfolio/institution/institutionPortfolioResolver.js
@@ -0,0 +1,209 @@
+import MlResolver from "../../../../commons/mlResolverDef";
+import MlRespPayload from "../../../../commons/mlPayload";
+import MlEmailNotification from "../../../../mlNotifications/mlEmailNotifications/mlEMailNotification";
+import MlAlertNotification from '../../../../mlNotifications/mlAlertNotifications/mlAlertNotification'
+var _ = require('lodash')
+var extendify = require('extendify');
+
+MlResolver.MlMutationResolver['createInstitutePortfolio'] = (obj, args, context, info) => {
+  try {
+    if (args && args.userId && args.communityType) {
+      user = MlInstitutionPortfolio.findOne({"$and": [{'userId': args.userId}, {'communityId': args.communityType}]})
+      if (!user) {
+        mlDBController.insert('MlInstitutionPortfolio', {
+          userId: args.userId,
+          communityType: args.communityType,
+          portfolioDetailsId: args.portfolioDetailsId
+        }, context)
+      }
+    }
+  } catch (e) {
+    console.log("Error: In creating Institute portfolio");
+  }
+}
+
+MlResolver.MlMutationResolver['updateInstitutePortfolio'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId)
+  {
+    try {
+      let institutePortfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+      let updateFor = args.portfolio;
+      if (institutePortfolio){
+        for (key in updateFor) {
+          if (institutePortfolio.hasOwnProperty(key)){
+            if(_.isArray(updateFor[key]) && _.isArray(institutePortfolio[key])){
+              institutePortfolio[key] = updateArrayofObjects(updateFor[key], institutePortfolio[key])
+            }
+            else if(_.isObject(updateFor[key]) && _.isObject(institutePortfolio[key]))
+            {
+              _.mergeWith(institutePortfolio[key], updateFor[key], function (objValue, srcValue) {
+                if (_.isArray(objValue)) {
+                  return objValue.concat(srcValue);
+                }
+              });
+            }
+          }
+          else{
+            institutePortfolio[key] = updateFor[key]
+          }
+        }
+
+        let ret = mlDBController.update('MlInstitutionPortfolio', {"portfolioDetailsId": args.portfoliodetailsId}, institutePortfolio, {$set: true}, context)
+        if (ret) {
+          let details = MlPortfolioDetails.findOne({"_id":args.portfoliodetailsId})
+          MlEmailNotification.onPortfolioUpdate(details);
+          let institutealert =  MlAlertNotification.onPortfolioUpdates()
+          let code = 200;
+          let response = new MlRespPayload().successPayload(institutealert, code);
+          return response;
+        }
+      }
+      else{
+        let code = 400;
+        let response = new MlRespPayload().errorPayload("Invalid Portfolio Request", code);
+        return response;
+      }
+    }
+    catch (e) {
+      let code = 400;
+      let response = new MlRespPayload().errorPayload(e.message, code);
+      return response;
+    }
+  }
+}
+
+
+MlResolver.MlQueryResolver['fetchInstitutePortfolioManagement'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId) {
+    let portfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+    if (portfolio && portfolio.hasOwnProperty('management')) {
+      return portfolio['management'];
+    }
+  }
+  return [];
+}
+
+MlResolver.MlQueryResolver['fetchInstitutePortfolioAboutUs'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId) {
+    let instituteAboutUsArray = {}
+    let portfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+    instituteAboutUsArray["aboutUs"] = portfolio&&portfolio.aboutUs?portfolio.aboutUs:{};
+    instituteAboutUsArray["clients"] = portfolio&&portfolio.clients?portfolio.clients:[];
+    instituteAboutUsArray["serviceProducts"] = portfolio&&portfolio.serviceProducts?portfolio.serviceProducts:{};
+    instituteAboutUsArray["information"] = portfolio&&portfolio.information?portfolio.information:{};
+    instituteAboutUsArray["rating"] = portfolio&&portfolio.rating?portfolio.rating:null;
+
+
+    //for view action
+    MlResolver.MlMutationResolver['createView'](obj,{resourceId:args.portfoliodetailsId,resourceType:'portfolio'}, context, info);
+
+    if (instituteAboutUsArray) {
+      return instituteAboutUsArray
+    }
+
+  }
+
+  return {};
+}
+
+MlResolver.MlQueryResolver['fetchInstitutePortfolioAwards'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId) {
+    let portfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+    if (portfolio && portfolio.hasOwnProperty('awardsRecognition')) {
+      if(portfolio && portfolio['awardsRecognition']){
+        portfolio.awardsRecognition.map(function(awards,index) {
+          if(portfolio.awardsRecognition[index]){
+            let awardData = MlAwards.findOne({"_id" : awards.awardId}) || {};
+            portfolio.awardsRecognition[index].awardName = awardData.awardDisplayName || "";
+          }
+
+        })
+      }
+      return portfolio['awardsRecognition'];
+    }
+  }
+
+  return [];
+}
+
+MlResolver.MlQueryResolver['fetchInstitutePortfolioMemberships'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId) {
+    let portfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+    if (portfolio && portfolio.hasOwnProperty('memberships')) {
+      return portfolio['memberships'];
+    }
+  }
+
+  return {};
+}
+
+MlResolver.MlQueryResolver['fetchInstitutePortfolioCompliances'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId) {
+    let portfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+    if (portfolio && portfolio.hasOwnProperty('compliances')) {
+      return portfolio['compliances'];
+    }
+  }
+
+  return {};
+}
+
+MlResolver.MlQueryResolver['fetchInstitutePortfolioLicenses'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId) {
+    let portfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+    if (portfolio && portfolio.hasOwnProperty('licenses')) {
+      return portfolio['licenses'];
+    }
+  }
+
+  return {};
+}
+
+MlResolver.MlQueryResolver['fetchInstitutePortfolioData'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId) {
+    let portfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+    if (portfolio && portfolio.hasOwnProperty('data')) {
+      return portfolio['data'];
+    }
+  }
+  return {};
+}
+
+MlResolver.MlQueryResolver['fetchInstitutePortfolioCharts'] = (obj, args, context, info) => {
+  if (args.portfoliodetailsId) {
+    let instituteChartsArray = {}
+    let portfolio = MlInstitutionPortfolio.findOne({"portfolioDetailsId": args.portfoliodetailsId})
+    instituteChartsArray["employmentOfCompanyChart"] = portfolio&&portfolio.employmentOfCompanyChart?portfolio.employmentOfCompanyChart:[];
+    instituteChartsArray["profitRevenueLiabilityChart"] = portfolio&&portfolio.profitRevenueLiabilityChart?portfolio.profitRevenueLiabilityChart:[];
+    instituteChartsArray["reviewOfCompanyChart"] = portfolio&&portfolio.reviewOfCompanyChart?portfolio.reviewOfCompanyChart:[];
+    instituteChartsArray["employeeBreakupDepartmentChart"] = portfolio&&portfolio.employeeBreakupDepartmentChart?portfolio.employeeBreakupDepartmentChart:[];
+    if(instituteChartsArray && instituteChartsArray.employeeBreakupDepartmentChart){
+      instituteChartsArray.employeeBreakupDepartmentChart.map(function(data,index) {
+        if(instituteChartsArray.employeeBreakupDepartmentChart[index]){
+          let entityData = MlDepartments.findOne({"_id":data.ebdDepartment}) || {};
+          instituteChartsArray.employeeBreakupDepartmentChart[index].ebdDepartmentName = entityData.displayName || "";
+        }
+
+      })
+    }
+    if (instituteChartsArray) {
+      return instituteChartsArray
+    }
+  }
+}
+
+updateArrayofObjects = (updateFor, source) =>{
+  if(_.isArray(updateFor) && _.isArray(source)){
+    _.each(updateFor, function (obj) {
+      let isObj = _.find(source, {index:obj.index})
+      let itemIndex = _.findIndex(source, {index:obj.index})
+      if(isObj &&  itemIndex >= 0){
+        _.mergeWith(source[itemIndex], obj)
+      }
+      else{
+        source.push(obj)
+      }
+    })
+  }
+  return source;
+}
---
+++ server/MlInternalUsers/admin/portfolio/institution/institutionPortfolioSchema.js
@@ -0,0 +1,434 @@
+/**
+ * Created by venkatasrinag on 3/4/17.
+ */
+import {mergeStrings} from 'gql-merge';
+import MlSchemaDef from '../../../../commons/mlSchemaDef';
+import MlResolver from '../../../../commons/mlResolverDef'
+
+
+let institutePortfolioSchema = `
+    type imagesTypeSchema{
+        fileUrl   : String,
+        fileName  : String,
+        isDefault : Boolean
+    }
+    
+    type instituteManagementOutput{
+       title :String
+       isTitlePrivate : Boolean
+       firstName : String
+       isFirstNamePrivate : Boolean
+       lastName : String
+       isLastNamePrivate : Boolean
+       middleName : String
+       isMiddleNamePrivate : Boolean
+       qualification : String
+       isQualificationPrivate :  Boolean
+       certification : String
+       isCertificationPrivate : Boolean
+       profilePic : String
+       isProfilePicPrivate : Boolean
+       gender : String
+       isGenderPrivate : Boolean
+       designation : String
+       isDesignationPrivate: Boolean
+       yearsOfExperience : String
+       isYOEPrivate : Boolean
+       joiningDate : String
+       isJoiningDatePrivate : Boolean
+       firstJobJoiningDate : String
+       isFJJDPrivate : Boolean
+       universities :   String
+       isUniversitiesPrivate: Boolean
+       awards : String
+       isAwardsPrivate : Boolean
+       linkedInUrl : String
+       isLinkedInUrlPrivate : Boolean
+       about : String
+       isAboutPrivate:Boolean
+       logo:imagesTypeSchema,
+       index: Int
+    }
+   
+    
+    type clientsOutput{
+        companyName:String,
+        isCompanyNamePrivate:Boolean,
+        logo:imagesTypeSchema,
+        description:String,
+        isDescriptionPrivate:Boolean,
+        makePrivate:Boolean,
+        index:Int,
+    }  
+    
+
+    type informationOutput{
+       description:String,
+       isDescriptionPrivate:Boolean
+    }
+    
+    type serviceProductsOutput{
+        description:String,
+        isDescriptionPrivate:Boolean
+    }
+    
+    type imageFilesInputSchemaOutput{
+       fileUrl: String,
+       fileName:String
+     }   
+    
+    type aboutUsOutput{
+        aboutImages      : [imagesTypeSchema]
+        aboutTitle : String
+        aboutDescription: String
+        annotatorId : String
+        isLogoPrivate :Boolean
+        isDescriptionPrivate : Boolean
+    }
+    
+    type awardsRecognitionOutput{
+          awardName:String
+          awardId:String
+          isAwardPrivate:Boolean
+          year:String
+          isYearPrivate:Boolean
+          description:String
+          isDescriptionPrivate:Boolean
+          logo:imagesTypeSchema,
+          makePrivate:Boolean,
+          index: Int
+    }
+
+    type membershipsOutput{
+        description:String, 
+        isDescriptionPrivate :Boolean
+    }
+    
+    type compliancesOutput{
+        description:String, 
+        isDescriptionPrivate :Boolean
+    }
+    
+    type licensesOutput{
+        description:String, 
+        isDescriptionPrivate :Boolean
+    }
+    
+    type ratingOutput{,
+        rating:String
+        isRatingPrivate:Boolean
+    }
+    type institutePortfolioAboutUsOutput{
+        portfolioDetailsId  : String
+        aboutUs             : aboutUsOutput
+        rating              : ratingOutput
+        serviceProducts     : serviceProductsOutput
+        information         : informationOutput
+        clients             : [clientsOutput]
+        data                : dataOutput
+    }
+
+   type EmploymentOfCompany{
+      eofAbout: String
+      eofFromMonth: String
+      eofFromYear: String
+      eofToMonth: String
+      eofToYear: String
+      eofNumberOfEmployment: Int
+      index : Int
+   }
+    type ProfitRevenueLiability{
+      prlEntityType: String
+      prlFromMonth: String
+      prlFromYear: String
+      prlToMonth: String
+      prlToYear: String
+      pelValueType: String
+      prlValue: Int
+      prlabout:String
+      index : Int
+    }
+    type ReviewOfCompany{
+      rofYear: String
+      rofValue: Int
+      rofAbout: String
+      index : Int
+    }
+    type EmployeeBreakupDepartment{
+      ebdFromMonth: String
+      ebdFromYear: String
+      ebdToMonth: String
+      ebdToYear: String
+      ebdDepartment: String
+      ebdNumberOfEmployment: Int
+      ebdAbout: String
+      index : Int
+      ebdDepartmentName:String
+    }
+        
+   type chartsOutput{
+      employmentOfCompanyChart:[EmploymentOfCompany]
+      profitRevenueLiabilityChart:[ProfitRevenueLiability]
+      reviewOfCompanyChart:[ReviewOfCompany]
+      employeeBreakupDepartmentChart:[EmployeeBreakupDepartment]
+    }
+    
+    type dataOutput{
+      balanceSheet: [imagesTypeSchema]
+      profitAndLoss: [imagesTypeSchema]
+      quaterlyReport: [imagesTypeSchema]
+      yearlyReport: [imagesTypeSchema]
+      halfYearlyReport: [imagesTypeSchema]
+      annualReport: [imagesTypeSchema]
+      cashFlow: [imagesTypeSchema]
+      shareHoldings: [imagesTypeSchema]
+      capitalStructure: [imagesTypeSchema]
+      ratio: [imagesTypeSchema]
+    }
+    
+    
+    
+    input logo{
+      fileName : String,
+      fileUrl:String,
+      isDefault:Boolean
+    }
+    
+    input instituteManagement{
+       title :String
+       isTitlePrivate : Boolean
+       firstName : String
+       isFirstNamePrivate : Boolean
+       lastName : String
+       isLastNamePrivate : Boolean
+       middleName : String
+       isMiddleNamePrivate : Boolean
+       qualification : String
+       isQualificationPrivate :  Boolean
+       certification : String
+       isCertificationPrivate : Boolean
+       profilePic : String
+       isProfilePicPrivate : Boolean
+       gender : String
+       isGenderPrivate : Boolean
+       designation : String
+       isDesignationPrivate: Boolean
+       yearsOfExperience : String
+       isYOEPrivate : Boolean
+       joiningDate : String
+       isJoiningDatePrivate : Boolean
+       firstJobJoiningDate : String
+       isFJJDPrivate : Boolean
+       universities :   String
+       isUniversitiesPrivate: Boolean
+       awards : String
+       isAwardsPrivate : Boolean
+       linkedInUrl : String
+       isLinkedInUrlPrivate : Boolean
+       about : String
+       isAboutPrivate:Boolean
+       logo:logo
+       index: Int
+    }
+  
+    input clients{
+        companyName:String,
+        isCompanyNamePrivate:Boolean,
+        logo:logo,
+        description:String,
+        isDescriptionPrivate:Boolean,
+        makePrivate:Boolean,
+        index:Int
+    }
+   
+    input investor{
+        name:String,
+        fundingType:String,
+        fundingTypeId:String,
+        investmentAmount:String,
+        investorImage:String,
+        description:String,
+        isNamePrivate:Boolean,
+        isInvestorImagePrivate:Boolean,
+        isInvestmentAmountPrivate:Boolean,
+        isDescriptionPrivate:Boolean,
+        logo:logo,
+        makePrivate:Boolean
+        index: Int
+    }
+    
+
+    input information{
+        description:String,
+        isDescriptionPrivate:Boolean
+    }
+    
+    input serviceProducts{
+        description:String,
+        isDescriptionPrivate:Boolean
+    }
+    
+    input legalIssue{
+        description:String,
+        isDescriptionPrivate:Boolean
+    }
+    
+    input data{
+      balanceSheet: [imageFilesInputSchema]
+      profitAndLoss: [imageFilesInputSchema]
+      quaterlyReport: [imageFilesInputSchema]
+      yearlyReport: [imageFilesInputSchema]
+      halfYearlyReport: [imageFilesInputSchema]
+      annualReport: [imageFilesInputSchema]
+      cashFlow: [imageFilesInputSchema]
+      shareHoldings: [imageFilesInputSchema]
+      capitalStructure: [imageFilesInputSchema]
+      ratio: [imageFilesInputSchema]
+    }
+    
+    input imageFilesInputSchema{
+       fileUrl: String,
+       fileName:String
+     }
+     
+    input rating{
+        rating:String
+        isRatingPrivate:Boolean
+    }
+    
+    input aboutUs{
+        aboutImages      : [imageFilesInputSchema]
+        aboutImages : String
+        aboutDescription : String
+        annotatorId : String
+        isLogoPrivate :Boolean
+        isDescriptionPrivate : Boolean
+    }
+    
+    input awardsRecognition{
+          awardName:String
+          awardId:String
+          isAwardPrivate:Boolean
+          year:String
+          isYearPrivate:Boolean
+          description:String
+          isDescriptionPrivate:Boolean
+          makePrivate:Boolean
+          logo : logo,
+          index: Int
+    }
+
+    input memberships{
+        description:String, 
+        isDescriptionPrivate :Boolean
+    }
+    
+    input compliances{
+         description:String, 
+        isDescriptionPrivate :Boolean
+    }
+    
+    input licenses{
+         description:String, 
+        isDescriptionPrivate :Boolean
+    }
+    input employmentOfCompany{
+      eofAbout: String
+      eofFromMonth: String
+      eofFromYear: String
+      eofToMonth: String
+      eofToYear: String
+      eofNumberOfEmployment: Int
+      index: Int
+    }
+    input profitRevenueLiability{
+      prlEntityType: String
+      prlFromMonth: String
+      prlFromYear: String
+      prlToMonth: String
+      prlToYear: String
+      pelValueType: String
+      prlValue: Int
+      prlabout:String
+      index : Int
+    }
+    input reviewOfCompany{
+      rofYear: String
+      rofValue: Int
+      rofAbout: String
+      index : Int
+    }
+    input employeeBreakupDepartment{
+      ebdFromMonth: String
+      ebdFromYear: String
+      ebdToMonth: String
+      ebdToYear: String
+      ebdDepartment: String
+      ebdNumberOfEmployment: Int
+      ebdAbout: String
+      index : Int
+    }
+   
+    input institutePortfolio{
+        portfolioDetailsId  : String
+        licenses            : licenses
+        compliances         : compliances
+        memberships         : memberships
+        awardsRecognition : [awardsRecognition]
+        aboutUs             : aboutUs
+        rating              : rating
+        serviceProducts     : serviceProducts
+        information         : information
+        investor            : [investor]
+        clients             : [clients]
+        management          : [instituteManagement]
+        employmentOfCompanyChart: [employmentOfCompany]
+        reviewOfCompanyChart: [reviewOfCompany]
+        profitRevenueLiabilityChart:  [profitRevenueLiability]
+        employeeBreakupDepartmentChart: [employeeBreakupDepartment]
+        legalIssue          : legalIssue
+        data                : data
+    }
+    type institutePortfolioOutput{
+        _id                  : String
+        userId               : String
+        communityType        : String
+        portfolioDetailsId   : String
+        aboutUs              : aboutUsOutput
+    }
+    
+    type Query{
+        fetchInstitutePortfolioAboutUs(portfoliodetailsId:String!):institutePortfolioAboutUsOutput
+        fetchInstitutePortfolioLicenses(portfoliodetailsId:String!):licensesOutput
+        fetchInstitutePortfolioMemberships(portfoliodetailsId:String!):membershipsOutput
+        fetchInstitutePortfolioCompliances(portfoliodetailsId:String!):compliancesOutput
+        fetchInstitutePortfolioManagement(portfoliodetailsId:String!):[instituteManagementOutput]
+        fetchInstitutePortfolioAwards(portfoliodetailsId:String!):[awardsRecognitionOutput]
+        fetchInstitutePortfolioCharts(portfoliodetailsId:String):chartsOutput   
+        fetchInstitutePortfolioData(portfoliodetailsId:String):dataOutput
+    }
+    
+    type Mutation{
+        createInstitutePortfolio(portfolio:institutePortfolio):response
+        updateInstitutePortfolio(portfoliodetailsId:String,portfolio:institutePortfolio):response
+        
+    }
+`
+
+MlSchemaDef['schema'] = mergeStrings([MlSchemaDef['schema'], institutePortfolioSchema]);
+
+let supportedApi = [
+  {api:'fetchInstitutePortfolioAboutUs', actionName:'READ', moduleName:"PORTFOLIO"},
+  {api:'fetchInstitutePortfolioMemberships', actionName:'READ', moduleName:"PORTFOLIO"},
+  {api:'fetchInstitutePortfolioCompliances', actionName:'READ', moduleName:"PORTFOLIO"},
+  {api:'fetchInstitutePortfolioLicenses', actionName:'READ', moduleName:"PORTFOLIO"},
+  {api:'fetchInstitutePortfolioManagement', actionName:'READ', moduleName:"PORTFOLIO"},
+  {api:'fetchInstitutePortfolioAwards', actionName:'READ', moduleName:"PORTFOLIO"},
+  {api:'fetchInstitutePortfolioCharts', actionName:'READ', moduleName:"PORTFOLIO"},
+  {api:'fetchInstitutePortfolioData', actionName:'READ', moduleName:"PORTFOLIO"},
+
+  {api:'createInstitutePortfolio', actionName:'CREATE', moduleName:"PORTFOLIO"},
+  {api:'updateInstitutePortfolio', actionName:'UPDATE', moduleName:"PORTFOLIO"},
+  {api:'createInstitutePortfolioChart', actionName:'UPDATE', moduleName:"PORTFOLIO"}
+]
+MlResolver.MlModuleResolver.push(supportedApi)

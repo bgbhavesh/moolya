af3a06d8a6202e43ebe7c004f51d952722254a62
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-08-22 14:49:21 +0530

    appheader communityname

--- client/app/commons/actions/fetchUserDetails.js
+++ client/app/commons/actions/fetchUserDetails.js
@@ -21,6 +21,7 @@ export async function fetchUserDetailsHandler() {
             userName
             countryId
             registrationType
+            communityName
           }
         }
       }

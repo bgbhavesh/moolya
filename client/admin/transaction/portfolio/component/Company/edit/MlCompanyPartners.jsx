7d83e02647f42a68599453edff657e424f637678
Mukhil.Padmanabhan <mukhil.padmanabhan@raksan.in>
2017-08-28 17:10:43 +0530

    Experience validations done in portfolios

--- client/admin/transaction/portfolio/component/Company/edit/MlCompanyPartners.jsx
+++ client/admin/transaction/portfolio/component/Company/edit/MlCompanyPartners.jsx
@@ -95,14 +95,51 @@ export default class MlCompanyPartners extends React.Component {
     })
   }
 
+
+  handleYearsOfExperience(value) {
+    let blankSpace = value.indexOf(' ') >= 0;
+    let experience = parseInt(value);
+    let valuesArray = value.split(".");
+    let decimalExperience = valuesArray.length > 0 ?  valuesArray[0] : "";
+    if(decimalExperience) {
+      experience = parseInt(decimalExperience);
+      if(experience > 75){
+        toastr.error('Experience cannot be more than 75 years')
+        return false;
+      }
+    }
+    if(blankSpace) {
+      toastr.error('Blank spaces are not allowed')
+      return false;
+    } else if(experience > 75 ) {
+      toastr.error('Experience cannot be more than 75 years')
+      return false;
+    } else if (!experience) {
+      toastr.error('Experience not valid')
+      return false
+    } else {return true}
+  }
+
   handleBlur(e) {
     let details = this.state.data;
     let name = e.target.name;
-    details = _.omit(details, [name]);
-    details = _.extend(details, {[name]: e.target.value});
-    this.setState({data: details}, function () {
-      this.sendDataToParent()
-    })
+    let validExperience;
+    if(name === "yearsOfExperience") {
+      validExperience = this.handleYearsOfExperience(e.target.value)
+      if(validExperience) {
+        details = _.omit(details, [name]);
+        details = _.extend(details, {[name]: e.target.value});
+        this.setState({data: details}, function () {
+          this.sendDataToParent()
+        })
+      }
+    } else {
+      details = _.omit(details, [name]);
+      details = _.extend(details, {[name]: e.target.value});
+      this.setState({data: details}, function () {
+        this.sendDataToParent()
+      })
+    }
   }
   onSavePrincipalAction(e) {
     this.setState({partnersList: this.state.partners, popoverOpenP: false})
@@ -185,6 +222,7 @@ export default class MlCompanyPartners extends React.Component {
       })
     partners = arr;
       // funderPrincipal=_.omit(funderPrincipal,["privateFields"]);
+    console.log('partners', partners)
       this.setState({partners: partners})
       this.props.getPartnersDetails(partners, this.state.privateKey);
 

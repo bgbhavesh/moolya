8abba8fa2785c4c7957aedb577f9443b6de3aaa9
sravani_sistla <sravani.sistla@raksan.in>
2017-08-30 20:57:27 +0530

    UI Changes

--- client/admin/settings/cities/component/MlCitiesList.jsx
+++ client/admin/settings/cities/component/MlCitiesList.jsx
@@ -11,7 +11,7 @@ export default class MlCitiesList extends Component {
     return (
       <div className="admin_main_wrap">
         <div className="admin_padding_wrap">
-          <h2>City List</h2>
+          <h2>Cities List</h2>
 
           <MlTableViewContainer {...mlCitiesTableConfig}/>
 
--- server/MlExternalUsers/MenuSeedData/browserMenuSeedData.js
+++ server/MlExternalUsers/MenuSeedData/browserMenuSeedData.js
@@ -6,7 +6,7 @@ if(Meteor.isServer){
     "name":"mlBrowserMenu",
     "menu" : [
       {
-        "image":"ml ml-dashboard",
+        "image":"ml my-ml-dashboard",
         "link": "/app/dashboard",
         "isLink" : true,
         "isMenu" : true,

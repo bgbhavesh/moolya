47ca6a448d2d0c05336aa03a23a92966c0aab93b
Pankaj <pankajkumar.jatav@raksan.in>
2017-09-08 18:54:34 +0530

    WIP :: Search and filter for MyList

--- client/app/profile/components/myList/MyList.jsx
+++ client/app/profile/components/myList/MyList.jsx
@@ -11,7 +11,11 @@ import MyFavourites from './MyFavourites';
 import MyWishlist from './MyWishlist';
 import MyFollowers from './MyFollowers';
 import MyFollowings from './MyFollowings';
-
+import MlInfiniteScroll from '../../../../commons/core/mlInfiniteScroll/components/MlInfiniteScroll';
+import {mlAppMyConnectionConfig} from '../../config/mlAppMyConnectionsConfig';
+import {mlAppMyFavouritesConfig} from '../../config/mlAppMyFavouritesConfig';
+import {mlAppMyFollowersConfig} from '../../config/mlAppMyFollowersConfig';
+import {mlAppMyFollowingsConfig} from '../../config/mlAppMyFollowingsConfig';
 
 export default class MyList extends React.Component{
   constructor(props){
@@ -37,10 +41,22 @@ export default class MyList extends React.Component{
 
   getTabComponents(){
     let tabs = [
-      {tabClassName:'ml ml-connect', title: <b>My Connections</b>,component: <MyConnections/>},
-      {tabClassName:'ml my-ml-favourites',title: <b>My Favourites</b>,component: <MyFavourites/>},
-      {tabClassName:'ml ml-connect',title: <b>My Followers</b>,component: <MyFollowers/>},
-      {tabClassName:'ml ml-connect',title: <b>I Follow</b>,component: <MyFollowings/>}
+      {
+        tabClassName:'ml ml-connect', title: <b>My Connections</b>,
+        component: <MlInfiniteScroll viewMode={false} showInfinity={false} config={mlAppMyConnectionConfig} />
+      },
+      {
+        tabClassName:'ml my-ml-favourites',title: <b>My Favourites</b>,
+        component: <MlInfiniteScroll viewMode={false} showInfinity={false} config={mlAppMyFavouritesConfig} />
+      },
+      {
+        tabClassName:'ml ml-connect',title: <b>My Followers</b>,
+        component: <MlInfiniteScroll viewMode={false} showInfinity={false} config={mlAppMyFollowersConfig} />
+      },
+      {
+        tabClassName:'ml ml-connect',title: <b>I Follow</b>,
+        component: <MlInfiniteScroll viewMode={false} showInfinity={false} config={mlAppMyFollowingsConfig} />
+      }
     ]
     return tabs;
   }

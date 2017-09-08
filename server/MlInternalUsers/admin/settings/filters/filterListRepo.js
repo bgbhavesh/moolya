818edf88f912f74911858cd39e208d08d598d6a2
sireesha <sireesha.dhavala@raksanconsulting.com>
2017-09-09 00:16:49 +0530

    FIXED::MOOLYA-2815 And stepzilla issue

--- server/MlInternalUsers/admin/settings/filters/filterListRepo.js
+++ server/MlInternalUsers/admin/settings/filters/filterListRepo.js
@@ -186,7 +186,7 @@ export default class MlFilterListRepo{
             if(userProfile.hierarchyLevel == 4 || userProfile.hierarchyLevel == 3 || userProfile.hierarchyLevel == 2 || userProfile.hierarchyLevel == 0){
               let arrayOfGenSubChapter = _.pluck(requestParams.filteredListId, 'value') || [];
               if(requestParams.fieldActive == "Cluster"){
-                result= MlSubChapters.find({clusterId: {$in : arrayOfGenSubChapter},isActive : true}).fetch();
+                result= MlSubChapters.find({clusterId: {$in : arrayOfGenSubChapter}, chapterId: {$in : arrayOfGenSubChapter},isActive : true}).fetch();
               }else if(requestParams.fieldActive == "Chapter"){
                 result= MlSubChapters.find({chapterId: {$in : arrayOfGenSubChapter},isActive : true}).fetch();
               }else{

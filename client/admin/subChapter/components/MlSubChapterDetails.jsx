a3feb35579a74c3163b6631a522e65ce201183dc
vishwadeep <vishwadeep.kapoor@raksan.in>
2017-09-15 16:21:57 +0530

    ideas isActive with default value

--- client/admin/subChapter/components/MlSubChapterDetails.jsx
+++ client/admin/subChapter/components/MlSubChapterDetails.jsx
@@ -255,6 +255,14 @@ class MlSubChapterDetails extends React.Component {
 
           <div className="admin_padding_wrap">
             <h2>Sub-Chapter Details</h2>
+            <div onClick={this.anchorRedirect}>
+              <a href="">
+                <div className="hanging_btn">
+                  <span className="ml ml-moolya-symbol"></span>
+                  Info
+                </div>
+              </a>
+            </div>
             <div className="col-md-6 nopadding-left">
               <div className="form_bg left_wrap">
                 <ScrollArea
@@ -329,14 +337,7 @@ class MlSubChapterDetails extends React.Component {
                             className="form-control float-label">
                   </textarea>
                     </div>
-                    <div onClick={this.anchorRedirect}>
-                      <a href="">
-                        <div className="hanging_btn">
-                          <span className="ml ml-moolya-symbol"></span>
-                          Info
-                        </div>
-                      </a>
-                    </div>
+
                   </form>
                 </ScrollArea>
               </div>

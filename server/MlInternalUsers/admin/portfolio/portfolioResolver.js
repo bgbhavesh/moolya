b8576a4928961771120f6cdc9aeea6bdfd88bc8a
Kanwar <kanwarjeet.singh@raksan.in>
2017-09-19 18:48:50 +0530

    Added Branches and No-Info Image

--- server/MlInternalUsers/admin/portfolio/portfolioResolver.js
+++ server/MlInternalUsers/admin/portfolio/portfolioResolver.js
@@ -13,7 +13,7 @@ import {getCommunityName} from '../../../commons/utils';
 import MlNotificationController from '../../../mlNotifications/mlAppNotifications/mlNotificationsController'
 import mlSmsConstants from '../../../mlNotifications/mlSmsNotifications/mlSmsConstants'
 import mlRegistrationRepo from "../../admin/registration/mlRegistrationRepo";
-import  MlSiteMapInsertion from '../../../MlExternalUsers/sitemap/microSiteRepo/MlSiteMapInsertion'
+import  MlSiteMapInsertion from '../../../MlExternalUsers/microSite/microSiteRepo/MlSiteMapInsertion'
 /**
  * @module [externaluser portfolio Landing]
  * @params [context.userId]

3034459b60cd06defaddfa378869e3714e0f9e90
sravani_sistla <sravani.sistla@raksan.in>
2017-09-21 15:56:07 +0530

    Phone Number Validation and Email Validation For Registration Icons Added For Communities UI Changes For App

--- client/commons/validations/mlfieldValidation.js
+++ client/commons/validations/mlfieldValidation.js
@@ -56,3 +56,8 @@ export function validatedPhoneNumber(countryCode, contactNumber){
   }
   return isValidPhoneNumber;
 }
+
+export function validatedEmailId(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}

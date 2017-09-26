463cca82acde1b05fcda9df9008a712e44bcb7bd
A S <amit.singh@raksan.in>
2017-09-26 22:37:54 +0530

    Ui fixes for ProcessMapping/Document Mapping create screens

--- client/admin/settings/documentMapping/component/MlAddDocumentMapping.jsx
+++ client/admin/settings/documentMapping/component/MlAddDocumentMapping.jsx
@@ -198,12 +198,7 @@ class MlAddDocumentMapping extends React.Component{
         <div className="admin_padding_wrap">
           <h2>Create Document</h2>
           <div className="main_wrap_scroll">
-            <ScrollArea
-              speed={0.8}
-              className="main_wrap_scroll"
-              smoothScrolling={true}
-              default={true}
-            >
+
           <div className="col-md-6 nopadding-left">
             <div className="left_wrap">
               <ScrollArea
@@ -284,7 +279,7 @@ class MlAddDocumentMapping extends React.Component{
               </form>
             </div>
           </div>
-            </ScrollArea>
+
           </div>
           <MlActionComponent ActionOptions={MlActionConfig} showAction='showAction' actionName="actionName"/>
 

a6c9e44c1dd488d9f5ea2390ed7fba7be730ad50
shubhankit <shubhankit.saxena@raksan.in>
2017-09-28 16:52:37 +0530

    updates: MOOLYA-3384

--- client/commons/fetchAssignUsers.js
+++ client/commons/fetchAssignUsers.js
@@ -19,6 +19,7 @@ export async function fetchAssignUsersActionHandler(clusterId, chapterId, subCha
             username,
             profile{
               isActive
+              profileImage
             }
         }
       }

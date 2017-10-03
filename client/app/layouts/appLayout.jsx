fbd647eba72d4116afe61df107408e8603c9596f
Akhil <akhil.pab@gmail.com>
2017-10-03 18:37:27 +0530

    WIP: MOOLYA-2940 Add redux into moolya

--- client/app/layouts/appLayout.jsx
+++ client/app/layouts/appLayout.jsx
@@ -4,8 +4,21 @@ import { render } from 'react-dom';
 import {appClient} from '../core/appConnection';
 import {ApolloProvider} from 'react-apollo';
 import MetaTags from 'react-meta-tags';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 import MlAppLayout from '../core/components/MlAppComponent'
+
+const store = createStore(
+  combineReducers({
+    appRedux: () => ({type: 'App'}),
+    apollo: appClient.reducer(),
+  }),
+  {}, // initial state
+  compose(
+    applyMiddleware(appClient.middleware()),
+    (typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined') ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+  )
+);
 
 export default class AppLayout extends Component{
     constructor(props,AppLayoutcontext){
@@ -19,7 +32,7 @@ export default class AppLayout extends Component{
                     <title> {'moolya'}</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                 </MetaTags>
-                <ApolloProvider client={appClient}>
+                <ApolloProvider store={store} client={appClient}>
                     <div className="moolya_app">
                         <MlAppLayout {...this.props}/>
                     </div>

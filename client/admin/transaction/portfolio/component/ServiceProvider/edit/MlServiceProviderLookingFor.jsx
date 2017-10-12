cbbad515aeef6e7c385ff1370b7af2450ccae51d
poojitha <pujitha.maru@raksanconsulting.com>
2017-10-12 14:41:41 +0530

    UI Changes

--- client/admin/transaction/portfolio/component/ServiceProvider/edit/MlServiceProviderLookingFor.jsx
+++ client/admin/transaction/portfolio/component/ServiceProvider/edit/MlServiceProviderLookingFor.jsx
@@ -281,11 +281,10 @@ export default class MlServiceProviderLookingFor extends Component {
                       return (<div className="col-lg-2 col-md-3 col-sm-3" key={idx}>
                         <a id={"create_client" + idx}>
                           <div className="list_block">
-                            <div className="cluster_status">
                               <FontAwesome name='unlock' id="makePrivate" defaultValue={details.makePrivate}/>
                               <input type="checkbox" className="lock_input" id="makePrivate"
                                      checked={details.makePrivate}/>
-                            </div>
+
                             <div className="hex_outer" onClick={that.onTileClick.bind(that, idx)}>
                               <span className="ml my-ml-browser_3"/>
                             </div>

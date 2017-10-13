c77034df71aad36d867e94db084660d5c3477885
rajat shekhar <rajatshekhar.srivastava@raksanconsulting.com>
2017-10-13 14:53:51 +0530

    Missing Lat Lng of SubChapter issue fixed

--- server/seed-data.js
+++ server/seed-data.js
@@ -847,3 +847,22 @@ let templates = [{stepName:"Soft",stepCode:"SOFT",templateCode:"",templateName:"
 
 
 /******************************************* Templates <Start> *******************************************************/
+
+
+
+// SubChapter Latitude and Longitude
+if(Meteor.isServer){
+  var subC = MlSubChapters.find({$or:[{latitude:{$exists:false}}, {latitude:NaN}]}).fetch()
+  addNoise = () => {
+    let x = Math.random() * 0.0007 ;
+    x=Math.pow(-1,new Date().getMilliseconds()%2) * x + ""; // random +/-
+    return parseFloat(x.substr(0,8));
+  }
+
+  subC.forEach(function(sub){
+    var chapter = MlChapters.findOne({_id:sub.chapterId});
+    var lat = chapter.latitude + addNoise();
+    var lng = chapter.longitude + addNoise();
+    MlSubChapters.update({_id:sub._id}, {$set:{latitude:lat, longitude:lng}})
+  })
+}

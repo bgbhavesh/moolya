492d523f54a568d013454db9cfd77181ba66b463
Vipul Jain <vipul.jain@raksan.in>
2017-10-25 13:43:53 +0530

    Generate Absolute Path generic function

---
+++ client/lib/mlGenerateAbsolutePath.js
@@ -0,0 +1,27 @@
+/*
+generate the absolute path for static files using the relative path
+ */
+
+let removeStartingSlash = (path)=>{
+  if(path[0]=='/') path = path.split('/')[1];
+  return path;
+}
+
+
+let generateAbsolutePath = (relativePath,folderName)=>{
+  if(!relativePath) return;
+  let baseUrl = Meteor.settings.public.minio.minioConfig.endPoint + ':'
+                    + Meteor.settings.public.minio.minioConfig.port + '/'
+                    + Meteor.settings.public.minio.minioConfig.bucketName + '/';
+
+  relativePath = removeStartingSlash(relativePath);
+
+  if(folderName) relativePath = folderName + '/' + relativePath;
+
+  let absolutePath = baseUrl + relativePath;
+
+  return absolutePath;
+};
+
+
+module.exports = generateAbsolutePath;

62fc3f2a69d0c110e0bdfdff26288859bf30c76c
sravani_sistla <sravani.sistla@raksan.in>
2017-10-25 19:04:54 +0530

    App Profile Changes fixed

--- client/admin/transaction/requested/component/step7.jsx
+++ client/admin/transaction/requested/component/step7.jsx
@@ -4,19 +4,19 @@ import { render } from 'react-dom';
 
 export default class Step7 extends React.Component{
 componentDidMount()
-  { 
+  {
     var WinHeight = $(window).height();
     $('.step_form_wrap').height(WinHeight-(160+$('.admin_header').outerHeight(true)));
   }
   render(){
     return (
       <div className="step_form_wrap step5">
-      
-      
-    
-    
-     
-      </div>      
+
+
+    <h3>Coming Soon....</h3>
+
+
+      </div>
     )
   }
 };

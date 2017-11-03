973863db27ebdbb786bc9f82e86648988d8e6d40
sravani_sistla <sravani.sistla@raksan.in>
2017-11-03 17:36:27 +0530

    Moolya::1952 Issue Fixed

--- server/MlInternalUsers/admin/settings/roles/roleResolver.js
+++ server/MlInternalUsers/admin/settings/roles/roleResolver.js
@@ -30,9 +30,8 @@ MlResolver.MlMutationResolver['createRole'] = (obj, args, context, info) => {
     let response = new MlRespPayload().errorPayload("Role Name is required", code);
     return response;
   }
-
   // if(MlClusters.find({roleName:role.roleName}).count() > 0){
-  if (mlDBController.find('MlRoles', {roleName: role.roleName}, context).count() > 0) {
+  if (mlDBController.find('MlRoles', {roleName:{'$regex' : "^"+role.roleName+"$", "$options" : "i"}} && {displayName:{'$regex' : "^"+role.displayName+"$", "$options" : "i"}}, context).count() > 0) {
     let code = 409;
     let response = new MlRespPayload().errorPayload("Already Exist", code);
     return response;

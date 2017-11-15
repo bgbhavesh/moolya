09fac532763fe2c2b7090362a9dfc42c49d8e18e
Pankaj <pankajkumar.jatav@raksan.in>
2017-11-15 17:25:39 +0530

    FIXED :: MOOLYA-3436

--- client/admin/transaction/portfolio/component/Funders/edit/MlFunderServiceBooking.jsx
+++ client/admin/transaction/portfolio/component/Funders/edit/MlFunderServiceBooking.jsx
@@ -253,11 +253,16 @@ export default class FunderAboutView extends React.Component{
                     <tbody>
                     <tr>
                       <td>Actual Amount</td>
-                      <td>: {that.props.serviceDetails.payment?that.props.serviceDetails.payment.tasksAmount:0}</td>
+                      <td>: {that.props.serviceDetails.payment?that.props.serviceDetails.payment.tasksDerived:0}</td>
                     </tr>
                     <tr>
                       <td>Discount Amount</td>
-                      <td>: {that.props.serviceDetails.payment?that.props.serviceDetails.payment.tasksDiscount:0}</td>
+                      <td>: {
+                        that.props.serviceDetails.payment ?
+                          that.props.serviceDetails.payment.discountType == "amount" ?
+                            that.props.serviceDetails.payment.discountValue :
+                            (that.props.serviceDetails.payment.tasksDerived / 100 * that.props.serviceDetails.payment.discountValue).toFixed(4)
+                        : 0 }</td>
                     </tr>
                     {/*<tr>*/}
                       {/*<td>Tax</td>*/}
@@ -266,7 +271,12 @@ export default class FunderAboutView extends React.Component{
                     <tr>
                       <td>Facilitation charges</td>
                       <td>{ that.props.serviceDetails.finalAmount && that.props.serviceDetails.payment ?
-                        parseFloat(that.props.serviceDetails.finalAmount - that.props.serviceDetails.payment.tasksDerived).toFixed(4)
+                        parseFloat(that.props.serviceDetails.finalAmount -
+                          that.props.serviceDetails.payment.tasksDerived +
+                          (that.props.serviceDetails.payment.discountType == "amount" ?
+                            that.props.serviceDetails.payment.discountValue :
+                            that.props.serviceDetails.payment.tasksDerived / 100 * that.props.serviceDetails.payment.discountValue)
+                        ).toFixed(4)
                         : 0 }</td>
                     </tr>
                     <tr>

ce7ea342b77f120325a724126b306e47b4844336
vishwadeep_raksan <vishwadeep.kapoor@raksan.in>
2017-11-28 15:04:25 +0530

    fixed:: MOOLYA-3455

--- client/admin/community/components/MlCommunityList.jsx
+++ client/admin/community/components/MlCommunityList.jsx
@@ -1,8 +1,11 @@
 import React, { Component, PropTypes } from 'react';
-import { render } from 'react-dom';
-var FontAwesome = require('react-fontawesome');
 import communityRoutes from '../actions/communityRouteHandler';
 export default class MlCommunityList extends Component {
+  constructor(props){
+    super(props);
+    this.userhierarchy = this.props.userhierarchy;
+    return this
+  }
 
   render(){
     const data=this.props.data||[];
@@ -10,9 +13,13 @@ export default class MlCommunityList extends Component {
       <div className="col-lg-2 col-md-4 col-sm-4" key={idx}>
         <div className="list_block">
           <div className={`cluster_status ${prop.isActive?"active":"inactive"}_cl `}></div>
-          {prop.isActive?<a href={communityRoutes.communityListRoute(prop.code)}> <div className={"hex_outer"}><span className={prop.communityImageLink}></span></div></a>
+          {prop.isActive || this.userhierarchy === 4 ? <a href={communityRoutes.communityListRoute(prop.code)}>
+              <div className={"hex_outer"}><span className={prop.communityImageLink}></span></div>
+            </a>
             :
-            <a> <div className={"hex_outer"}><span className={prop.communityImageLink}></span></div></a>
+            <a>
+              <div className={"hex_outer"}><span className={prop.communityImageLink}></span></div>
+            </a>
           }
           <h3>{prop.name}</h3>
         </div>

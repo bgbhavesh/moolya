ea34d8197ae285956ba68f7cfd8bdd5bcfd99a45
vipul <vipul.jain@raksan.in>
2017-11-30 19:23:49 +0530

    FIXED::MOOLYA-540

--- client/routes/routes.jsx
+++ client/routes/routes.jsx
@@ -9,24 +9,32 @@ import MlLogout from '../login/layouts/MlLogout'
 
 let userId = Meteor.userId();
 const localStorageLoginToken = localStorage.getItem('Meteor.loginToken');
+const rememberMe = localStorage.getItem('rememberMe');
 
 FlowRouter.route('/', {
   action: async function() {
-    let response = await findUserActionHandler(localStorageLoginToken);
-    console.log(response)
-    if(response){
-      if(response.success){
-        let user = JSON.parse(response.result);
-        if(user){
-          FlowRouter.go("/app");
-        }else{
-          FlowRouter.go("/admin");
+
+
+    if(!rememberMe || rememberMe==='false'){  //check for remember me
+      localStorage.clear();
+      FlowRouter.go('/login');
+    }
+    else{
+      let response = await findUserActionHandler(localStorageLoginToken);
+      if(response){
+        if(response.success){
+          let user = JSON.parse(response.result);
+          if(user){
+            FlowRouter.go("/app");
+          }else{
+            FlowRouter.go("/admin");
+          }
+        }else {
+          FlowRouter.go('/login');
         }
       }else {
         FlowRouter.go('/login');
       }
-    }else {
-      FlowRouter.go('/login');
     }
   },
   triggersEnter: [function(context, redirect) {

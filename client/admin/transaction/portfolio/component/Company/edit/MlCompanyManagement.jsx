0975885ff4a3ac2f18b9a6a929bf6fbf4be83253
sravani_sistla <sravani.sistla@raksan.in>
2017-12-06 19:24:09 +0530

    scroll fix for CMP

--- client/admin/transaction/portfolio/component/Company/edit/MlCompanyManagement.jsx
+++ client/admin/transaction/portfolio/component/Company/edit/MlCompanyManagement.jsx
@@ -66,6 +66,14 @@ export default class MlCompanyManagement extends Component {
 
   }
   componentDidUpdate() {
+    var className = this.props.isAdmin ? "admin_header" : "app_header"
+    var WinWidth = $(window).width();
+    var WinHeight = $(window).height();
+    // $('.main_wrap_scroll').height(WinHeight-($('.admin_header').outerHeight(true)+120));
+    $('.main_wrap_scroll').height(WinHeight - ($('.' + className).outerHeight(true) + 120));
+    if (WinWidth > 768) {
+      $(".main_wrap_scroll").mCustomScrollbar({ theme: "minimal-dark" });
+    }
     initalizeFloatLabel();
     OnLockSwitch();
     dataVisibilityHandler();
@@ -413,7 +421,7 @@ export default class MlCompanyManagement extends Component {
           <div>
             <h2>Management</h2>
             <div className="tab_wrap_scroll">
-
+            <div className="main_wrap_scroll">
               <div className="col-lg-12">
                 <div className="row">
                   <div className="col-lg-2 col-md-3 col-sm-3">
@@ -569,6 +577,7 @@ export default class MlCompanyManagement extends Component {
                 <div className="ml_btn text-center" style={{'textAlign':'center'}}>
                   <a className="save_btn" onClick={this.onSaveAction}>Save</a>
                 </div>
+              </div>
               </div>
             </div>
           </div>)}

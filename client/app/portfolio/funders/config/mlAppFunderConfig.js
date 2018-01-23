2a3ccc51dd84d54b54ffdabffe703f3f26a1523b
sireesha <sireesha.dhavala@raksan.in>
2018-01-23 18:51:59 +0530

    Changes in few files

--- client/app/portfolio/funders/config/mlAppFunderConfig.js
+++ client/app/portfolio/funders/config/mlAppFunderConfig.js
@@ -13,7 +13,7 @@ export const mlAppFunderConfig=new MlViewer.View({
   pagination:true,
   moduleName:"FunderPortfolio",
   sort:true,
-  viewComponent:<MlAppFunderListView type="FUN"/>,
+  viewComponent:<MlAppFunderListView />,
   showActionComponent:true,
   graphQlQuery:gql`
               query SearchQuery($offset: Int, $limit: Int,$fieldsData:[GenericFilter], $sortData:[SortFilter]) {

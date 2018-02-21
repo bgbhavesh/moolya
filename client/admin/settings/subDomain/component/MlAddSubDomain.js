ecf8b7e586953e297395dbaf2f6f65c6f4068871
Srikanth R <srikanth.rachakonda@raksan.in>
2018-02-21 18:57:00 +0530

    MOOLYA-3638 subDomain add & edit issue resolved

--- client/admin/settings/subDomain/component/MlAddSubDomain.js
+++ client/admin/settings/subDomain/component/MlAddSubDomain.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import {Meteor} from 'meteor/meteor';
 import {render} from 'react-dom';
-import { graphql } from 'react-apollo';
+import {graphql} from 'react-apollo';
 import gql from 'graphql-tag'
 import MlActionComponent from '../../../../commons/components/actions/ActionComponent'
 import formHandler from '../../../../commons/containers/MlFormHandler';
-import Moolyaselect from  '../../../commons/components/MlAdminSelectWrapper'
+import Moolyaselect from '../../../commons/components/MlAdminSelectWrapper'
 import {addSubDomain} from '../actions/addSubDomainAction'
-import {OnToggleSwitch,initalizeFloatLabel} from '../../../utils/formElemUtil';
+import {OnToggleSwitch, initalizeFloatLabel} from '../../../utils/formElemUtil';
 import {mlFieldValidations} from '../../../../commons/validations/mlfieldValidation';
 import MlLoader from '../../../../commons/components/loader/loader'
+
 class MlAddSubDomain extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state={
-      industry:''
+    this.state = {
+      industry: ''
     }
     this.optionsBySelectTypeOfIndustry.bind(this)
     this.createSubDomain.bind(this)
@@ -26,71 +28,76 @@ class MlAddSubDomain extends React.Component {
   };
 
   async handleSuccess(response) {
-    if (response){
-      if(response.success)
+    if (response) {
+      if (response.success)
         FlowRouter.go("/admin/settings/documentProcess/SubDomainList");
       else
         toastr.error(response.result);
     }
   };
 
-  optionsBySelectTypeOfIndustry(val){
-    this.setState({industry:val})
+  optionsBySelectTypeOfIndustry(val) {
+    this.setState({industry: val})
     console.log(val)
   }
 
   async createSubDomain() {
-    let ret = mlFieldValidations(this.refs)
+    let ret = mlFieldValidations(this.refs);
     if (ret) {
       toastr.error(ret);
-    } else {
-      let subdomainInfo = {
+    }
+    else {
+      let subDomainInfo = {
         name: this.refs.name.value,
         displayName: this.refs.displayName.value,
         about: this.refs.about.value,
         industryId: this.state.industry,
         isActive: this.refs.isActive.checked,
         // icon:this.refs.assetIcon.files
-      }
+      };
 
-      const response = await addSubDomain(subdomainInfo)
+      const response = await addSubDomain(subDomainInfo);
       if (response.success) {
-        toastr.success("'Sub-Domain' added successfully")
-         FlowRouter.go("/admin/settings/documentProcess/SubDomainList");
-      } else
+        toastr.success("'Sub-Domain' added successfully");
+        FlowRouter.go("/admin/settings/documentProcess/SubDomainList");
+      }
+      else {
         toastr.error(response.result)
+      }
       // return response;
     }
   }
-  componentDidMount()  {
-    OnToggleSwitch(false,true);
+
+  componentDidMount() {
+    OnToggleSwitch(false, true);
     initalizeFloatLabel();
   }
+
   render() {
     let MlActionConfig = [
       {
         showAction: true,
         actionName: 'save',
-        handler: async(event) => this.props.handler(this.createSubDomain.bind(this), this.handleSuccess.bind(this), this.handleError.bind(this))
+        handler: async (event) => this.props.handler(this.createSubDomain.bind(this), this.handleSuccess.bind(this), this.handleError.bind(this))
       },
       {
         showAction: true,
         actionName: 'cancel',
-        handler: async(event) => {
+        handler: async (event) => {
           FlowRouter.go("/admin/settings/documentProcess/SubDomainList")
         }
       }
     ]
 
 
-    let industriesquery=gql` query{
+    let industriesquery = gql` query{
     data:fetchIndustries{label:industryName,value:_id}
     }`;
 
-    const showLoader=this.state.loading;
+    const showLoader = this.state.loading;
     return (
       <div className="admin_main_wrap">
-        {showLoader===true?(<MlLoader/>):(
+        {showLoader === true ? (<MlLoader/>) : (
 
           <div className="admin_padding_wrap">
             <h2>Create Sub Domain</h2>
@@ -98,11 +105,13 @@ class MlAddSubDomain extends React.Component {
               <div className="form_bg">
                 <form>
                   <div className="form-group mandatory">
-                    <input type="text" ref="name" placeholder="Name" className="form-control float-label" data-required={true} data-errMsg="Name is required"/>
+                    <input type="text" ref="name" placeholder="Name" className="form-control float-label"
+                           data-required={true} data-errMsg="Name is required"/>
                   </div>
                   <br className="clearfix"/>
                   <div className="form-group mandatory">
-                    <input type="text" ref="displayName" placeholder="Display Name" className="form-control float-label" data-required={true} data-errMsg="Display Name is required"/>
+                    <input type="text" ref="displayName" placeholder="Display Name" className="form-control float-label"
+                           data-required={true} data-errMsg="Display Name is required"/>
                   </div>
                 </form>
               </div>
@@ -111,7 +120,12 @@ class MlAddSubDomain extends React.Component {
               <div className="form_bg">
                 <form>
                   <div className="form-group">
-                    <Moolyaselect multiSelect={false} mandatory={true} ref="industry" placeholder="Select Type Of Industry" className="form-control float-label" valueKey={'value'} labelKey={'label'}  selectedValue={this.state.industry} queryType={"graphql"} query={industriesquery} onSelect={this.optionsBySelectTypeOfIndustry.bind(this)} isDynamic={true} data-required={true} data-errMsg="Industry Type is required"/>
+                    <Moolyaselect multiSelect={false} mandatory={true} ref="industry"
+                                  placeholder="Select Type Of Industry" className="form-control float-label"
+                                  valueKey={'value'} labelKey={'label'} selectedValue={this.state.industry}
+                                  queryType={"graphql"} query={industriesquery}
+                                  onSelect={this.optionsBySelectTypeOfIndustry.bind(this)} isDynamic={true}
+                                  data-required={true} data-errMsg="Industry Type is required"/>
                   </div>
                   <div className="form-group">
                     <textarea ref="about" placeholder="About" className="form-control float-label" id=""></textarea>
@@ -132,7 +146,7 @@ class MlAddSubDomain extends React.Component {
       </div>
     )
   }
-}
-;
+
+};
 
 export default MlAddSubDomain = formHandler()(MlAddSubDomain);
--- server/MlInternalUsers/admin/settings/subDomain/subDomainSchema.js
+++ server/MlInternalUsers/admin/settings/subDomain/subDomainSchema.js
@@ -3,54 +3,54 @@ import MlSchemaDef from '../../../../commons/mlSchemaDef'
 import MlResolver from '../../../../commons/mlResolverDef'
 
 let SubDomainSchema = `
-    type SubDomain
-    {
-        _id:String,
-        name:String,
-        displayName :String,
-        about: String,
-        industryId:String,
-        industryName:String,
-        isActive:Boolean
+  type SubDomain
+  {
+      _id:String,
+      name:String,
+      displayName :String,
+      about: String,
+      industryId:String,
+      industryName:String,
+      isActive:Boolean
       createdBy     : String
       createdDate   : Date
       updatedBy     : String
       updatedDate   : Date
-    }
-    
-    input SubDomainMasterData
-    {
-        name :String,
-        displayName :String,
-        about: String,
-        industryId:String,
-        isActive:Boolean
+  }
+  
+  input SubDomainMasterData
+  {
+      name :String,
+      displayName :String,
+      about: String,
+      industryId:String,
+      isActive:Boolean
       createdBy     : String
       createdDate   : Date
       updatedBy     : String
       updatedDate   : Date
-    }
-    
-    type Mutation 
-    {
-        createSubDomain(SubDomainMasterData:SubDomainMasterData, moduleName:String, actionName:String):response
-        updateSelectedSubDomain(SubDomainId:String, SubDomainMasterData:SubDomainMasterData):response
-    }
-    type Query{
-        findSubDomain(SubDomainId:String): SubDomain
-        fetchIndustryDomain(industryId: String):[SubDomain]
-        fetchIndustryBasedSubDomain(industryId: [String]):[SubDomain]
-    }
-`
+  }
+  
+  type Mutation 
+  {
+      createSubDomain(SubDomainMasterData:SubDomainMasterData, moduleName:String, actionName:String):response
+      updateSelectedSubDomain(SubDomainId:String, SubDomainMasterData:SubDomainMasterData):response
+  }
+  type Query{
+      findSubDomain(SubDomainId:String): SubDomain
+      fetchIndustryDomain(industryId: String):[SubDomain]
+      fetchIndustryBasedSubDomain(industryId: [String]):[SubDomain]
+  }
+`;
 
 MlSchemaDef['schema'] = mergeStrings([MlSchemaDef['schema'], SubDomainSchema]);
 let supportedApi = [
-  {api:'createSubDomain', actionName:'CREATE', moduleName:"SUBDOMAIN"},
-  {api:'updateSelectedSubDomain', actionName:'UPDATE', moduleName:"SUBDOMAIN"},
+  {api: 'createSubDomain', actionName: 'CREATE', moduleName: "SUBDOMAIN"},
+  {api: 'updateSelectedSubDomain', actionName: 'UPDATE', moduleName: "SUBDOMAIN"},
 
-  {api:'findSubDomain', actionName:'READ', moduleName:"SUBDOMAIN"},
-  {api:'fetchIndustryDomain', actionName:'READ', moduleName:"SUBDOMAIN", isWhiteList:true},
-  {api:'fetchIndustryBasedSubDomain', actionName:'READ', moduleName:"SUBDOMAIN", isWhiteList:true}
-]
+  {api: 'findSubDomain', actionName: 'READ', moduleName: "SUBDOMAIN"},
+  {api: 'fetchIndustryDomain', actionName: 'READ', moduleName: "SUBDOMAIN", isWhiteList: true},
+  {api: 'fetchIndustryBasedSubDomain', actionName: 'READ', moduleName: "SUBDOMAIN", isWhiteList: true}
+];
 MlResolver.MlModuleResolver.push(supportedApi)
 
